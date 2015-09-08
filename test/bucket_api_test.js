d067b6777504e28c37bb16edc6b54e5f12a55454
Lauren Spiegel <lauren.spiegel@scality.com>
2015-09-08 12:27:46 -0700

    Summarize rules for Get Bucket (List Objects) functionality.

--- test/bucket_api_test.js
+++ test/bucket_api_test.js
@@ -2,6 +2,7 @@
 1) Do we need to stub to prevent side effects?
 2) What about headers and AWS options?
 3) Specific response/error messages if key not found, etc.?
+4) Why not use AWS terminology?
 */
 
 
@@ -11,7 +12,7 @@ var Bucket = require("../lib/bucket.js");
 var sinon = require("sinon");
 
 
-describe("bucket API",function(){
+describe("bucket API for getting, putting and deleting objects in a bucket",function(){
 
   it("should create a bucket with a keyMap", function(done){
   	var bucket = new Bucket();
@@ -71,20 +72,24 @@ describe("bucket API",function(){
 		});
 	});
 
-
-
-	// it("should be able to get a key from a bucket using getPrefix AWS search"), function(done){
-
-	// });
-
-
-
 });
 
 
-describe('getPrefix', function() {
- 
+describe('bucket API for getting a subset of objects from a bucket', function() {
+/*	Implementation of AWS GET Bucket (List Objects) functionality
+	Rules:  
+		1) 	Return individual keys if key matches prefix but does not have delimeter.
+		2)	Return grouped keys under common prefixes if key starts with prefix and matches other keys 
+				before occurrence of delimeter.  For instance, "key2/sample" and "key2/moreSample" will be 
+				grouped together if prefix is "key" and delimeter is "/".
+		3)	If marker specified, only return keys that occur alphabetically AFTER the marker.
+		4)	If specify limit/max-keys, will only return up to that max.  All keys grouped under common-prefix,
+				will only count as one key to reach limit.  If not all keys returned due to reaching limit, 
+				"IsTruncated" will be set to true and "NextMarker" will specify the last key returned in
+				this search so that it can serve as the marker in the next search.  
+				*/
 
+	
  
 });
 
