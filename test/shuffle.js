51e2f6ce3aaa8bb5298a9a0c8cbebef121cb3381
Adrien Vergé <adrien.verge@scality.com>
2015-10-14 23:37:38 +0200

    Cleanup: Fix 2724 syntax problems
    Run the following homemade script on the code base to fix trivial syntax
    errors (mostly tabulations, indentation and unnecessary spaces):
    
        sed -i ':a; s/^\(\s*\)\t/\1    /g; ta' $(git ls-files '*.js')
        sed -i 's|^\(\s*\)//\(\S\)|\1// \2|g' $(git ls-files '*.js')
        sed -i ':a; /^\n*$/{$d;N;};/\n$/ba' $(git ls-files '*.js')
        node_modules/.bin/eslint --fix $(git ls-files '*.js')
        echo 'gg=G:wq' >/tmp/cmd.vim  # Vim plugin: pangloss/vim-javascript
        for file in $(git ls-files '*.js'); do vim -s /tmp/cmd.vim $file; done
    
    The number of syntax problems detected by eslint drops from 4534 to
    1810.

--- test/shuffle.js
+++ test/shuffle.js
@@ -1,18 +1,18 @@
 module.exports = function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
 };
