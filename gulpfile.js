6e42c3eebedf2904a5843d070b0ece1cde9ab4b8
Lauren Spiegel <lhspiegel@gmail.com>
2015-10-15 12:09:25 -0700

    CLEANUP: Style refactor of index.js and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    shell = require('gulp-shell');
+import gulp from 'gulp';
+import jshint from 'gulp-jshint';
+import shell from 'gulp-shell';
 
 /* lint all the things! */
-gulp.task('lint', function () {
+gulp.task('lint', function lintIt() {
     return gulp.src('./lib/*.js')
                             .pipe(jshint())
                             .pipe(jshint.reporter('default'))
                             .pipe(jshint.reporter('fail'));
-                            });
+});
 
     /* install pre-commit hook */
-    gulp.task('precommit-hook', shell.task([
-                'cp .pre-commit .git/hooks/pre-commit 2>/dev/null || :'
-    ]));
+gulp.task('precommit-hook', shell.task([
+    'cp .pre-commit .git/hooks/pre-commit 2>/dev/null || :'
+]));
 
-    gulp.task('default', ['lint']);
+gulp.task('default', ['lint']);
