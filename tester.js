400623d0f031cdd8b002625da074f6a4fcc559f5
Michael Zapata <michael.zapata@scality.com>
2015-11-04 20:33:37 +0100

    tests(functional): add s3cmd commands

--- tester.js
+++ tester.js
@@ -1,16 +1,19 @@
-'use strict';
+'use strict'; // eslint-disable-line strict
 
 const fs = require('fs');
 const proc = require('child_process');
+const process = require('process');
 
 const testDir = `${__dirname}/tests/functional`;
 
 function testing(dir) {
-    console.log(`testing ${dir}`);
+    process.stdout.write(`testing ${dir}\n`);
     let master;
-    try { master = fs.readFileSync(`${testDir}/${dir}/master.json`); }
-    catch (err) {
-        console.log('master.json file not found, skipping this directory');
+    try {
+        master = fs.readFileSync(`${testDir}/${dir}/master.json`);
+    } catch (err) {
+        process.stdout.write('master.json file not found, '
+                             + 'skipping this directory\n');
         return true;
     }
     const config = JSON.parse(master.toString());
@@ -22,7 +25,7 @@ function testing(dir) {
 }
 
 function main() {
-    console.log(`reading dirs in ${__dirname}/tests/functional`);
+    process.stdout.write(`reading dirs in ${__dirname}/tests/functional\n`);
     const dirs = fs.readdirSync(testDir);
     return dirs.reduce((prev, dir) => {
         return prev && testing(dir);
