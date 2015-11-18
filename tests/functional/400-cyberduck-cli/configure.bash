7a5ae04bad01703f12a59264da9d903edd982416
Michael Zapata <michael.zapata@scality.com>
2015-11-18 22:22:31 +0100

    feat(babel): Upgrade to babel6
    Fix Babel6 switch
    
    This commit actually changes the way functions are exported by the test/unit
    helpers module.
    
    Currently the CI does not keep hidden files. The branch we checkout for this
    dev branch includes the safekeeping of hidden files.
    
    Multipart Upload putPartLocation method was failing due to an access to a
    directory using a non-existant variable as the key. The data retrieved this way
    was actually not used afterwards, so the lie was removed altogether.
    
    Switch to node 4.2.2 and ubuntu
    
    The checkAuth module was exporting an object as default. As such, the importing
    code cannot access a property of the default export by using the bracket
    syntax. Hence, we use the default export and access the right member of the
    object to find the checkAuth function.
    
     - Switch to ubuntu for the base ironman docker image:
       - Update configure scripts to use ubuntu's apt-get instead of yum, and use
       the proper distrib-specific package names.
     - Fix java test suite:
       - maven needs a compiler version in pom.xml

--- tests/functional/400-cyberduck-cli/configure.bash
+++ tests/functional/400-cyberduck-cli/configure.bash
@@ -1,22 +1,16 @@
 #!/bin/bash
-yum -y -q clean all
 #install cyberduck cli 
-echo -e "[duck-stable]\n\
-name=duck-stable\n\
-baseurl=https://repo.cyberduck.io/stable/\$basearch/\n\
-enabled=1\n\
-sslverify=false\n\
-gpgcheck=0" |  tee /etc/yum.repos.d/duck-stable.repo > /dev/null
-yum -y -q install duck
+echo "deb https://s3.amazonaws.com/repo.deb.cyberduck.io stable main" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FE7097963FEFBE72
+apt-get update
+apt-get install -y -q duck
+
 mkdir -p /home/ironman/.duck/profiles
 sed -i "s/IP/${IP}/g" S3\ \(HTTP\).cyberduckprofile
 cp S3\ \(HTTP\).cyberduckprofile  /home/ironman/.duck/profiles
+
 #install aws cli to create the bucket in which cyber duck will work
-yum -y -q install python-pip openssl openssl-devel libffi libffi-devel wget
-pip install pyopenssl ndg-httpsclient pyasn1
 pip install awscli
 
 #shunit is not yet in the standard repo
-wget http://mirrors.karan.org/epel7/Packages/shunit2/20131231015457/2.1.6-3.el6.x86_64/shunit2-2.1.6-3.el7.noarch.rpm
-yum -y -q localinstall  ./shunit2-2.1.6-3.el7.noarch.rpm 
-
+apt-get install -y -q shunit2
--- tests/functional/500-boto/configure.bash
+++ tests/functional/500-boto/configure.bash
@@ -1,5 +1,2 @@
 #!/bin/bash
-yum -y -q clean all
-#install python
-yum -y -q install python python-pip
 pip install pytest
