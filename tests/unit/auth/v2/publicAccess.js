c1346164044784fd2bde47395fdd2b1d52a6f2f4
Rahul Padigela <rahul.padigela@scality.com>
2015-12-07 18:46:59 -0800

    REFACTOR: Modularize V2 Authentication
    - Refactors original authentication implementation by extracting
    authentication from routes into it's own modules.
    - Auth is now delegated to API level, making the module extensible for
    future implementations of auth, like V4.
    - Refactor includes small changes to comply with the latest guidelines
    and code aesthetics in general.

---
+++ tests/unit/auth/v2/publicAccess.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import auth from '../../../../lib/auth/auth';
+
+describe('Public Access', () => {
+    it('should grant access to a user that provides absolutely' +
+        'no authentication information and should assign that user the ' +
+        'All Users Group accessKey', (done) => {
+        const request = {
+            method: 'GET',
+            lowerCaseHeaders: { host: 's3.amazonaws.com'},
+            url: '/bucket',
+            query: {},
+        };
+        auth(request, (err, accessKey) => {
+            expect(err).to.be.null;
+            expect(accessKey).to
+                .equal('http://acs.amazonaws.com/groups/global/AllUsers');
+            done();
+        });
+    });
+
+    it('should not grant access to a request that contains ' +
+    'an authorization header without proper credentials', (done) => {
+        const request = {
+            method: 'GET',
+            lowerCaseHeaders: {
+                host: 's3.amazonaws.com',
+                authorization: 'noAuth'},
+            url: '/bucket',
+            query: {},
+        };
+        auth(request, (err) => {
+            expect(err).to.equal('MissingSecurityHeader');
+            done();
+        });
+    });
+});
