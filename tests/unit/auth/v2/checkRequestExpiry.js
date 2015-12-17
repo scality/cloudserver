b72e466653f31fca9517fc3bf5c7aabf0cc86188
Rahul Padigela <rahul.padigela@scality.com>
2015-12-17 11:32:33 -0800

    fix(tests): reactivate authentication unit tests
    Authentication tests were removed from mocha following #110, they are now
    picked up again.
    
    Fixes #135.

--- tests/unit/auth/v2/checkRequestExpiry.js
+++ tests/unit/auth/v2/checkRequestExpiry.js
@@ -1,12 +1,12 @@
 import { expect } from 'chai';
-import checkTimestamp from '../../../../lib/auth/v2/checkTimestamp';
+import checkRequestExpiry from '../../../../lib/auth/v2/checkRequestExpiry';
 
 describe('checkTimestamp for timecheck in header auth', () => {
     it('should return true if the date in the header is ' +
        'more than 15 minutes old', () => {
         let timeStamp = 'Mon Sep 21 2015 17:12:58 GMT-0700 (PDT)';
         timeStamp = Date.parse(timeStamp);
-        const result = checkTimestamp(timeStamp);
+        const result = checkRequestExpiry(timeStamp);
         expect(result).to.be.true;
     });
 
@@ -15,14 +15,14 @@ describe('checkTimestamp for timecheck in header auth', () => {
         // Note: This test will have to be updated in 2095
         let timeStamp = 'Mon Sep 25 2095 17:12:58 GMT-0700 (PDT)';
         timeStamp = Date.parse(timeStamp);
-        const result = checkTimestamp(timeStamp);
+        const result = checkRequestExpiry(timeStamp);
         expect(result).to.be.true;
     });
 
     it('should return false if the date in the header is ' +
        'within 15 minutes of current time', () => {
         const timeStamp = new Date();
-        const result = checkTimestamp(timeStamp);
+        const result = checkRequestExpiry(timeStamp);
         expect(result).to.be.false;
     });
 });
