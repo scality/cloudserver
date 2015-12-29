6e31d50e9ffd5efe105a704078f5e0437728ce22
Lauren Spiegel <lhspiegel@gmail.com>
2015-12-28 17:59:35 -0800

    HF: Fix getServiceIntegration due to rebase issue
    The branch that was merged in for getServiceIntegration
    did not have the final changes.  This commit fixes that.
    
    This uses the ListObject functionnality, as users are actually
    `buckets` in our metadata backend.

--- tests/unit/api/listParts.js
+++ tests/unit/api/listParts.js
@@ -35,16 +35,16 @@ const partTwoKey = `4db92ccc-d89d-49d3-9fa6-e9c2c1eb31b0` +
     `${splitter}2${splitter}2015-11-30T22:41:40.207Z${splitter}f3a9fb2071d35` +
     `03b703938a74eb99846${splitter}6000000${splitter}` +
     `ff22f316b16956ff5118c93abce7d62d`;
-const partThreeKey = `4db92ccc-d89d-49d3-9fa6-e9c2c1eb31b0...` +
-    `!*!3${splitter}2015-11-30T22:41:52.102Z` +
+const partThreeKey = `4db92ccc-d89d-49d3-9fa6-e9c2c1eb31b0${splitter}` +
+    `3${splitter}2015-11-30T22:41:52.102Z` +
     `${splitter}f3a9fb2071d3503b703938a` +
     `74eb99846${splitter}6000000${splitter}dea282f70edb6fc5f9433cd6f525d4a6`;
-const partFourKey = `4db92ccc-d89d-49d3-9fa6-e9c2c1eb31b0${splitter}4...` +
-    `!*!2015-11-30T22:42:03.493Z${splitter}f3a9fb2071d3503b703938a74eb99846` +
-    `${splitter}6000000${splitter}afe24bc40153982e1f7f28066f7af6a4`;
-const partFiveKey = `4db92ccc-d89d-49d3-9fa6-e9c2c1eb31b0${splitter}5...` +
-    `!*!2015-11-30T22:42:22.876Z${splitter}555e4cd2f9eff38109d7a3ab13995a32` +
-    `${splitter}18${splitter}85bc16f5769687070fb13cfe66b5e41f`;
+const partFourKey = `4db92ccc-d89d-49d3-9fa6-e9c2c1eb31b0${splitter}4` +
+    `${splitter}2015-11-30T22:42:03.493Z${splitter}f3a9fb2071d3503b703938a74e` +
+    `b99846${splitter}6000000${splitter}afe24bc40153982e1f7f28066f7af6a4`;
+const partFiveKey = `4db92ccc-d89d-49d3-9fa6-e9c2c1eb31b0${splitter}5` +
+    `${splitter}2015-11-30T22:42:22.876Z${splitter}555e4cd2f9eff38109d` +
+    `7a3ab13995a32${splitter}18${splitter}85bc16f5769687070fb13cfe66b5e41f`;
 
 describe('List Parts API', () => {
     beforeEach(done => {
