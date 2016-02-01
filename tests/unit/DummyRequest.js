d4e135e712951c0a922cb1ee0c27322ae40fb424
Michael Zapata <michael.zapata@scality.com>
2016-02-01 17:37:56 +0100

    feat(putObject): stream the data
    Buffer arrays are replaced by the full-fledged use of NodeJS streaming
    capabilities
    
    Add a DummyRequest class to tests, forcing the use of our new internal
    API in tests.

---
+++ tests/unit/DummyRequest.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+export default class DummyRequest extends http.IncomingMessage {
+    constructor(obj, msg) {
+        super();
+        Object.keys(obj).forEach(x => this[x] = obj[x]);
+        this.push(msg);
+        this.push(null);
+    }
+}
