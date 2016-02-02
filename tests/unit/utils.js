e9a0a123e6833f1df1a1177292bd9618cbe3bd0a
Adrien Vergé <adrien.verge@scality.com>
2016-02-02 15:48:45 +0100

    Domains: Rewrite and test bucket extraction from request
    This is a preparation for the next change: being able to have custom
    domain names (i.e. not `s3.amazonaws.com`).
    
    This commit simplifies the function that extracts the bucket name from
    the host domain. It also adds many tests to make sure the function does
    (and will, when changed) behave as we want.

--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -2,6 +2,78 @@ import assert from 'assert';
 
 import utils from '../../lib/utils';
 
+
+describe('utils.getBucketNameFromHost', () => {
+    it('should extract valid buckets for one endpoint', () => {
+        [
+            'b', 'mybucket',
+            'buck-et', '-buck-et', 'buck-et-',
+            'buck_et', '_buck_et', 'buck_et_',
+            'buck.et', 'bu.ck.et', 'bu.ck-et',
+        ].forEach(bucket => {
+            const headers = {
+                host: `${bucket}.s3.eu-west-1.amazonaws.com`,
+            };
+            const result = utils.getBucketNameFromHost({ headers });
+            assert.strictEqual(result, bucket);
+        });
+    });
+
+    it('should also accept website endpoints', () => {
+        [
+            'in-french.bucket.is-seau.s3-website-eu-west-1.amazonaws.com',
+            'in-french.bucket.is-seau.s3-website-us-east-1.amazonaws.com',
+            'in-french.bucket.is-seau.s3-website-ap-southeast-2.amazonaws.com',
+            'in-french.bucket.is-seau.s3-website-eu-central-1.amazonaws.com',
+            'in-french.bucket.is-seau.s3-website-ap-northeast-1.amazonaws.com',
+        ].forEach(host => {
+            const headers = { host };
+            const result = utils.getBucketNameFromHost({ headers });
+            assert.strictEqual(result, 'in-french.bucket.is-seau');
+        });
+    });
+
+    it('should return undefined when non dns-style', () => {
+        [
+            's3.amazonaws.com',
+            's3.eu.central-1.amazonaws.com',
+            's3.eu-west-1.amazonaws.com',
+            's3-external-1.amazonaws.com',
+            's3.us-east-1.amazonaws.com',
+            's3-us-gov-west-1.amazonaws.com',
+            's3-fips-us-gov-west-1.amazonaws.com',
+        ].forEach(host => {
+            const headers = { host };
+            const result = utils.getBucketNameFromHost({ headers });
+            assert.strictEqual(result, undefined);
+        });
+    });
+
+    it('should return undefined when IP addresses', () => {
+        [
+            '127.0.0.1',
+            '8.8.8.8',
+        ].forEach(host => {
+            const headers = { host };
+            const result = utils.getBucketNameFromHost({ headers });
+            assert.strictEqual(result, undefined);
+        });
+    });
+
+    it('should throw when bad request', () => {
+        [
+            {},
+            {host: ''},
+            {host: 'not/a/valid/endpoint'},
+            {host: 'this.domain.is.not.in.config'},
+        ].forEach(headers => {
+            assert.throws(() => {
+                utils.getBucketNameFromHost({ headers });
+            });
+        });
+    });
+});
+
 describe('utils.isValidBucketName', () => {
     it('should return false if bucketname is fewer than ' +
         '3 characters long', () => {
