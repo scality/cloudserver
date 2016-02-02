252cae35efa91a17d9152a793332b243cf03b6e2
Adrien Vergé <adrien.verge@scality.com>
2016-02-02 15:13:00 +0100

    Unit tests: Move utils tests to tests/unit/utils.js

--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -1,11 +1,48 @@
 import assert from 'assert';
 
-import utils from '../../../lib/utils';
+import utils from '../../lib/utils';
+
+describe('utils.isValidBucketName', () => {
+    it('should return false if bucketname is fewer than ' +
+        '3 characters long', () => {
+        const result = utils.isValidBucketName('no');
+        assert.strictEqual(result, false);
+    });
+
+    it('should return false if bucketname is greater than ' +
+        '63 characters long', () => {
+        const longString = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' +
+            'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+        const result = utils.isValidBucketName(longString);
+        assert.strictEqual(result, false);
+    });
+
+    it('should return false if bucketname contains ' +
+        'capital letters', () => {
+        const result = utils.isValidBucketName('noSHOUTING');
+        assert.strictEqual(result, false);
+    });
+
+    it('should return false if bucketname is an IP address', () => {
+        const result = utils.isValidBucketName('172.16.254.1');
+        assert.strictEqual(result, false);
+    });
+
+    it('should return false if bucketname is not DNS compatible', () => {
+        const result = utils.isValidBucketName('*notvalid*');
+        assert.strictEqual(result, false);
+    });
+
+    it('should return true if bucketname does not break rules', () => {
+        const result = utils.isValidBucketName('okay');
+        assert.strictEqual(result, true);
+    });
+});
 
 const bucketName = 'bucketname';
 const objName = 'testObject';
 
-describe('normalizeRequest utility', () => {
+describe('utils.normalizeRequest', () => {
     it('should parse bucket name from path', () => {
         const request = {
             url: `/${bucketName}`,
