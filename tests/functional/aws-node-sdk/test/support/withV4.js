d19f39bb15e0930338e727e46390d6e59fa5f3e0
sergey <korchevskiysergey@hotmail.com>
2016-03-03 17:07:18 +0100

    TEST: run tests one more with v4 signature

---
+++ tests/functional/aws-node-sdk/test/support/withV4.js
@@ -0,0 +1,23 @@
+const versions = ['default', 'v4'];
+
+function withV4(testFn) {
+    versions.forEach(version => {
+        let config;
+
+        if (version === 'v4') {
+            config = {
+                signatureVersion: version,
+            };
+        } else {
+            config = {};
+        }
+
+        describe(`With ${version} signature`, (cfg => {
+            return function tcWrap() {
+                testFn.call(this, cfg);
+            };
+        })(config));
+    });
+}
+
+export default withV4;
