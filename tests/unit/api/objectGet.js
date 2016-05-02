b27e0cf2bdce8966ab4cf366796bf5a9f98e9023
Lauren Spiegel <lhspiegel@gmail.com>
2016-05-02 15:10:57 -0700

    Modify objectMD model to prepare for getRange.
    Now our data retrieval information contains size and start.
    Also, we have the name of the data store to prepare for
    multiple potential backends.

--- tests/unit/api/objectGet.js
+++ tests/unit/api/objectGet.js
@@ -1,11 +1,16 @@
 import assert from 'assert';
+import async from 'async';
 import crypto from 'crypto';
+import { parseString } from 'xml2js';
 
 import bucketPut from '../../../lib/api/bucketPut';
 import { cleanup, DummyRequestLogger, makeAuthInfo } from '../helpers';
+import completeMultipartUpload from '../../../lib/api/completeMultipartUpload';
+import DummyRequest from '../DummyRequest';
+import initiateMultipartUpload from '../../../lib/api/initiateMultipartUpload';
 import objectPut from '../../../lib/api/objectPut';
 import objectGet from '../../../lib/api/objectGet';
-import DummyRequest from '../DummyRequest';
+import objectPutPart from '../../../lib/api/objectPutPart';
 
 const log = new DummyRequestLogger();
 const canonicalID = 'accessKey1';
@@ -62,57 +67,134 @@ describe('objectGet API', () => {
         });
     });
 
-    it('should get the object data', done => {
+    it('should get the object data retrieval info', done => {
         bucketPut(authInfo, testPutBucketRequest, log, () => {
             objectPut(authInfo, testPutObjectRequest, log, (err, result) => {
                 assert.strictEqual(result, correctMD5);
-                objectGet(authInfo, testGetRequest, log, (err, readable) => {
-                    const chunks = [];
-                    readable.on('data', chunk => {
-                        chunks.push(chunk);
-                    });
-                    readable.on('end', () => {
-                        const buf = Buffer.concat(chunks);
-                        assert.deepStrictEqual(buf, postBody);
-                        done();
-                    });
+                objectGet(authInfo, testGetRequest, log, (err, dataGetInfo) => {
+                    assert.deepStrictEqual(dataGetInfo,
+                        [{
+                            key: 1,
+                            dataStoreName: 'mem',
+                        }]);
+                    done();
                 });
             });
         });
     });
 
-    it('should get the object data for large objects', done => {
-        const testBigData = crypto.randomBytes(1000000);
-        const correctBigMD5 =
-            crypto.createHash('md5').update(testBigData).digest('hex');
-
-        const testPutBigObjectRequest = new DummyRequest({
-            bucketName,
-            namespace,
-            objectKey: objectName,
-            headers: { 'x-amz-meta-test': 'some metadata' },
-            url: `/${bucketName}/${objectName}`,
-        }, testBigData);
-        bucketPut(authInfo, testPutBucketRequest, log, () => {
-            objectPut(authInfo, testPutBigObjectRequest, log, (err, result) => {
-                assert.strictEqual(result, correctBigMD5);
-                objectGet(authInfo, testGetRequest, log, (err, readable) => {
-                    const md5Hash = crypto.createHash('md5');
-                    const chunks = [];
-                    readable.on('data', chunk => {
-                        const cBuffer = new Buffer(chunk, 'binary');
-                        chunks.push(cBuffer);
-                        md5Hash.update(cBuffer);
+    it('should get the object data retrieval info for an object put by MPU',
+        done => {
+            const partBody = new Buffer('I am a part\n');
+            const initiateRequest = {
+                bucketName,
+                namespace,
+                objectKey: objectName,
+                headers: { host: `${bucketName}.s3.amazonaws.com` },
+                url: `/${objectName}?uploads`,
+            };
+            async.waterfall([
+                function waterfall1(next) {
+                    bucketPut(authInfo, testPutBucketRequest, log, next);
+                },
+                function waterfall2(next) {
+                    initiateMultipartUpload(
+                        authInfo, initiateRequest, log, next);
+                },
+                function waterfall3(result, next) {
+                    parseString(result, next);
+                },
+                function waterfall4(json, next) {
+                    const testUploadId =
+                    json.InitiateMultipartUploadResult.UploadId[0];
+                    const md5Hash = crypto.createHash('md5').update(partBody);
+                    const calculatedHash = md5Hash.digest('hex');
+                    const partRequest = new DummyRequest({
+                        bucketName,
+                        namespace,
+                        objectKey: objectName,
+                        headers: {
+                            'host': `${bucketName}.s3.amazonaws.com`,
+                            // Part (other than last part) must be at least 5MB
+                            'content-length': '5242880',
+                        },
+                        url: `/${objectName}?partNumber=1&uploadId` +
+                            `=${testUploadId}`,
+                        query: {
+                            partNumber: '1',
+                            uploadId: testUploadId,
+                        },
+                        calculatedHash,
+                    }, partBody);
+                    objectPutPart(authInfo, partRequest, log, () => {
+                        next(null, testUploadId, calculatedHash);
                     });
-                    readable.on('end', () => {
-                        const resultmd5Hash = md5Hash.digest('hex');
-                        assert.strictEqual(resultmd5Hash, correctBigMD5);
-                        done();
+                },
+                function waterfall5(testUploadId, calculatedHash, next) {
+                    const part2Request = new DummyRequest({
+                        bucketName,
+                        namespace,
+                        objectKey: objectName,
+                        headers: {
+                            'host': `${bucketName}.s3.amazonaws.com`,
+                            'content-length': '12',
+                        },
+                        url: `/${objectName}?partNumber=2&uploadId=` +
+                            `${testUploadId}`,
+                        query: {
+                            partNumber: '2',
+                            uploadId: testUploadId,
+                        },
+                        calculatedHash,
+                    }, partBody);
+                    objectPutPart(authInfo, part2Request, log, () => {
+                        next(null, testUploadId, calculatedHash);
                     });
-                });
-            });
+                },
+                function waterfall6(testUploadId, calculatedHash, next) {
+                    const completeBody = '<CompleteMultipartUpload>' +
+                        '<Part>' +
+                        '<PartNumber>1</PartNumber>' +
+                        `<ETag>"${calculatedHash}"</ETag>` +
+                        '</Part>' +
+                        '<Part>' +
+                        '<PartNumber>2</PartNumber>' +
+                        `<ETag>"${calculatedHash}"</ETag>` +
+                        '</Part>' +
+                        '</CompleteMultipartUpload>';
+                    const completeRequest = {
+                        bucketName,
+                        namespace,
+                        objectKey: objectName,
+                        parsedHost: 's3.amazonaws.com',
+                        url: `/${objectName}?uploadId=${testUploadId}`,
+                        headers: { host: `${bucketName}.s3.amazonaws.com` },
+                        query: { uploadId: testUploadId },
+                        post: completeBody,
+                    };
+                    completeMultipartUpload(authInfo, completeRequest,
+                        log, next);
+                },
+            ],
+    function waterfallFinal() {
+        objectGet(authInfo, testGetRequest, log, (err, dataGetInfo) => {
+            assert.deepStrictEqual(dataGetInfo,
+                [{
+                    key: 1,
+                    dataStoreName: 'mem',
+                    size: 5242880,
+                    start: 0,
+                },
+                {
+                    key: 2,
+                    dataStoreName: 'mem',
+                    size: 12,
+                    start: 5242880,
+                }]);
+            done();
         });
     });
+        });
 
     it('should get a 0 bytes object', done => {
         const postBody = '';
