8a0be2834b727c23d5ead9b45574e2b0f1329a20
Guillaume Gimenez <ploki@blackmilk.fr>
2016-07-25 15:18:19 -0700

    FT: PUT in datastore now requires a size parameter
    sproxydclient.put code needs the size of the data stream in order to
    create an HTTP PUT request.
    Letting sproxydclient to look at the 'content-length' attribute of
    something that should not be more than a stream defeats encapsulation.
    
    BTW, It's also beneficial to the in_memory backend.

--- tests/unit/DummyRequest.js
+++ tests/unit/DummyRequest.js
@@ -6,6 +6,17 @@ export default class DummyRequest extends http.IncomingMessage {
         Object.keys(obj).forEach(x => {
             this[x] = obj[x];
         });
+        const contentLength = this.headers['content-length'];
+        if (this.parsedContentLength === undefined) {
+            if (contentLength !== undefined) {
+                this.parsedContentLength = parseInt(contentLength, 10);
+            } else if (msg !== undefined) {
+                this.parsedContentLength = msg.length;
+            } else {
+                this.parsedContentLength = 0;
+            }
+        }
+
         if (Array.isArray(msg)) {
             msg.forEach( part => {
                 this.push(part);
