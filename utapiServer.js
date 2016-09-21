e9c1a281067884996b6a8a49d3a20d963c5861d5
Rahul Padigela <rahul.padigela@scality.com>
2016-09-20 19:34:10 -0700

    cleanup: remove unneeded babel ignores
    As utapi is being compiled on post install, it doesn't require babel
    transpiling.
    
    Fix #155

--- utapiServer.js
+++ utapiServer.js
@@ -1,16 +1,4 @@
 'use strict'; // eslint-disable-line strict
 
-require('babel-core/register')({
-    ignore: filename =>
-        !(filename.startsWith(`${__dirname}/constants.js`) ||
-        filename.startsWith(`${__dirname}/lib/`) ||
-        filename.startsWith(`${__dirname}/tests/`) ||
-        filename.startsWith(`${__dirname}/utapi/`) ||
-        filename.startsWith(`${__dirname}/node_modules/utapi/lib/`) ||
-        filename.startsWith(`${__dirname}/node_modules/utapi/utils/`) ||
-        filename.startsWith(`${__dirname}/node_modules/utapi/router/`) ||
-        filename.startsWith(`${__dirname}/node_modules/utapi/responses/`) ||
-        filename.startsWith(`${__dirname}/node_modules/utapi/validators/`) ||
-        filename.startsWith(`${__dirname}/node_modules/utapi/handlers/`)),
-});
+require('babel-core/register')();
 require('./lib/utapi/utapi.js').default();
