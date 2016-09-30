skibum55/S3

FT: Implement copy part

lib/api/api.js
@@ -21,6 +21,7 @@ import objectHead from './objectHead';
 import objectPut from './objectPut';
 import objectPutACL from './objectPutACL';
 import objectPutPart from './objectPutPart';
+import objectPutCopyPart from './objectPutCopyPart';
 import serviceGet from './serviceGet';
 import vault from '../auth/vault';
 
@@ -29,7 +30,7 @@ auth.setAuthHandler(vault);
 
 const api = {
     callApiMethod(apiMethod, request, log, callback, locationConstraint) {
-        if (apiMethod === 'objectCopy') {
+        if (apiMethod === 'objectCopy' || apiMethod === 'objectPutCopyPart') {
             let source =
                 querystring.unescape(request.headers['x-amz-copy-source']);
             // If client sends the source bucket/object with a leading /,
@@ -63,7 +64,7 @@ const api = {
                         log.trace('auth error on put portion of request');
                         return callback(err);
                     }
-                    return objectCopy(authInfo, request, sourceBucket,
+                    return this[apiMethod](authInfo, request, sourceBucket,
                         sourceObject, log, callback);
                 }, 's3', putRequestContext);
             }, 's3', getRequestContext);
@@ -103,6 +104,7 @@ const api = {
     objectPut,
     objectPutACL,
     objectPutPart,
+    objectPutCopyPart,
     serviceGet,
 };
 

scality/cloudserver

lib/api/api.js
@@ -21,6 +21,7 @@ import objectHead from './objectHead';
 import objectPut from './objectPut';
 import objectPutACL from './objectPutACL';
 import objectPutPart from './objectPutPart';
+import objectPutCopyPart from './objectPutCopyPart';
 import prepareRequestContexts from
     './apiUtils/authorization/prepareRequestContexts';
 import serviceGet from './serviceGet';
@@ -32,7 +33,7 @@ const api = {
     callApiMethod(apiMethod, request, log, callback, locationConstraint) {
         let sourceBucket;
         let sourceObject;
-        if (apiMethod === 'objectCopy') {
+        if (apiMethod === 'objectCopy' || apiMethod === 'objectPutCopyPart') {
             let source =
                 querystring.unescape(request.headers['x-amz-copy-source']);
             // If client sends the source bucket/object with a leading /,
@@ -71,8 +72,9 @@ const api = {
                 return bucketPut(userInfo, request, locationConstraint,
                     log, callback);
             }
-            if (apiMethod === 'objectCopy') {
-                return objectCopy(userInfo, request, sourceBucket,
+            if (apiMethod === 'objectCopy' ||
+                apiMethod === 'objectPutCopyPart') {
+                return this[apiMethod](userInfo, request, sourceBucket,
                     sourceObject, log, callback);
             }
             if (apiMethod === 'objectPut' || apiMethod === 'objectPutPart') {
@@ -101,6 +103,7 @@ const api = {
     objectPut,
     objectPutACL,
     objectPutPart,
+    objectPutCopyPart,
     serviceGet,
 };
 