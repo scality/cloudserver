fac81fe65deb7df1f36d011696151a1c9b781d8b
Electra Chong <electra.chong@scality.com>
2016-10-10 13:35:01 -0700

    BF: Store / return AWS compatible headers
    Related to issue fix #282
    Optional headers Cache-Control, Content-Disposition, Content-Encoding, and Expires are missing in objectGet and objectHead response.
    * Stores header values if specified in objectPut or objectCopy request.
    * Returns stored values in objectGet and objectHead responses.
    * Will follow-up in separate PR to address storing header values for multipartUpload

--- tests/functional/aws-node-sdk/test/object/get.js
+++ tests/functional/aws-node-sdk/test/object/get.js
@@ -3,15 +3,35 @@ import assert from 'assert';
 import withV4 from '../support/withV4';
 import BucketUtility from '../../lib/utility/bucket-util';
 
+const bucketName = 'buckettestgetobject';
+const objectName = 'someObject';
+// Specify sample headers to check for in GET response
+const cacheControl = 'max-age=86400';
+const contentDisposition = 'attachment; filename="fname.ext";';
+const contentEncoding = 'aws-chunked,gzip';
+// AWS Node SDK requires Date object, ISO-8601 string, or
+// a UNIX timestamp for Expires header
+const expires = new Date();
 
 describe('GET object', () => {
     withV4(sigCfg => {
         let bucketUtil;
         let s3;
 
-        before(() => {
+        before(done => {
             bucketUtil = new BucketUtility('default', sigCfg);
             s3 = bucketUtil.s3;
+            // Create a bucket to put object to get later
+            s3.createBucket({ Bucket: bucketName }, done);
+        });
+
+        after(done => {
+            s3.deleteObject({ Bucket: bucketName, Key: objectName }, err => {
+                if (err) {
+                    return done(err);
+                }
+                return s3.deleteBucket({ Bucket: bucketName }, done);
+            });
         });
 
 
@@ -21,8 +41,43 @@ describe('GET object', () => {
                     assert.notEqual(err, null,
                         'Expected failure but got success');
                     assert.strictEqual(err.code, 'NoSuchBucket');
-                    done();
+                    return done();
                 });
             });
+
+        describe('Additional headers: [Cache-Control, Content-Disposition, ' +
+        'Content-Encoding, Expires]', () => {
+            before(done => {
+                const params = {
+                    Bucket: bucketName,
+                    Key: objectName,
+                    CacheControl: cacheControl,
+                    ContentDisposition: contentDisposition,
+                    ContentEncoding: contentEncoding,
+                    Expires: expires,
+                };
+                s3.putObject(params, err => done(err));
+            });
+            it('should return additional headers if specified in objectPUT ' +
+              'request', done => {
+                s3.getObject({ Bucket: bucketName, Key: objectName },
+                  (err, res) => {
+                      if (err) {
+                          return done(err);
+                      }
+                      assert.strictEqual(res.CacheControl,
+                        cacheControl);
+                      assert.strictEqual(res.ContentDisposition,
+                        contentDisposition);
+                      // Should remove V4 streaming value 'aws-chunked'
+                      // to be compatible with AWS behavior
+                      assert.strictEqual(res.ContentEncoding,
+                        'gzip');
+                      assert.strictEqual(res.Expires,
+                          expires.toGMTString());
+                      return done();
+                  });
+            });
+        });
     });
 });
