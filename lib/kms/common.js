enishi/S3

FT: Node v4.5.0 and new buffer api switch
Update to the new buffer api

lib/kms/common.js
@@ -32,7 +32,7 @@ export default class Common {
     @return {buffer} - random key
     */
     static createDataKey() {
-        return new Buffer(crypto.randomBytes(this._keySize()));
+        return Buffer.from(crypto.randomBytes(this._keySize()));
     }
 
     /**
@@ -78,7 +78,7 @@ export default class Common {
              * bumping the cryptoScheme number saved in the object
              * metadata along with the dataKey.
              */
-            const salt = new Buffer('ItsTasty');
+            const salt = Buffer.from('ItsTasty', 'binary');
             const iterations = 1;
             return crypto.pbkdf2(
                 dataKey, salt, iterations,
@@ -137,7 +137,7 @@ export default class Common {
                     /* Above, we advanced to the latest boundary not
                     greater than the offset amount. Here we advance by
                     the toSkip amount if necessary. */
-                    const dummyBuffer = new Buffer(toSkip);
+                    const dummyBuffer = Buffer.alloc(toSkip);
                     cipher.write(dummyBuffer);
                     cipher.read();
                 }

scality/cloudserver

lib/kms/common.js
@@ -32,7 +32,7 @@ export default class Common {
     @return {buffer} - random key
     */
     static createDataKey() {
-        return new Buffer(crypto.randomBytes(this._keySize()));
+        return Buffer.from(crypto.randomBytes(this._keySize()));
     }
 
     /**
@@ -78,7 +78,7 @@ export default class Common {
              * bumping the cryptoScheme number saved in the object
              * metadata along with the dataKey.
              */
-            const salt = new Buffer('ItsTasty');
+            const salt = Buffer.from('ItsTasty', 'utf8');
             const iterations = 1;
             return crypto.pbkdf2(
                 dataKey, salt, iterations,
@@ -137,7 +137,7 @@ export default class Common {
                     /* Above, we advanced to the latest boundary not
                     greater than the offset amount. Here we advance by
                     the toSkip amount if necessary. */
-                    const dummyBuffer = new Buffer(toSkip);
+                    const dummyBuffer = Buffer.alloc(toSkip);
                     cipher.write(dummyBuffer);
                     cipher.read();
                 }