232dfc073e57d00be2d5f7c823190cc8fd5e7a41
Bennett Buchanan <bennett.buchanan@scality.com>
2016-11-02 17:51:45 -0700

    COMPAT: Return expected response for `MaxUpload` and `Delimiter` params
    Currently, the value of `MaxUploads` for listMultipartUpload API is
    the number of MPU in a bucket. This is not the behavior of AWS. This
    commit changes the API's behavior so the value of `MaxUploads` can be
    specified by the user using the `MaxUploads` parameter. This commit also
    returns the expected default of 1000 if no `MaxUploads` is not defined.
    If `MaxUploads` is defined as `0`, the behavior of AWS is to return an
    empty `Uploads` array and set `IsTruncated` to `false`. This commit also
    adds this expected behavior.
    
    Currently, when using the file-backend, if a `Delimiter` parameter is
    defined and it does not result in a `CommonPrefixes` array that is
    greater than 0, `Delimiter` is set to ''. This causes the XML response
    to omit the `Delimeter` key. For compatibility with AWS, this commit
    tests an associated change in Arsenal that fixes the `Delimiter` response.
    
    A new test has been added to verify these behaviors.
    
    [squash me] Use object destructuring for variables
    
    [squash me] Clean up code and comments
    
    [squash me] Remove callbacks and return all every Promise
    
    [squash me] Handle 0 `MaxUploads` object in `getExpectedObj()`
    
    [squash me] Separate assertions from object creation
    
    [squash me] Remove redundant assertion statements

--- tests/functional/aws-node-sdk/test/object/mpu.js
+++ tests/functional/aws-node-sdk/test/object/mpu.js
@@ -6,31 +6,80 @@ import BucketUtility from '../../lib/utility/bucket-util';
 const bucket = 'object-test-mpu';
 const objectKey = 'toAbort&<>"\'';
 
-const throwErr = (str, err) => {
-    process.stdout.write(`${str}: ${err}\n`);
-    throw err;
-};
+// Get the expected object of listMPU API.
+function getExpectedObj(res, data) {
+    // If `maxUploads` is not given as a parameter, it should default to 1000.
+    const maxUploads = data.maxUploads === undefined ? 1000 : data.maxUploads;
 
-const checkValues = (res, uploadId, displayName, cb) => {
-    const prefix = res.Prefix ? res.Prefix : undefined;
-    const delimeter = res.Delimiter ? res.Delimiter : undefined;
-    assert.deepStrictEqual(res.KeyMarker, '');
-    assert.deepStrictEqual(res.UploadIdMarker, '');
-    assert.deepStrictEqual(res.Prefix, prefix);
-    assert.deepStrictEqual(res.Delimiter, delimeter);
-    assert.deepStrictEqual(res.Uploads.length, 1);
-    assert.deepStrictEqual(res.Uploads[0].UploadId, uploadId);
-    assert.deepStrictEqual(res.Uploads[0].StorageClass, 'STANDARD');
-    assert.deepStrictEqual(res.Uploads[0].Owner.DisplayName, displayName);
-    cb();
-};
+    // If `MaxUploads` is defined as 0, `IsTruncated` is set to `false` despite
+    // the fact that there may be multipart uploads in the bucket.
+    if (maxUploads === 0) {
+        return {
+            Bucket: bucket,
+            KeyMarker: '',
+            UploadIdMarker: '',
+            MaxUploads: 0,
+            IsTruncated: false,
+            Uploads: [],
+            CommonPrefixes: [],
+        };
+    }
 
-describe('aws-node-sdk test suite of listMultipartUploads', () => {
+    const { prefixVal, delimiter, uploadId, displayName, userId } = data;
+    const initiated = new Date(res.Uploads[0].Initiated.toISOString());
+    const expectedObj = {
+        Bucket: bucket,
+        KeyMarker: '',
+        UploadIdMarker: '',
+        NextKeyMarker: objectKey,
+        Prefix: prefixVal,
+        Delimiter: delimiter,
+        NextUploadIdMarker: uploadId,
+        MaxUploads: maxUploads,
+        IsTruncated: false,
+        Uploads: [{
+            UploadId: uploadId,
+            Key: objectKey,
+            Initiated: initiated,
+            StorageClass: 'STANDARD',
+            Owner:
+            {
+                DisplayName: displayName,
+                ID: userId,
+            },
+            Initiator:
+            {
+                DisplayName: displayName,
+                ID: userId,
+            },
+        }],
+        CommonPrefixes: [],
+    };
+
+    // If no `prefixVal` is given, it should not be included in the response.
+    if (!prefixVal) {
+        delete expectedObj.Prefix;
+    }
+
+    // If no `delimiter` is given, it should not be included in the response.
+    if (!delimiter) {
+        delete expectedObj.Delimiter;
+    }
+
+    return expectedObj;
+}
+
+// Compare the response object with the expected object.
+function checkValues(res, data) {
+    const expectedObj = getExpectedObj(res, data);
+    assert.deepStrictEqual(res, expectedObj);
+}
+
+describe('aws-node-sdk test suite of listMultipartUploads', () =>
     withV4(sigCfg => {
         let bucketUtil;
         let s3;
-        let uploadId;
-        let displayName;
+        const data = {};
 
         beforeEach(() => {
             bucketUtil = new BucketUtility('default', sigCfg);
@@ -39,45 +88,56 @@ describe('aws-node-sdk test suite of listMultipartUploads', () => {
             return s3.createBucketAsync({ Bucket: bucket })
             .then(() => bucketUtil.getOwner())
             .then(res => {
-                // In this case, the owner of the bucket will also be the MPU
-                // upload owner. We need this value for testing comparison.
-                displayName = res.DisplayName;
+                // The owner of the bucket will also be the MPU upload owner.
+                data.displayName = res.DisplayName;
+                data.userId = res.ID;
             })
             .then(() => s3.createMultipartUploadAsync({
                 Bucket: bucket,
                 Key: objectKey,
             }))
             .then(res => {
-                uploadId = res.UploadId;
-            })
-            .catch(err => throwErr('Error in beforeEach', err));
+                data.uploadId = res.UploadId;
+            });
         });
 
         afterEach(() =>
             s3.abortMultipartUploadAsync({
                 Bucket: bucket,
                 Key: objectKey,
-                UploadId: uploadId,
+                UploadId: data.uploadId,
             })
             .then(() => bucketUtil.empty(bucket))
             .then(() => bucketUtil.deleteOne(bucket))
-            .catch(err => throwErr('Error in afterEach', err))
         );
 
-        it('should list ongoing multipart uploads', done => {
+        it('should list ongoing multipart uploads', () =>
             s3.listMultipartUploadsAsync({ Bucket: bucket })
-            .then(res => checkValues(res, uploadId, displayName, done))
-            .catch(done);
-        });
+            .then(res => checkValues(res, data))
+        );
 
-        it('should list ongoing multipart uploads with params', done => {
-            s3.listMultipartUploadsAsync({
+        it('should list ongoing multipart uploads with params', () => {
+            data.prefixVal = 'to';
+            data.delimiter = 'test-delimiter';
+            data.maxUploads = 1;
+
+            return s3.listMultipartUploadsAsync({
                 Bucket: bucket,
                 Prefix: 'to',
+                Delimiter: 'test-delimiter',
                 MaxUploads: 1,
             })
-            .then(res => checkValues(res, uploadId, displayName, done))
-            .catch(done);
+            .then(res => checkValues(res, data));
         });
-    });
-});
+
+        it('should list 0 multipart uploads when MaxUploads is 0', () => {
+            data.maxUploads = 0;
+
+            return s3.listMultipartUploadsAsync({
+                Bucket: bucket,
+                MaxUploads: 0,
+            })
+            .then(res => checkValues(res, data));
+        });
+    })
+);
