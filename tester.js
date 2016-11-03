f219af95ffa45079e64b1401582ed0bcf85e6be1
David Pineau <david.pineau@scality.com>
2016-11-03 15:49:20 +0100

    tests: Rework functional testing for improved test visibility
    * Harmonize how the functional tests are launched:
      - Use local package.json to tell how to launch tests
      - Rework tester.js to use the package.json instead of the obsolete master.json files
      - Remove obsolete master.json files
    
    * Provide convenience npm scripts for S3 to launch individual functional test suites
    
    * Attempt to reorganize circle.yml in order to improve the visibility of each
    test suite. Hopefully, thanks to this, the CircleCI UI will help us find more
    quickly whichever test failed. This has of course the downside of (probably)
    increasing testing time, but the visibility is a clear upside.
    
    * Add server output redirection for individual functional tests, so as to clear
    out the tests' outputs
    
    * Add a wait script to wait for S3 to be ready before launching tests, failing
    when the timeout is reached to ensure we do not wait forever.

--- tester.js
+++ tester.js
@@ -9,32 +9,21 @@ function log(message) {
     return process.stdout.write(`${message}\n`);
 }
 
-function runTest(dir, fileName) {
-    const testFile = path.join(dir, fileName);
-
-    const test = cp.spawnSync('mocha', ['-t', '40000', testFile], {
-        stdio: 'inherit',
-        cwd: dir,
-    });
-
-    return !test.status;
-}
-
 function testing(dir) {
-    let masterConfig;
-
     log(`testing ${dir}`);
 
     try {
-        masterConfig = require(path.join(rootPath, dir, 'master.json'));
-    } catch (err) {
-        return log('master.json file not found, skipping this directory');
+        fs.statSync(path.join(rootPath, dir, 'package.json'));
+    } catch (e) {
+        return log('package.json file not found, skipping this directory');
     }
 
-    return masterConfig.tests.files.reduce((prev, file) => {
-        const cwd = path.join(rootPath, dir);
-        return prev && runTest(cwd, file);
-    }, true);
+    const test = cp.spawnSync('npm', ['test'], {
+        stdio: 'inherit',
+        cwd: path.join(rootPath, dir),
+    });
+
+    return !test.status;
 }
 
 function main() {
