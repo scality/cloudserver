a7e0be056c74409892ed85b4420d940a3afb1c63
Dora Korpar <dora.korpar@scality.com>
2016-11-10 17:00:50 -0800

    Add backend healthcheck functionality

--- tests/unit/healthchecks/healthcheckResponse.js
+++ tests/unit/healthchecks/healthcheckResponse.js
@@ -1,6 +1,6 @@
 import assert from 'assert';
 
-import { isHealthy } from '../../../lib/routes';
+import { isHealthy } from '../../../lib/utilities/healthcheckHandler';
 
 describe('isHealthy function for setHealthCheckResponse', () => {
     it('should return true', () => {
