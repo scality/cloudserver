72a7848f432b8257e15678327f59adc22a24c108
Austin Moran <austin.moran@surveysampling.com>
2016-11-17 23:00:47 -0500

    fix instanceof check for compatibility with node 4.5.0

--- tests/unit/predicate/simpleHandler.js
+++ tests/unit/predicate/simpleHandler.js
@@ -6,8 +6,7 @@ const stream = require('stream');
 module.exports = function (params, callback) {
     assert.strictEqual('object', typeof params.Records[0].s3);
     const s3 = params.Records[0].s3;
-    assert(s3.object.body instanceof stream.Readable);
-    assert(s3.object.body instanceof stream.Writable);
+    assert(s3.object.body instanceof stream.Duplex);
     s3.object.metadata['simple-handler'] = 'passed';
     s3.object.contentType = 'text/plain';
     s3.object.key = 'food.txt';
