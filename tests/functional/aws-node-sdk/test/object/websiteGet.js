c59d9b51bf088eaf5069882a72eb3d2cca9513b8
Nicolas Humbert <nicolas.humbert@live.fr>
2016-12-10 21:22:17 -0800

    Add websiteGet tests against AWS

--- tests/functional/aws-node-sdk/test/object/websiteGet.js
+++ tests/functional/aws-node-sdk/test/object/websiteGet.js
@@ -2,30 +2,47 @@ import assert from 'assert';
 import fs from 'fs';
 import http from 'http';
 import path from 'path';
+const AWS = require('aws-sdk');
 
-import { S3 } from 'aws-sdk';
+// import { S3 } from 'aws-sdk';
 import Browser from 'zombie';
 
 import conf from '../../../../../lib/Config';
-import getConfig from '../support/config';
+// import getConfig from '../support/config';
 import { WebsiteConfigTester } from '../../lib/utility/website-util';
 
+// const config = getConfig('default', { signatureVersion: 'v4' });
+// const s3 = new S3(config);
+
+// against real AWS
+const s3 = new AWS.S3({
+    accessKeyId: process.env.A,
+    secretAccessKey: process.env.S,
+    endpoint: 's3.amazonaws.com',
+    sslEnabled: false,
+    s3ForcePathStyle: true,
+    signatureVersion: 'v4',
+});
+
 const transport = conf.https ? 'https' : 'http';
-const bucket = 'bucketwebsitetester';
+const bucket = 'mybucketwebsite';
 const hostname = `${bucket}.s3-website-us-east-1.amazonaws.com`;
 
-const endpoint = `${transport}://${hostname}:8000`;
+// const endpoint = `${transport}://${hostname}:8000`;
+const endpoint = `${transport}://${hostname}:80`;
 
 // TODO: Add this endpoint in Integration for CI
 
 describe('User visits bucket website endpoint', () => {
     const browser = new Browser();
 
-    it('should return 405 when user requests method other than get or head',
+    // Have not manage to reproduce that using postman for ex
+    it.skip('should return 405 when user requests method other than get or head',
         done => {
             const options = {
                 hostname,
-                port: 8000,
+                // port: 8000
+                port: 80,
                 method: 'POST',
             };
             const req = http.request(options, res => {
@@ -46,23 +63,21 @@ describe('User visits bucket website endpoint', () => {
 
     it('should return 404 when no such bucket', done => {
         browser.visit(endpoint, () => {
-            browser.assert.status(404);
-            browser.assert.text('title', '404 Not Found');
-            browser.assert.text('h1', '404 Not Found');
-            browser.assert.element('#code');
-            browser.assert.text('#code', 'Code: NoSuchBucket');
-            browser.assert.text('#message',
-                'Message: The specified bucket does not exist.');
+            WebsiteConfigTester.checkHTML(browser, '404-no-such-bucket',
+              bucket);
             done();
         });
     });
 
     describe('with existing bucket', () => {
-        let s3;
         beforeEach(done => {
-            const config = getConfig('default', { signatureVersion: 'v4' });
-            s3 = new S3(config);
-            s3.createBucket({ Bucket: bucket }, err => done(err));
+            // s3.createBucket({ Bucket: bucket }, err => done(err));
+            s3.createBucket({ Bucket: bucket }, err => {
+                if (err) {
+                    return done(err);
+                }
+                return setTimeout(() => done(), 5000);
+            });
         });
 
         afterEach(done => {
@@ -71,14 +86,8 @@ describe('User visits bucket website endpoint', () => {
 
         it('should return 404 when no website configuration', done => {
             browser.visit(endpoint, () => {
-                browser.assert.status(404);
-                browser.assert.text('title', '404 Not Found');
-                browser.assert.text('h1', '404 Not Found');
-                browser.assert.text('#code',
-                    'Code: NoSuchWebsiteConfiguration');
-                browser.assert.text('#message',
-                    'Message: The specified bucket does not ' +
-                    'have a website configuration');
+                WebsiteConfigTester.checkHTML(browser,
+                  '404-no-such-website-configuration', bucket);
                 done();
             });
         });
@@ -109,11 +118,181 @@ describe('User visits bucket website endpoint', () => {
 
             it('should serve indexDocument if no key requested', done => {
                 browser.visit(endpoint, () => {
-                    browser.assert.status(200);
-                    browser.assert.text('title',
-                        'Best testing website ever');
-                    browser.assert.text('h1', 'Welcome to my ' +
-                        'extraordinary bucket website testing page');
+                    WebsiteConfigTester.checkHTML(browser, 'index-user');
+                    done();
+                });
+            });
+        });
+        describe('with path in request without key', () => {
+            beforeEach(done => {
+                const webConfig = new WebsiteConfigTester('index.html');
+                s3.putBucketWebsite({ Bucket: bucket,
+                    WebsiteConfiguration: webConfig }, err => {
+                    assert.strictEqual(err,
+                        null, `Found unexpected err ${err}`);
+                    s3.putObject({ Bucket: bucket,
+                        Key: 'www/index.html',
+                        ACL: 'public-read',
+                        Body: fs.readFileSync(path.join(__dirname,
+                            '/websiteFiles/index.html')),
+                        ContentType: 'text/html' }, done);
+                });
+            });
+
+            afterEach(done => {
+                s3.deleteObject({ Bucket: bucket, Key: 'www/index.html' },
+                done);
+            });
+
+            it('should serve indexDocument if path request without key',
+            done => {
+                browser.visit(`${endpoint}/www`, () => {
+                    WebsiteConfigTester.checkHTML(browser, 'index-user');
+                    done();
+                });
+            });
+        });
+
+        describe('with private key', () => {
+            beforeEach(done => {
+                const webConfig = new WebsiteConfigTester('index.html');
+                s3.putBucketWebsite({ Bucket: bucket,
+                    WebsiteConfiguration: webConfig }, err => {
+                    assert.strictEqual(err,
+                        null, `Found unexpected err ${err}`);
+                    s3.putObject({ Bucket: bucket,
+                        Key: 'index.html',
+                        ACL: 'private',
+                        Body: fs.readFileSync(path.join(__dirname,
+                            '/websiteFiles/index.html')),
+                        ContentType: 'text/html' }, done);
+                });
+            });
+
+            afterEach(done => {
+                s3.deleteObject({ Bucket: bucket, Key: 'index.html' }, done);
+            });
+
+            it('should return 403 if key is private', done => {
+                browser.visit(endpoint, () => {
+                    WebsiteConfigTester.checkHTML(browser, '403-access-denied');
+                    done();
+                });
+            });
+        });
+
+        describe('with no key', () => {
+            beforeEach(done => {
+                const webConfig = new WebsiteConfigTester('index.html');
+                s3.putBucketWebsite({ Bucket: bucket,
+                    WebsiteConfiguration: webConfig }, done);
+            });
+
+            it('should return 403 if no key', done => {
+                browser.visit(endpoint, () => {
+                    WebsiteConfigTester.checkHTML(browser, '403-access-denied');
+                    done();
+                });
+            });
+        });
+
+        describe('redirect all requests to http://www.scality.com', () => {
+            beforeEach(done => {
+                const redirectAllTo = {
+                    HostName: 'www.scality.com',
+                };
+                const webConfig = new WebsiteConfigTester(null, null,
+                  redirectAllTo);
+                s3.putBucketWebsite({ Bucket: bucket,
+                    WebsiteConfiguration: webConfig }, done);
+            });
+
+            it('should redirect to http://www.scality.com', done => {
+                browser.visit(endpoint, () => {
+                    WebsiteConfigTester.checkHTML(browser, 'scality');
+                    done();
+                });
+            });
+
+            it('should redirect to http://www.scality.com/about-us', done => {
+                browser.visit(`${endpoint}/about-us`, () => {
+                    WebsiteConfigTester.checkHTML(browser, 'scality-about-us');
+                    done();
+                });
+            });
+        });
+
+        // If redirectAllTo protocol return an http (not https) request,
+        // this test will fail.
+        describe('redirect all requests to https://scality.com', () => {
+            beforeEach(done => {
+                const redirectAllTo = {
+                    HostName: 'scality.com',
+                    Protocol: 'https',
+                };
+                const webConfig = new WebsiteConfigTester(null, null,
+                  redirectAllTo);
+                s3.putBucketWebsite({ Bucket: bucket,
+                    WebsiteConfiguration: webConfig }, done);
+            });
+
+            it('should redirect to http://scality.com', done => {
+                browser.visit(endpoint, () => {
+                    WebsiteConfigTester.checkHTML(browser, 'scality');
+                    done();
+                });
+            });
+
+            it('should redirect to https://scality.com/about-us', done => {
+                browser.visit(`${endpoint}/about-us`, () => {
+                    WebsiteConfigTester.checkHTML(browser, 'scality-about-us');
+                    done();
+                });
+            });
+        });
+
+        describe('with user\'s error', () => {
+            beforeEach(done => {
+                const webConfig = new WebsiteConfigTester('index.html',
+                'error.html');
+                s3.putBucketWebsite({ Bucket: bucket,
+                    WebsiteConfiguration: webConfig }, err => {
+                    assert.strictEqual(err,
+                        null, `Found unexpected err ${err}`);
+                    s3.putObject({ Bucket: bucket,
+                        Key: 'error.html',
+                        ACL: 'public-read',
+                        Body: fs.readFileSync(path.join(__dirname,
+                            '/websiteFiles/error.html')),
+                        ContentType: 'text/html' }, done);
+                });
+            });
+
+            afterEach(done => {
+                s3.deleteObject({ Bucket: bucket, Key: 'error.html' }, done);
+            });
+
+            it('should serve user\'s error file if an error occurred', done => {
+                browser.visit(endpoint, () => {
+                    WebsiteConfigTester.checkHTML(browser, 'error-user');
+                    done();
+                });
+            });
+        });
+
+        describe('error with unfound user\'s error', () => {
+            beforeEach(done => {
+                const webConfig = new WebsiteConfigTester('index.html',
+                'error.html');
+                s3.putBucketWebsite({ Bucket: bucket,
+                    WebsiteConfiguration: webConfig }, done);
+            });
+
+            it('should serve s3 error file if unfound user\'s error file ' +
+            'and an error occurred', done => {
+                browser.visit(endpoint, () => {
+                    WebsiteConfigTester.checkHTML(browser,
+                      '403-retrieve-error-document');
                     done();
                 });
             });
