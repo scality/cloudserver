cloudberrylab/S3

FT(kinetic)- implement kinetic backend
 - The backend is just there for using the Kinetic class

lib/data/wrapper.js
@@ -1,11 +1,12 @@
 import async from 'async';
 import { errors } from 'arsenal';
+import assert from 'assert';
 import Sproxy from 'sproxydclient';
 import file from './file/backend';
+import kinetic from './kinetic/backend';
 import inMemory from './in_memory/backend';
 import config from '../Config';
 import MD5Sum from '../utilities/MD5Sum';
-import assert from 'assert';
 import kms from '../kms/wrapper';
 
 let client;
@@ -17,6 +18,9 @@ if (config.backends.data === 'mem') {
 } else if (config.backends.data === 'file') {
     client = file;
     implName = 'file';
+} else if (config.backends.data === 'kinetic') {
+    client = kinetic;
+    implName = 'kinetic';
 } else if (config.backends.data === 'scality') {
     client = new Sproxy({
         bootstrap: config.sproxyd.bootstrap,

scality/cloudserver

lib/data/wrapper.js
@@ -1,11 +1,12 @@
 import async from 'async';
 import { errors } from 'arsenal';
+import assert from 'assert';
 import Sproxy from 'sproxydclient';
 import file from './file/backend';
+import kinetic from './kinetic/backend';
 import inMemory from './in_memory/backend';
 import config from '../Config';
 import MD5Sum from '../utilities/MD5Sum';
-import assert from 'assert';
 import kms from '../kms/wrapper';
 
 let client;
@@ -17,6 +18,9 @@ if (config.backends.data === 'mem') {
 } else if (config.backends.data === 'file') {
     client = file;
     implName = 'dataFile';
+} else if (config.backends.data === 'kinetic') {
+    client = kinetic;
+    implName = 'dataKinetic';
 } else if (config.backends.data === 'scality') {
     client = new Sproxy({
         bootstrap: config.sproxyd.bootstrap,