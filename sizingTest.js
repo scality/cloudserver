59ea45e2cdb27e8c413593afc71458493307a278
Bennett Buchanan <bennett.buchanan@scality.com>
2017-02-06 11:00:45 -0800

    [squash] Cleanup

--- sizingTest.js
+++ sizingTest.js
@@ -13,8 +13,7 @@ const s3 = new AWS.S3({
     signatureCache: false,
 });
 
-const second = 1;
-const minute = second * 60;
+const minute = 60;
 const fifteenMinutes = minute * 15;
 const hour = minute * 60;
 const day = hour * 24;
@@ -25,6 +24,7 @@ const interval = second * 1000;
 const timeOut = minute;
 let currentSecond = 0;
 
+// Perform a series of AWS operations.
 function awsOperations(callback) {
     const Bucket = `bucket-${currentSecond}`;
     return async.waterfall([
@@ -35,27 +35,28 @@ function awsOperations(callback) {
                 Key: `object-${currentSecond}-${n}`,
                 Body: Buffer.alloc(1),
             }, err => cb(err)),
-        err => next(err)),
+            err => next(err)),
         next => async.times(20, (n, cb) =>
             s3.getObject({
                 Bucket,
                 Key: `object-${currentSecond}-${n}`,
             }, err => cb(err)),
-        err => next(err)),
+            err => next(err)),
     ], err => callback(err));
 }
 
+// Write output to stdout.
 function log(err, isLast) {
     if (err) {
         return process.stdout.write(`Error: ${err}`);
     }
     // Log the number of operations that have occurred.
-    const count = currentSecond % 15 === 0 ? `${currentSecond}` : '';
+    const count = currentSecond % logReqInterval === 0 ? `${currentSecond}` : '';
     const end = isLast ? '\nComplete\n' : '';
     return process.stdout.write(`.${count}${end}`);
 }
 
-// Perform all the actions once per `interval`, until the `timeout` has passed.
+// Perform all the actions once per `interval`, until `timeout` has occurred.
 const countDown = setInterval(() => {
     if (++currentSecond === timeOut) {
         clearInterval(countDown);
