67745772de2e4536e277009d490648034ac3791c
Vinh Tao <vinh.tao@scality.com>
2017-03-13 14:08:17 +0100

    clean: some optimization and dev dependencies

--- tests/functional/raw-node/test/routes/unsupportedQuries.js
+++ tests/functional/raw-node/test/routes/unsupportedQuries.js
@@ -8,7 +8,7 @@ const objectKey = 'key';
 const itSkipIfAWS = process.env.AWS_ON_AIR ? it.skip : it;
 
 describe('unsupported query requests:', () => {
-    constants.unsupportedQueries.forEach(query => {
+    Object.keys(constants.unsupportedQueries).forEach(query => {
         itSkipIfAWS(`should respond with NotImplemented for ?${query} request`,
         done => {
             const queryObj = {};
@@ -24,7 +24,7 @@ describe('unsupported query requests:', () => {
 
     itSkipIfAWS('should accept blacklisted query key as a query value ' +
     'to a query key that is not on the blacklist', done => {
-        const queryObj = { test: constants.unsupportedQueries[0] };
+        const queryObj = { test: Object.keys(constants.unsupportedQueries)[0] };
         makeS3Request({ method: 'GET', queryObj, bucket, objectKey }, err => {
             assert.strictEqual(err.code, 'NoSuchBucket');
             assert.strictEqual(err.statusCode, 404);
