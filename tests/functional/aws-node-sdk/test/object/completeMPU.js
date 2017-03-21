f22be3850d8f391626a6fe71cd2f51db6437e3de
Vinh Tao <vinh.tao@scality.com>
2017-03-21 12:35:50 +0100

    test: update completeMPU

--- tests/functional/aws-node-sdk/test/object/completeMPU.js
+++ tests/functional/aws-node-sdk/test/object/completeMPU.js
@@ -1,85 +1,141 @@
 import assert from 'assert';
-
 import withV4 from '../support/withV4';
 import BucketUtility from '../../lib/utility/bucket-util';
+import {
+    constants,
+    removeAllVersions,
+} from '../../lib/utility/versioning-util.js';
 
 const date = Date.now();
 const bucket = `completempu${date}`;
 const key = 'key';
-const bodyFirstPart = Buffer.allocUnsafe(10).fill(0);
-const bodySecondPart = Buffer.allocUnsafe(5).fill(0);
 
 function checkNoError(err) {
     assert.equal(err, null,
         `Expected success, got error ${JSON.stringify(err)}`);
 }
 
+const testing = process.env.VERSIONING === 'no' ? describe.skip : describe;
+
 describe('Complete MPU', () => {
     withV4(sigCfg => {
-        let bucketUtil;
-        let s3;
-        let uploadId;
-        let firstEtag;
+        const bucketUtil = new BucketUtility('default', sigCfg);
+        const s3 = bucketUtil.s3;
 
-        beforeEach(() => {
-            bucketUtil = new BucketUtility('default', sigCfg);
-            s3 = bucketUtil.s3;
-            return s3.createBucketAsync({ Bucket: bucket })
-            .then(() => s3.createMultipartUploadAsync({
-                Bucket: bucket, Key: key }))
-            .then(res => {
-                uploadId = res.UploadId;
-                return s3.uploadPartAsync({ Bucket: bucket, Key: key,
-                  PartNumber: 1, UploadId: uploadId, Body: bodyFirstPart });
-            })
-            .then(res => {
-                firstEtag = res.ETag;
-                return firstEtag;
-            })
-            .then(() => s3.uploadPartAsync({ Bucket: bucket, Key: key,
-                PartNumber: 2, UploadId: uploadId, Body: bodySecondPart }))
-            .catch(err => {
-                process.stdout.write(`Error in beforeEach: ${err}\n`);
-                throw err;
-            });
-        });
-
-        afterEach(() => {
-            process.stdout.write('Emptying bucket');
-            return bucketUtil.empty(bucket)
-            .then(() => {
-                process.stdout.write('Deleting bucket');
-                return bucketUtil.deleteOne(bucket);
-            })
-            .catch(err => {
-                process.stdout.write('Error in afterEach');
-                throw err;
-            });
-        });
-
-        it('should complete an MPU with fewer parts than were ' +
-            'originally put', done => {
+        function _completeMpuAndCheckVid(uploadId, eTag, expectedVid, cb) {
             s3.completeMultipartUpload({
                 Bucket: bucket,
                 Key: key,
                 MultipartUpload: {
-                    Parts: [{ ETag: firstEtag, PartNumber: 1 }],
+                    Parts: [{ ETag: eTag, PartNumber: 1 }],
                 },
                 UploadId: uploadId },
             (err, data) => {
                 checkNoError(err);
-                // to show that the mpu completed with just 1 part
-                assert.strictEqual(data.ETag.slice(-3), '-1"');
+                const versionId = data.VersionId;
+                if (expectedVid) {
+                    assert.notEqual(versionId, undefined);
+                } else {
+                    assert.strictEqual(versionId, expectedVid);
+                }
                 return s3.getObject({
                     Bucket: bucket,
                     Key: key,
                 },
                 (err, data) => {
                     checkNoError(err);
-                    // to show that data in completed key is just first part
-                    assert.strictEqual(data.ContentLength, '10');
-                    done();
+                    if (versionId) {
+                        assert.strictEqual(data.VersionId, versionId);
+                    }
+                    cb();
                 });
+            });
+        }
+
+        function _initiateMpuAndPutOnePart() {
+            const result = {};
+            return s3.createMultipartUploadAsync({
+                Bucket: bucket, Key: key })
+            .then(data => {
+                result.uploadId = data.UploadId;
+                return s3.uploadPartAsync({ Bucket: bucket, Key: key,
+                  PartNumber: 1, UploadId: data.UploadId, Body: 'foo' });
+            })
+            .then(data => {
+                result.eTag = data.ETag;
+                return result;
+            })
+            .catch(err => {
+                process.stdout.write(`Error in beforeEach: ${err}\n`);
+                throw err;
+            });
+        }
+
+        beforeEach(done => {
+            s3.createBucket({ Bucket: bucket }, done);
+        });
+
+        afterEach(done => {
+            removeAllVersions({ Bucket: bucket }, err => {
+                if (err) {
+                    return done(err);
+                }
+                return s3.deleteBucket({ Bucket: bucket }, done);
+            });
+        });
+
+        describe('on bucket without versioning configuration', () => {
+            let uploadId;
+            let eTag;
+
+            beforeEach(() => _initiateMpuAndPutOnePart()
+                .then(result => {
+                    uploadId = result.uploadId;
+                    eTag = result.eTag;
+                })
+            );
+
+            it('should complete an MPU with fewer parts than were ' +
+                'originally put without returning a version id', done => {
+                _completeMpuAndCheckVid(uploadId, eTag, undefined, done);
+            });
+        });
+
+        testing('on bucket with enabled versioning', () => {
+            let uploadId;
+            let eTag;
+
+            beforeEach(() => s3.putBucketVersioningAsync({ Bucket: bucket,
+                    VersioningConfiguration: constants.versioningEnabled })
+                .then(() => _initiateMpuAndPutOnePart())
+                .then(result => {
+                    uploadId = result.uploadId;
+                    eTag = result.eTag;
+                })
+            );
+
+            it('should complete an MPU with fewer parts than were ' +
+                'originally put and return a version id', done => {
+                _completeMpuAndCheckVid(uploadId, eTag, true, done);
+            });
+        });
+
+        testing('on bucket with suspended versioning', () => {
+            let uploadId;
+            let eTag;
+
+            beforeEach(() => s3.putBucketVersioningAsync({ Bucket: bucket,
+                    VersioningConfiguration: constants.versioningSuspended })
+                .then(() => _initiateMpuAndPutOnePart())
+                .then(result => {
+                    uploadId = result.uploadId;
+                    eTag = result.eTag;
+                })
+            );
+
+            it('should complete an MPU with fewer parts than were ' +
+                'originally put and should not return a version id', done => {
+                _completeMpuAndCheckVid(uploadId, eTag, undefined, done);
             });
         });
     });
