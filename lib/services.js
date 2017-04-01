jcre/S3

ft: versioning tools

lib/services.js
@@ -13,10 +13,14 @@ import { isBucketAuthorized, isObjAuthorized } from
 import metadata from './metadata/wrapper';
 import { logger } from './utilities/logger';
 import removeAWSChunked from './api/apiUtils/object/removeAWSChunked';
+import { VID_INF } from './metadata/VersionID';
 
 const usersBucket = constants.usersBucket;
 const oldUsersBucket = constants.oldUsersBucket;
 
+// TODO update Arsenal to provide VID_INF
+// const VID_INF = versioning.VersioningConstants.VID_INF;
+// const VID_INF = `00000000000000${Date.now()}000000`.slice(-20);
 
 export default {
     getService(authInfo, request, log, splitter, cb, overrideUserbucket) {
@@ -99,8 +103,12 @@ export default {
                 return cb(null, bucket, null);
             });
         }
-        return metadata.getBucketAndObjectMD(bucketName, objectKey, {}, log,
-        (err, data) => {
+        let versionId = params.versionId;
+        if (versionId === 'null') {
+            versionId = undefined;
+        }
+        return metadata.getBucketAndObjectMD(bucketName, objectKey,
+        { versionId }, log, (err, data) => {
             if (err) {
                 log.debug('metadata get failed', { error: err });
                 return cb(err);
@@ -129,6 +137,18 @@ export default {
                 // async waterfall to work
                 log.trace('Bucket found', { bucketName });
                 return cb(null, bucket, null);
+            }
+            if (params.versionId === 'null') {
+                if (obj.nullVersionId && !obj.isNull) {
+                    log.debug('null version exists, get the null version');
+                    params.versionId = obj.nullVersionId; // eslint-disable-line
+                    return this.metadataValidateAuthorization(params, cb);
+                }
+                if (obj.versionId && !obj.isNull) {
+                    log.debug('null version does not exists');
+                    return cb(null, bucket, null);
+                }
+                // otherwise the master version is the null version
             }
             // TODO: Add bucket policy and IAM checks
             if (!isObjAuthorized(bucket, obj, requestType, canonicalID)) {
@@ -171,7 +191,7 @@ export default {
         const { objectKey, authInfo, size, contentMD5, metaHeaders,
             contentType, cacheControl, contentDisposition, contentEncoding,
             expires, multipart, headers, overrideMetadata, log,
-            lastModifiedDate } = params;
+            lastModifiedDate, versioning, versionId } = params;
         log.trace('storing object in metadata');
         assert.strictEqual(typeof bucketName, 'string');
         const omVal = {};
@@ -196,7 +216,7 @@ export default {
         omVal['last-modified'] = lastModifiedDate || new Date().toJSON();
         omVal['content-md5'] = contentMD5;
 
-        omVal['x-amz-server-version-id'] = '';
+        // omVal['x-amz-server-version-id'] = '';
         // TODO: Handle this as a utility function for all object puts
         // similar to normalizing request but after checkAuth so
         // string to sign is not impacted.  This is GH Issue#89.
@@ -215,7 +235,7 @@ export default {
         }
 
         // simple/no version. will expand once object versioning is introduced
-        omVal['x-amz-version-id'] = 'null';
+        // omVal['x-amz-version-id'] = 'null';
         omVal.acl = {
             Canned: 'private',
             FULL_CONTROL: [],
@@ -223,6 +243,20 @@ export default {
             READ: [],
             READ_ACP: [],
         };
+
+        const options = {};
+        if (versioning) {
+            options.versioning = versioning;
+        }
+        if (versionId || versionId === '') {
+            options.versionId = versionId;
+        }
+        omVal.isNull = params.isNull;
+        omVal.nullVersionId = params.nullVersionId;
+        omVal.isDeleteMarker = params.isDeleteMarker;
+        if (versionId && versionId !== 'null') {
+            omVal.versionId = versionId;
+        }
 
         // Store user provided metadata.  TODO: limit size.
         // For multipart upload this also serves to transfer
@@ -245,41 +279,40 @@ export default {
         log.trace('object metadata', { omVal });
         // If this is not the completion of a multipart upload
         // parse the headers to get the ACL's if any
-        if (!multipart) {
-            const parseAclParams = {
-                headers,
-                resourceType: 'object',
-                acl: omVal.acl,
-                log,
-            };
-            log.trace('parsing acl from headers');
-            acl.parseAclFromHeaders(parseAclParams, (err, parsedACL) => {
-                if (err) {
-                    log.debug('error parsing acl', { error: err });
-                    return cb(err);
+        async.waterfall([
+            callback => {
+                if (multipart) {
+                    return callback();
                 }
-                omVal.acl = parsedACL;
-                metadata.putObjectMD(bucketName, objectKey, omVal, {}, log,
-                err => {
+                const parseAclParams = {
+                    headers,
+                    resourceType: 'object',
+                    acl: omVal.acl,
+                    log,
+                };
+                log.trace('parsing acl from headers');
+                acl.parseAclFromHeaders(parseAclParams, (err, parsedACL) => {
                     if (err) {
-                        log.error('error from metadata', { error: err });
-                        return cb(err);
+                        log.warn('error parsing acl', { error: err });
+                        return callback(err);
                     }
-                    log.trace('object successfully stored in metadata');
-                    return cb(err, contentMD5);
+                    omVal.acl = parsedACL;
+                    return callback();
                 });
-                return undefined;
-            });
-        } else {
-            metadata.putObjectMD(bucketName, objectKey, omVal, {}, log, err => {
-                if (err) {
-                    log.error('error from metadata', { error: err });
-                    return cb(err);
-                }
-                log.trace('object successfully stored in metadata');
-                return cb(err, contentMD5);
-            });
-        }
+                return null;
+            },
+            callback => metadata.putObjectMD(bucketName, objectKey, omVal,
+                    options, log, callback),
+        ], (err, data) => {
+            if (err) {
+                log.error('error from metadata', { error: err });
+                return cb(err);
+            }
+            log.trace('object successfully stored in metadata');
+            // for versioning only, other features need to process their data
+            const versionId = data ? JSON.parse(data).versionId : undefined;
+            return cb(err, { contentMD5, versionId });
+        });
     },
 
     /**
@@ -287,37 +320,31 @@ export default {
      * @param {string} bucketName - bucket in which objectMD is stored
      * @param {object} objectMD - object's metadata
      * @param {string} objectKey - object key name
+     * @param {object} options - other instructions, such as { versionId } to
+     *                           delete a specific version of the object
      * @param {Log} log - logger instance
      * @param {function} cb - callback from async.waterfall in objectGet
      * @return {undefined}
      */
-    deleteObject(bucketName, objectMD, objectKey, log, cb) {
+    deleteObject(bucketName, objectMD, objectKey, options, log, cb) {
         log.trace('deleting object from bucket');
         assert.strictEqual(typeof bucketName, 'string');
         assert.strictEqual(typeof objectMD, 'object');
-        if (objectMD['x-amz-version-id'] === 'null') {
-            log.trace('object identified as non-versioned');
-            // non-versioned buckets
-            log.trace('deleteObject: deleting non-versioned object');
-            return metadata.deleteObjectMD(bucketName, objectKey, {}, log,
-                err => {
-                    if (err) {
-                        return cb(err);
-                    }
-                    cb();
-                    log.trace('deleteObject: metadata delete OK');
-                    const deleteLog = logger.newRequestLogger();
-                    if (objectMD.location === null) {
-                        return undefined;
-                    } else if (!Array.isArray(objectMD.location)) {
-                        return data.delete(objectMD.location, deleteLog);
-                    }
-                    return data.batchDelete(objectMD.location, deleteLog);
-                });
-        }
-        // versioning
-        log.debug('deleteObject: versioning not fully implemented');
-        return metadata.deleteObjectMD(bucketName, objectKey, {}, log, cb);
+        return metadata.deleteObjectMD(bucketName, objectKey, options, log,
+            (err, res) => {
+                if (err) {
+                    return cb(err, res);
+                }
+                cb(null, res); // this is smart
+                log.trace('deleteObject: metadata delete OK');
+                const deleteLog = logger.newRequestLogger();
+                if (objectMD.location === null) {
+                    return undefined;
+                } else if (!Array.isArray(objectMD.location)) {
+                    return data.delete(objectMD.location, deleteLog);
+                }
+                return data.batchDelete(objectMD.location, deleteLog);
+            });
     },
 
     /**
@@ -734,5 +761,131 @@ export default {
         async.eachLimit(keysToDelete, 5, (key, callback) => {
             metadata.deleteObjectMD(mpuBucketName, key, {}, log, callback);
         }, err => cb(err));
+    },
+
+    versioningPreprocessing(bucketName, bucketMD, objectKey, objMD,
+            reqVersionId, log, callback) {
+        const options = {};
+        // bucket is not versioning enabled
+        if (!bucketMD.getVersioningConfiguration()) {
+            options.deleteData = true;
+            return callback(null, options);
+        }
+        // bucket is versioning enabled
+        const mstVersionId = objMD ? objMD.versionId : undefined;
+        const mstIsNull = objMD ? objMD.isNull : false;
+        const vstat = bucketMD.getVersioningConfiguration().Status;
+        if (!reqVersionId) {
+            // non-version-specific versioning operation
+            if (mstVersionId === undefined || mstIsNull) {
+                // object does not exist or is not versioned (before versioning)
+                if (vstat === 'Suspended') {
+                    // versioning is suspended, overwrite the existing version
+                    options.versionId = '';
+                    options.isNull = true;
+                    options.deleteData = true;
+                    return callback(null, options);
+                }
+                // versioning is enabled, create a new version
+                options.versioning = true;
+                if (objMD) {
+                    // store master version in a new key
+                    const versionId = mstIsNull ? mstVersionId : VID_INF;
+                    objMD.versionId = versionId; // eslint-disable-line
+                    objMD.isNull = true; // eslint-disable-line
+                    options.nullVersionId = versionId;
+                    return metadata.putObjectMD(bucketName, objectKey, objMD,
+                            { versionId }, log, err => callback(err, options));
+                }
+                return callback(null, options);
+            }
+            // master is versioned and is not a null version
+            const nullVersionId = objMD.nullVersionId;
+            if (vstat === 'Suspended') {
+                // versioning is suspended, overwrite the existing version
+                options.versionId = '';
+                options.isNull = true;
+                if (nullVersionId === undefined) {
+                    return callback(null, options);
+                }
+                return metadata.deleteObjectMD(bucketName, objectKey,
+                    { versionId: nullVersionId }, log,
+                    err => callback(err, options));
+            }
+            // versioning is enabled, put the new version
+            options.versioning = true;
+            options.nullVersionId = nullVersionId;
+            return callback(null, options);
+        } else if (!mstVersionId) {
+            // version-specific versioning operation, master is not versioned
+            if (vstat === 'Suspended' || reqVersionId === 'null') {
+                // object does not exist or is not versioned (before versioning)
+                options.versionId = '';
+                options.isNull = true;
+                options.deleteData = true;
+                return callback(null, options);
+            }
+            // TODO check AWS behaviour
+            return callback(errors.BadRequest);
+        } else if (mstIsNull) {
+            // master is versioned and is a null version
+            if (reqVersionId === 'null') {
+                // overwrite the existing version, make new version null
+                options.versionId = '';
+                options.isNull = true;
+                options.deleteData = true;
+                return callback(null, options);
+            }
+            // TODO check AWS behaviour
+            options.versionId = reqVersionId;
+            options.deleteData = true;
+            return callback(null, options);
+        }
+        // master is versioned and is not a null version
+        options.versionId = reqVersionId;
+        options.deleteData = true;
+        return callback(null, options);
+    },
+
+    preprocessingVersioningDelete(bucketName, bucketMD, objectName, objectMD,
+            reqVersionId, log, callback) {
+        const options = {};
+        // bucket is not versioning enabled
+        if (!bucketMD.getVersioningConfiguration()) {
+            options.deleteData = true;
+            return callback(null, options);
+        }
+        // bucket is versioning enabled
+        if (reqVersionId && reqVersionId !== 'null') {
+            // deleting a specific version
+            options.deleteData = true;
+            options.versionId = reqVersionId;
+            return callback(null, options);
+        }
+        if (reqVersionId) {
+            // deleting the 'null' version if it exists
+            if (objectMD.versionId === undefined) {
+                // object is not versioned, deleting it
+                options.deleteData = true;
+                return callback(null, options);
+            }
+            if (objectMD.isNull) {
+                // master is the null version
+                options.deleteData = true;
+                options.versionId = objectMD.versionId;
+                return callback(null, options);
+            }
+            if (objectMD.nullVersionId) {
+                // null version exists, deleting it
+                options.deleteData = true;
+                options.versionId = objectMD.nullVersionId;
+                return callback(null, options);
+            }
+            // null version does not exist, no deletion
+            // TODO check AWS behaviour for no deletion (seems having no error)
+            return callback(errors.NoSuchKey);
+        }
+        // not deleting any specific version, making a delete marker instead
+        return callback(null, options);
     },
 };

scality/cloudserver

lib/services.js
@@ -17,7 +17,6 @@ import removeAWSChunked from './api/apiUtils/object/removeAWSChunked';
 const usersBucket = constants.usersBucket;
 const oldUsersBucket = constants.oldUsersBucket;
 
-
 export default {
     getService(authInfo, request, log, splitter, cb, overrideUserbucket) {
         const canonicalID = authInfo.getCanonicalID();
@@ -63,7 +62,11 @@ export default {
             });
     },
 
-    /**
+     /**
+     * TODO: Remove this function in a clean-up PR as it is obsolete (does
+     * not handle versioned objects). Current references to this function
+     * can be switched to its equivalent, metadataValidateBucketAndObj(),
+     * in apiUtils/authorization/validation.
      * Checks whether resource exists and the user is authorized
      * @param {object} params - custom built object containing
      * resource name, type, authInfo etc.
@@ -171,7 +174,7 @@ export default {
         const { objectKey, authInfo, size, contentMD5, metaHeaders,
             contentType, cacheControl, contentDisposition, contentEncoding,
             expires, multipart, headers, overrideMetadata, log,
-            lastModifiedDate } = params;
+            lastModifiedDate, versioning, versionId } = params;
         log.trace('storing object in metadata');
         assert.strictEqual(typeof bucketName, 'string');
         const omVal = {};
@@ -214,8 +217,6 @@ export default {
             }
         }
 
-        // simple/no version. will expand once object versioning is introduced
-        omVal['x-amz-version-id'] = 'null';
         omVal.acl = {
             Canned: 'private',
             FULL_CONTROL: [],
@@ -223,6 +224,24 @@ export default {
             READ: [],
             READ_ACP: [],
         };
+
+        // options to send to metadata to create or overwrite versions
+        // when putting the object MD
+        const options = {};
+        if (versioning) {
+            options.versioning = versioning;
+        }
+        if (versionId || versionId === '') {
+            options.versionId = versionId;
+        }
+        // information to store about the version and the null version id
+        // in the object metadata
+        omVal.isNull = params.isNull;
+        omVal.nullVersionId = params.nullVersionId;
+        omVal.isDeleteMarker = params.isDeleteMarker;
+        if (versionId && versionId !== 'null') {
+            omVal.versionId = versionId;
+        }
 
         // Store user provided metadata.  TODO: limit size.
         // For multipart upload this also serves to transfer
@@ -245,41 +264,49 @@ export default {
         log.trace('object metadata', { omVal });
         // If this is not the completion of a multipart upload
         // parse the headers to get the ACL's if any
-        if (!multipart) {
-            const parseAclParams = {
-                headers,
-                resourceType: 'object',
-                acl: omVal.acl,
-                log,
-            };
-            log.trace('parsing acl from headers');
-            acl.parseAclFromHeaders(parseAclParams, (err, parsedACL) => {
-                if (err) {
-                    log.debug('error parsing acl', { error: err });
-                    return cb(err);
+        async.waterfall([
+            callback => {
+                if (multipart) {
+                    return callback();
                 }
-                omVal.acl = parsedACL;
-                metadata.putObjectMD(bucketName, objectKey, omVal, {}, log,
-                err => {
+                const parseAclParams = {
+                    headers,
+                    resourceType: 'object',
+                    acl: omVal.acl,
+                    log,
+                };
+                log.trace('parsing acl from headers');
+                acl.parseAclFromHeaders(parseAclParams, (err, parsedACL) => {
                     if (err) {
-                        log.error('error from metadata', { error: err });
-                        return cb(err);
+                        log.debug('error parsing acl', { error: err });
+                        return callback(err);
                     }
-                    log.trace('object successfully stored in metadata');
-                    return cb(err, contentMD5);
+                    omVal.acl = parsedACL;
+                    return callback();
                 });
-                return undefined;
-            });
-        } else {
-            metadata.putObjectMD(bucketName, objectKey, omVal, {}, log, err => {
-                if (err) {
-                    log.error('error from metadata', { error: err });
-                    return cb(err);
+                return null;
+            },
+            callback => metadata.putObjectMD(bucketName, objectKey, omVal,
+                    options, log, callback),
+        ], (err, data) => {
+            if (err) {
+                log.error('error from metadata', { error: err });
+                return cb(err);
+            }
+            log.trace('object successfully stored in metadata');
+            // if versioning is enabled, data will be returned from metadata
+            // as JSON containing a versionId which some APIs will need sent
+            // back to them
+            let versionId = undefined;
+            if (data) {
+                if (params.isNull && params.isDeleteMarker) {
+                    versionId = 'null';
+                } else if (!params.isNull) {
+                    versionId = JSON.parse(data).versionId;
                 }
-                log.trace('object successfully stored in metadata');
-                return cb(err, contentMD5);
-            });
-        }
+            }
+            return cb(err, { contentMD5, versionId });
+        });
     },
 
     /**
@@ -287,37 +314,31 @@ export default {
      * @param {string} bucketName - bucket in which objectMD is stored
      * @param {object} objectMD - object's metadata
      * @param {string} objectKey - object key name
+     * @param {object} options - other instructions, such as { versionId } to
+     *                           delete a specific version of the object
      * @param {Log} log - logger instance
      * @param {function} cb - callback from async.waterfall in objectGet
      * @return {undefined}
      */
-    deleteObject(bucketName, objectMD, objectKey, log, cb) {
+    deleteObject(bucketName, objectMD, objectKey, options, log, cb) {
         log.trace('deleting object from bucket');
         assert.strictEqual(typeof bucketName, 'string');
         assert.strictEqual(typeof objectMD, 'object');
-        if (objectMD['x-amz-version-id'] === 'null') {
-            log.trace('object identified as non-versioned');
-            // non-versioned buckets
-            log.trace('deleteObject: deleting non-versioned object');
-            return metadata.deleteObjectMD(bucketName, objectKey, {}, log,
-                err => {
-                    if (err) {
-                        return cb(err);
-                    }
-                    cb();
-                    log.trace('deleteObject: metadata delete OK');
-                    const deleteLog = logger.newRequestLogger();
-                    if (objectMD.location === null) {
-                        return undefined;
-                    } else if (!Array.isArray(objectMD.location)) {
-                        return data.delete(objectMD.location, deleteLog);
-                    }
-                    return data.batchDelete(objectMD.location, deleteLog);
-                });
-        }
-        // versioning
-        log.debug('deleteObject: versioning not fully implemented');
-        return metadata.deleteObjectMD(bucketName, objectKey, {}, log, cb);
+        return metadata.deleteObjectMD(bucketName, objectKey, options, log,
+            (err, res) => {
+                if (err) {
+                    return cb(err, res);
+                }
+                cb(null, res); // this is smart
+                log.trace('deleteObject: metadata delete OK');
+                const deleteLog = logger.newRequestLogger();
+                if (objectMD.location === null) {
+                    return undefined;
+                } else if (!Array.isArray(objectMD.location)) {
+                    return data.delete(objectMD.location, deleteLog);
+                }
+                return data.batchDelete(objectMD.location, deleteLog);
+            });
     },
 
     /**