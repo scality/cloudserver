dfb97ba4c657ec9be83b77c79cbbe3812b2da96b
Electra Chong <electra.chong@scality.com>
2017-05-31 17:07:39 -0700

    ft: limit user metadata to 2 KB
    AWS limits user-provided metadata to 2 KB. Now that we are implementing AWS and other backends, we should be consistent in the amount of user metadata we allow during puts.

--- tests/functional/aws-node-sdk/test/object/initiateMPU.js
+++ tests/functional/aws-node-sdk/test/object/initiateMPU.js
@@ -1,9 +1,13 @@
 const assert = require('assert');
+const async = require('async');
 
 const withV4 = require('../support/withV4');
 const BucketUtility = require('../../lib/utility/bucket-util');
+const genMaxSizeMetaHeaders
+    = require('../../lib/utility/genMaxSizeMetaHeaders');
 
 const bucket = `initiatempubucket${Date.now()}`;
+const key = 'key';
 
 describe('Initiate MPU', () => {
     withV4(sigCfg => {
@@ -25,10 +29,38 @@ describe('Initiate MPU', () => {
         it('should return InvalidRedirectLocation if initiate MPU ' +
         'with x-amz-website-redirect-location header that does not start ' +
         'with \'http://\', \'https://\' or \'/\'', done => {
-            const params = { Bucket: bucket, Key: 'key',
+            const params = { Bucket: bucket, Key: key,
                 WebsiteRedirectLocation: 'google.com' };
             s3.createMultipartUpload(params, err => {
                 assert.strictEqual(err.code, 'InvalidRedirectLocation');
+                assert.strictEqual(err.statusCode, 400);
+                done();
+            });
+        });
+
+        it('should return error if initiating MPU w/ > 2KB user-defined md',
+        done => {
+            const metadata = genMaxSizeMetaHeaders();
+            const params = { Bucket: bucket, Key: key, Metadata: metadata };
+            async.waterfall([
+                next => s3.createMultipartUpload(params, (err, data) => {
+                    assert.strictEqual(err, null, `Unexpected err: ${err}`);
+                    next(null, data.UploadId);
+                }),
+                (uploadId, next) => s3.abortMultipartUpload({
+                    Bucket: bucket,
+                    Key: key,
+                    UploadId: uploadId,
+                }, err => {
+                    assert.strictEqual(err, null, `Unexpected err: ${err}`);
+                    // add one more byte to push over limit for next call
+                    metadata.header0 = `${metadata.header0}${'0'}`;
+                    next();
+                }),
+                next => s3.createMultipartUpload(params, next),
+            ], err => {
+                assert(err, 'Expected err but did not find one');
+                assert.strictEqual(err.code, 'MetadataTooLarge');
                 assert.strictEqual(err.statusCode, 400);
                 done();
             });
--- tests/functional/aws-node-sdk/test/object/objectCopy.js
+++ tests/functional/aws-node-sdk/test/object/objectCopy.js
@@ -4,6 +4,8 @@ const withV4 = require('../support/withV4');
 const BucketUtility = require('../../lib/utility/bucket-util');
 
 const { taggingTests } = require('../../lib/utility/tagging');
+const genMaxSizeMetaHeaders
+    = require('../../lib/utility/genMaxSizeMetaHeaders');
 
 const sourceBucketName = 'supersourcebucket8102016';
 const sourceObjName = 'supersourceobject';
@@ -344,6 +346,52 @@ describe('Object Copy', () => {
                         successCopyCheck(err, res, originalMetadata,
                             sourceBucketName, destObjName, done)
                     );
+            });
+
+        it('should not return error if copying object w/ > ' +
+        '2KB user-defined md and COPY directive',
+            done => {
+                const metadata = genMaxSizeMetaHeaders();
+                const params = {
+                    Bucket: destBucketName,
+                    Key: destObjName,
+                    CopySource: `${sourceBucketName}/${sourceObjName}`,
+                    MetadataDirective: 'COPY',
+                    Metadata: metadata,
+                };
+                s3.copyObject(params, err => {
+                    assert.strictEqual(err, null, `Unexpected err: ${err}`);
+                    // add one more byte to be over the limit
+                    metadata.header0 = `${metadata.header0}${'0'}`;
+                    s3.copyObject(params, err => {
+                        assert.strictEqual(err, null, `Unexpected err: ${err}`);
+                        done();
+                    });
+                });
+            });
+
+        it('should return error if copying object w/ > 2KB ' +
+        'user-defined md and REPLACE directive',
+            done => {
+                const metadata = genMaxSizeMetaHeaders();
+                const params = {
+                    Bucket: destBucketName,
+                    Key: destObjName,
+                    CopySource: `${sourceBucketName}/${sourceObjName}`,
+                    MetadataDirective: 'REPLACE',
+                    Metadata: metadata,
+                };
+                s3.copyObject(params, err => {
+                    assert.strictEqual(err, null, `Unexpected err: ${err}`);
+                    // add one more byte to be over the limit
+                    metadata.header0 = `${metadata.header0}${'0'}`;
+                    s3.copyObject(params, err => {
+                        assert(err, 'Expected err but did not find one');
+                        assert.strictEqual(err.code, 'MetadataTooLarge');
+                        assert.strictEqual(err.statusCode, 400);
+                        done();
+                    });
+                });
             });
 
         it('should copy an object from a source to the same destination ' +
