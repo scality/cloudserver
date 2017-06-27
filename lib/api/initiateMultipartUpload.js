VadzimBelski-ScienceSoft/S3

Add MPU initiate/complete and putPart for AWS

lib/api/initiateMultipartUpload.js
@@ -11,6 +11,8 @@ const { metadataValidateBucketAndObj } = require('../metadata/metadataUtils');
 const utils = require('../utils');
 const locationConstraintCheck
     = require('./apiUtils/object/locationConstraintCheck');
+const { config } = require('../Config');
+const multipleBackendGateway = require('../data/multipleBackendGateway');
 
 /*
 Sample xml response:
@@ -75,8 +77,6 @@ function initiateMultipartUpload(authInfo, request, log, callback) {
         return callback(err);
     }
     const metaHeaders = utils.getMetaHeaders(request.headers);
-    // Generate uniqueID without dashes so that routing not messed up
-    const uploadId = UUID.v4().replace(/-/g, '');
     // TODO: Add this as a utility function for all object put requests
     // but after authentication so that string to sign is not impacted
     // This is GH Issue#89
@@ -102,55 +102,38 @@ function initiateMultipartUpload(authInfo, request, log, callback) {
         initiatorID = authInfo.getArn();
         initiatorDisplayName = authInfo.getIAMdisplayName();
     }
-    const xmlParams = {
-        bucketName,
-        objectKey,
-        uploadId,
-    };
-    const xml = _convertToXml(xmlParams);
 
-    function _storetheMPObject(destinationBucket, corsHeaders) {
-        const metadataStoreParams = {
+    const metadataStoreParams = {
+        objectKey,
+        storageClass,
+        metaHeaders,
+        eventualStorageBucket: bucketName,
+        headers: request.headers,
+        // The ownerID should be the account canonicalID.
+        ownerID: accountCanonicalID,
+        ownerDisplayName: authInfo.getAccountDisplayName(),
+        // If initiator is an IAM user, the initiatorID is the ARN.
+        // Otherwise, it is the same as the ownerID
+        // (the account canonicalID)
+        initiatorID,
+        // If initiator is an IAM user, the initiatorDisplayName is the
+        // IAM user's displayname.
+        // Otherwise, it is the same as the ownerDisplayName.
+        initiatorDisplayName,
+        splitter: constants.splitter,
+    };
+
+    function _getMPUBucket(destinationBucket, log, corsHeaders,
+    uploadId, cipherBundle, callback) {
+        const xmlParams = {
+            bucketName,
             objectKey,
             uploadId,
-            storageClass,
-            metaHeaders,
-            eventualStorageBucket: bucketName,
-            headers: request.headers,
-            // The ownerID should be the account canonicalID.
-            ownerID: accountCanonicalID,
-            ownerDisplayName: authInfo.getAccountDisplayName(),
-            // If initiator is an IAM user, the initiatorID is the ARN.
-            // Otherwise, it is the same as the ownerID
-            // (the account canonicalID)
-            initiatorID,
-            // If initiator is an IAM user, the initiatorDisplayName is the
-            // IAM user's displayname.
-            // Otherwise, it is the same as the ownerDisplayName.
-            initiatorDisplayName,
-            splitter: constants.splitter,
         };
+        const xml = _convertToXml(xmlParams);
+        metadataStoreParams.uploadId = uploadId;
 
-        const serverSideEncryption =
-            destinationBucket.getServerSideEncryption();
-        let cipherBundle = null;
-        if (serverSideEncryption) {
-            cipherBundle = {
-                algorithm: serverSideEncryption.algorithm,
-                masterKeyId: serverSideEncryption.masterKeyId,
-            };
-        }
-        const backendInfoObj = locationConstraintCheck(request, null,
-            destinationBucket, log);
-        if (backendInfoObj.err) {
-            return process.nextTick(() => {
-                callback(backendInfoObj.err);
-            });
-        }
-        metadataStoreParams.controllingLocationConstraint =
-            backendInfoObj.controllingLC;
-
-        return services.getMPUBucket(destinationBucket, bucketName, log,
+        services.getMPUBucket(destinationBucket, bucketName, log,
             (err, MPUbucket) => {
                 if (err) {
                     log.trace('error getting MPUbucket', {
@@ -180,6 +163,46 @@ function initiateMultipartUpload(authInfo, request, log, callback) {
                         return callback(null, xml, corsHeaders);
                     });
             });
+    }
+
+    function _storetheMPObject(destinationBucket, corsHeaders) {
+        const serverSideEncryption =
+            destinationBucket.getServerSideEncryption();
+        let cipherBundle = null;
+        if (serverSideEncryption) {
+            cipherBundle = {
+                algorithm: serverSideEncryption.algorithm,
+                masterKeyId: serverSideEncryption.masterKeyId,
+            };
+        }
+        const backendInfoObj = locationConstraintCheck(request, null,
+            destinationBucket, log);
+        if (backendInfoObj.err) {
+            return process.nextTick(() => {
+                callback(backendInfoObj.err);
+            });
+        }
+        const locConstraint = backendInfoObj.controllingLC;
+        metadataStoreParams.controllingLocationConstraint = locConstraint;
+
+        let uploadId;
+        if (config.backends.data === 'multiple') {
+            return multipleBackendGateway.createMPU(objectKey, bucketName,
+            metaHeaders, websiteRedirectHeader, locConstraint, log,
+            (err, data) => {
+                if (err) {
+                    return callback(err);
+                }
+                uploadId = data.UploadId;
+                return _getMPUBucket(destinationBucket, log, corsHeaders,
+                uploadId, cipherBundle, callback);
+            });
+        }
+        // Generate uniqueID without dashes so that routing not messed up
+        uploadId = UUID.v4().replace(/-/g, '');
+
+        return _getMPUBucket(destinationBucket, log, corsHeaders,
+        uploadId, cipherBundle, callback);
     }
 
     metadataValidateBucketAndObj(metadataValParams, log,

scality/cloudserver

lib/api/initiateMultipartUpload.js
@@ -13,7 +13,8 @@ const locationConstraintCheck
     = require('./apiUtils/object/locationConstraintCheck');
 const validateWebsiteHeader = require('./apiUtils/object/websiteServing')
     .validateWebsiteHeader;
-
+const { config } = require('../Config');
+const multipleBackendGateway = require('../data/multipleBackendGateway');
 
 /*
 Sample xml response:
@@ -85,8 +86,6 @@ function initiateMultipartUpload(authInfo, request, log, callback) {
         });
         return process.nextTick(() => callback(metaHeaders));
     }
-    // Generate uniqueID without dashes so that routing not messed up
-    const uploadId = UUID.v4().replace(/-/g, '');
     // TODO: Add this as a utility function for all object put requests
     // but after authentication so that string to sign is not impacted
     // This is GH Issue#89
@@ -112,55 +111,38 @@ function initiateMultipartUpload(authInfo, request, log, callback) {
         initiatorID = authInfo.getArn();
         initiatorDisplayName = authInfo.getIAMdisplayName();
     }
-    const xmlParams = {
-        bucketName,
-        objectKey,
-        uploadId,
-    };
-    const xml = _convertToXml(xmlParams);
 
-    function _storetheMPObject(destinationBucket, corsHeaders) {
-        const metadataStoreParams = {
+    const metadataStoreParams = {
+        objectKey,
+        storageClass,
+        metaHeaders,
+        eventualStorageBucket: bucketName,
+        headers: request.headers,
+        // The ownerID should be the account canonicalID.
+        ownerID: accountCanonicalID,
+        ownerDisplayName: authInfo.getAccountDisplayName(),
+        // If initiator is an IAM user, the initiatorID is the ARN.
+        // Otherwise, it is the same as the ownerID
+        // (the account canonicalID)
+        initiatorID,
+        // If initiator is an IAM user, the initiatorDisplayName is the
+        // IAM user's displayname.
+        // Otherwise, it is the same as the ownerDisplayName.
+        initiatorDisplayName,
+        splitter: constants.splitter,
+    };
+
+    function _getMPUBucket(destinationBucket, log, corsHeaders,
+    uploadId, cipherBundle, callback) {
+        const xmlParams = {
+            bucketName,
             objectKey,
             uploadId,
-            storageClass,
-            metaHeaders,
-            eventualStorageBucket: bucketName,
-            headers: request.headers,
-            // The ownerID should be the account canonicalID.
-            ownerID: accountCanonicalID,
-            ownerDisplayName: authInfo.getAccountDisplayName(),
-            // If initiator is an IAM user, the initiatorID is the ARN.
-            // Otherwise, it is the same as the ownerID
-            // (the account canonicalID)
-            initiatorID,
-            // If initiator is an IAM user, the initiatorDisplayName is the
-            // IAM user's displayname.
-            // Otherwise, it is the same as the ownerDisplayName.
-            initiatorDisplayName,
-            splitter: constants.splitter,
         };
+        const xml = _convertToXml(xmlParams);
+        metadataStoreParams.uploadId = uploadId;
 
-        const serverSideEncryption =
-            destinationBucket.getServerSideEncryption();
-        let cipherBundle = null;
-        if (serverSideEncryption) {
-            cipherBundle = {
-                algorithm: serverSideEncryption.algorithm,
-                masterKeyId: serverSideEncryption.masterKeyId,
-            };
-        }
-        const backendInfoObj = locationConstraintCheck(request, null,
-            destinationBucket, log);
-        if (backendInfoObj.err) {
-            return process.nextTick(() => {
-                callback(backendInfoObj.err);
-            });
-        }
-        metadataStoreParams.controllingLocationConstraint =
-            backendInfoObj.controllingLC;
-
-        return services.getMPUBucket(destinationBucket, bucketName, log,
+        services.getMPUBucket(destinationBucket, bucketName, log,
             (err, MPUbucket) => {
                 if (err) {
                     log.trace('error getting MPUbucket', {
@@ -190,6 +172,52 @@ function initiateMultipartUpload(authInfo, request, log, callback) {
                         return callback(null, xml, corsHeaders);
                     });
             });
+    }
+
+    function _storetheMPObject(destinationBucket, corsHeaders) {
+        const serverSideEncryption =
+            destinationBucket.getServerSideEncryption();
+        let cipherBundle = null;
+        if (serverSideEncryption) {
+            cipherBundle = {
+                algorithm: serverSideEncryption.algorithm,
+                masterKeyId: serverSideEncryption.masterKeyId,
+            };
+        }
+        const backendInfoObj = locationConstraintCheck(request, null,
+            destinationBucket, log);
+        if (backendInfoObj.err) {
+            return process.nextTick(() => {
+                callback(backendInfoObj.err);
+            });
+        }
+        const locConstraint = backendInfoObj.controllingLC;
+        metadataStoreParams.controllingLocationConstraint = locConstraint;
+
+        let uploadId;
+        if (config.backends.data === 'multiple') {
+            return multipleBackendGateway.createMPU(objectKey, metaHeaders,
+            destinationBucket, websiteRedirectHeader, locConstraint, log,
+            (err, dataBackendResObj) => {
+                if (err) {
+                    return callback(err);
+                }
+                if (dataBackendResObj) {
+                    // dataBackendResObj will be returned in data backend
+                    // handles mpu
+                    uploadId = dataBackendResObj.UploadId;
+                } else {
+                    uploadId = UUID.v4().replace(/-/g, '');
+                }
+                return _getMPUBucket(destinationBucket, log, corsHeaders,
+                uploadId, cipherBundle, callback);
+            });
+        }
+        // Generate uniqueID without dashes so that routing not messed up
+        uploadId = UUID.v4().replace(/-/g, '');
+
+        return _getMPUBucket(destinationBucket, log, corsHeaders,
+        uploadId, cipherBundle, callback);
     }
 
     metadataValidateBucketAndObj(metadataValParams, log,