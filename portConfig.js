bd4487eb07e78f38c519e0658a0354c4a10158bb
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2017-07-06 10:45:28 +0200

    change file tree

--- portConfig.js
+++ portConfig.js
@@ -6,7 +6,7 @@ var config = fs.readFileSync(configpath, { encoding: 'utf-8' });
 config = JSON.parse(config);
 
 config.port = 8000 + ((config.port - 8000 + 1) % 3);
-config.antidote.port = 8087 + ((config.antidote.port - 8087 + 100) % 300);
+config.antidote.port = 8187 + ((config.antidote.port - 8187 + 100) % 400);
 config.indexServerPort = 7000 + ((config.indexServerPort - 7000 + 1) % 3);
 
 config = JSON.stringify(config);
