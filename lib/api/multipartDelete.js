JoshPerkins/S3

Add abortMPU for AWS backend

lib/api/multipartDelete.js
@@ -5,9 +5,12 @@ const collectCorsHeaders = require('../utilities/collectCorsHeaders');
 const constants = require('../../constants');
 const data = require('../data/wrapper');
 const services = require('../services');
+const metadata = require('../metadata/wrapper');
 const { metadataValidateBucketAndObj } = require('../metadata/metadataUtils');
 const { pushMetric } = require('../utapi/utilities');
 const isLegacyAWSBehavior = require('../utilities/legacyAWSBehavior');
+const { config } = require('../Config');
+const multipleBackendGateway = require('../data/multipleBackendGateway');
 
 /**
  * multipartDelete - DELETE an open multipart upload from a bucket
@@ -69,23 +72,46 @@ function multipartDelete(authInfo, request, log, callback) {
                     return next(err, mpuBucket, mpuOverviewArray, destBucket);
                 });
         },
+        function getMPUMetadata(mpuBucket, mpuOverviewArray, destBucket, next) {
+            let splitter = constants.splitter;
+            // BACKWARD: Remove to remove the old splitter
+            if (mpuBucket.getMdBucketModelVersion() < 2) {
+                splitter = constants.oldSplitter;
+            }
+            // Reconstruct mpuOverviewKey
+            const mpuOverviewKey =
+                `overview${splitter}${objectKey}${splitter}${uploadId}`;
+
+            return metadata.getObjectMD(mpuBucket.getName(), mpuOverviewKey,
+            {}, log, (err, storedMetadata) => {
+                if (err) {
+                    return next(err, destBucket);
+                }
+                const location = storedMetadata.controllingLocationConstraint;
+                return next(null, mpuBucket, mpuOverviewArray, destBucket,
+                location, mpuOverviewKey);
+            });
+        },
         function getPartLocations(mpuBucket, mpuOverviewArray, destBucket,
-            next) {
+            location, mpuOverviewKey, next) {
             services.getMPUparts(mpuBucket.getName(), uploadId, log,
                 (err, result) => {
                     if (err) {
                         return next(err, destBucket);
                     }
                     const storedParts = result.Contents;
-                    return next(null, mpuBucket, storedParts, destBucket);
+                    return next(null, mpuBucket, storedParts, destBucket,
+                    location, mpuOverviewKey);
                 });
         },
-        function deleteData(mpuBucket, storedParts, destBucket, next) {
+        function deleteData(mpuBucket, storedParts, destBucket, location,
+        mpuOverviewKey, next) {
             // The locations were sent to metadata as an array
             // under partLocations.  Pull the partLocations.
             let locations = storedParts.map(item => item.value.partLocations);
             if (locations.length === 0) {
-                return next(null, mpuBucket, storedParts, destBucket);
+                return next(null, mpuBucket, storedParts, destBucket, location,
+                mpuOverviewKey);
             }
             // flatten the array
             locations = [].concat.apply([], locations);
@@ -96,17 +122,11 @@ function multipartDelete(authInfo, request, log, callback) {
                     }
                     cb();
                 });
-            }, () => next(null, mpuBucket, storedParts, destBucket));
+            }, () => next(null, mpuBucket, storedParts, destBucket, location,
+                        mpuOverviewKey));
         },
-        function deleteMetadata(mpuBucket, storedParts, destBucket, next) {
-            let splitter = constants.splitter;
-            // BACKWARD: Remove to remove the old splitter
-            if (mpuBucket.getMdBucketModelVersion() < 2) {
-                splitter = constants.oldSplitter;
-            }
-            // Reconstruct mpuOverviewKey
-            const mpuOverviewKey =
-                `overview${splitter}${objectKey}${splitter}${uploadId}`;
+        function deleteMetadata(mpuBucket, storedParts, destBucket, location,
+        mpuOverviewKey, next) {
             // Get the sum of all part sizes to include in pushMetric object
             const partSizeSum = storedParts.map(item => item.value.Size)
                 .reduce((currPart, nextPart) => currPart + nextPart, 0);
@@ -122,14 +142,26 @@ function multipartDelete(authInfo, request, log, callback) {
                         bucket: bucketName,
                         byteLength: partSizeSum,
                     });
+                    return next(null, destBucket, location);
+                });
+        },
+        function ifMultipleBackend(destBucket, location, next) {
+            if (config.backends.data === 'multiple') {
+                return multipleBackendGateway.abortMPU(objectKey,
+                uploadId, location, log, err => {
+                    if (err) {
+                        return next(err, destBucket);
+                    }
                     return next(null, destBucket);
                 });
+            }
+            return next(null, destBucket);
         },
     ], (err, destinationBucket) => {
         const corsHeaders = collectCorsHeaders(request.headers.origin,
             request.method, destinationBucket);
         const locationConstraint = destinationBucket ?
-          destinationBucket.getLocationConstraint() : null;
+            destinationBucket.getLocationConstraint() : null;
         if (err === errors.NoSuchUpload) {
             log.trace('did not find valid mpu with uploadId' +
             `${uploadId}`, { method: 'multipartDelete' });

scality/cloudserver

lib/api/multipartDelete.js
@@ -5,9 +5,12 @@ const collectCorsHeaders = require('../utilities/collectCorsHeaders');
 const constants = require('../../constants');
 const data = require('../data/wrapper');
 const services = require('../services');
+const metadata = require('../metadata/wrapper');
 const { metadataValidateBucketAndObj } = require('../metadata/metadataUtils');
 const { pushMetric } = require('../utapi/utilities');
 const isLegacyAWSBehavior = require('../utilities/legacyAWSBehavior');
+const { config } = require('../Config');
+const multipleBackendGateway = require('../data/multipleBackendGateway');
 
 /**
  * multipartDelete - DELETE an open multipart upload from a bucket
@@ -69,23 +72,58 @@ function multipartDelete(authInfo, request, log, callback) {
                     return next(err, mpuBucket, mpuOverviewArray, destBucket);
                 });
         },
+        function ifMultipleBackend(mpuBucket, mpuOverviewArray, destBucket,
+        next) {
+            let splitter = constants.splitter;
+            // BACKWARD: Remove to remove the old splitter
+            if (mpuBucket.getMdBucketModelVersion() < 2) {
+                splitter = constants.oldSplitter;
+            }
+            // Reconstruct mpuOverviewKey
+            const mpuOverviewKey =
+                `overview${splitter}${objectKey}${splitter}${uploadId}`;
+
+            if (config.backends.data === 'multiple') {
+                return metadata.getObjectMD(mpuBucket.getName(), mpuOverviewKey,
+                {}, log, (err, storedMetadata) => {
+                    if (err) {
+                        return next(err, destBucket);
+                    }
+                    const location = storedMetadata.
+                        controllingLocationConstraint;
+                    return multipleBackendGateway.abortMPU(objectKey, uploadId,
+                    location, destBucket, log, err => {
+                        if (err) {
+                            return next(err, destBucket);
+                        }
+                        return next(null, mpuBucket, mpuOverviewArray,
+                        destBucket, mpuOverviewKey);
+                    });
+                });
+            }
+            return next(null, mpuBucket, mpuOverviewArray, destBucket,
+            mpuOverviewKey);
+        },
         function getPartLocations(mpuBucket, mpuOverviewArray, destBucket,
-            next) {
+            mpuOverviewKey, next) {
             services.getMPUparts(mpuBucket.getName(), uploadId, log,
                 (err, result) => {
                     if (err) {
                         return next(err, destBucket);
                     }
                     const storedParts = result.Contents;
-                    return next(null, mpuBucket, storedParts, destBucket);
+                    return next(null, mpuBucket, storedParts, destBucket,
+                    mpuOverviewKey);
                 });
         },
-        function deleteData(mpuBucket, storedParts, destBucket, next) {
+        function deleteData(mpuBucket, storedParts, destBucket, mpuOverviewKey,
+        next) {
             // The locations were sent to metadata as an array
             // under partLocations.  Pull the partLocations.
             let locations = storedParts.map(item => item.value.partLocations);
             if (locations.length === 0) {
-                return next(null, mpuBucket, storedParts, destBucket);
+                return next(null, mpuBucket, storedParts, destBucket,
+                mpuOverviewKey);
             }
             // flatten the array
             locations = [].concat.apply([], locations);
@@ -96,17 +134,11 @@ function multipartDelete(authInfo, request, log, callback) {
                     }
                     cb();
                 });
-            }, () => next(null, mpuBucket, storedParts, destBucket));
+            }, () => next(null, mpuBucket, storedParts, destBucket,
+                        mpuOverviewKey));
         },
-        function deleteMetadata(mpuBucket, storedParts, destBucket, next) {
-            let splitter = constants.splitter;
-            // BACKWARD: Remove to remove the old splitter
-            if (mpuBucket.getMdBucketModelVersion() < 2) {
-                splitter = constants.oldSplitter;
-            }
-            // Reconstruct mpuOverviewKey
-            const mpuOverviewKey =
-                `overview${splitter}${objectKey}${splitter}${uploadId}`;
+        function deleteMetadata(mpuBucket, storedParts, destBucket,
+        mpuOverviewKey, next) {
             // Get the sum of all part sizes to include in pushMetric object
             const partSizeSum = storedParts.map(item => item.value.Size)
                 .reduce((currPart, nextPart) => currPart + nextPart, 0);
@@ -129,7 +161,7 @@ function multipartDelete(authInfo, request, log, callback) {
         const corsHeaders = collectCorsHeaders(request.headers.origin,
             request.method, destinationBucket);
         const locationConstraint = destinationBucket ?
-          destinationBucket.getLocationConstraint() : null;
+            destinationBucket.getLocationConstraint() : null;
         if (err === errors.NoSuchUpload) {
             log.trace('did not find valid mpu with uploadId' +
             `${uploadId}`, { method: 'multipartDelete' });