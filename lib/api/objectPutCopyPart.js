etsangsplk/S3

ft: Add ObjectPutCopyPart for AWS backend

lib/api/objectPutCopyPart.js
@@ -13,9 +13,14 @@ const services = require('../services');
 const setUpCopyLocator = require('./apiUtils/object/setUpCopyLocator');
 const validateHeaders = require('../utilities/validateHeaders');
 const { metadataValidateBucketAndObj } = require('../metadata/metadataUtils');
+const { config } = require('../Config');
+const multipleBackendGateway = require('../data/multipleBackendGateway');
 
 const versionIdUtils = versioning.VersionID;
 
+const skipError = new Error('skip');
+// eslint-disable-next-line camelcase
+const cloudBackends = { aws_s3: true };
 
 /**
  * PUT Part Copy during a multipart upload.
@@ -218,12 +223,14 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
         function goGetData(dataLocator, destBucketMD,
             objectLocationConstraint, copyObjectSize, sourceVerId, next) {
             const serverSideEncryption = destBucketMD.getServerSideEncryption();
+            const lastModified = new Date().toJSON();
 
             // skip if 0 byte object
             if (dataLocator.length === 0) {
                 return process.nextTick(() => {
                     next(null, destBucketMD, [], constants.emptyFileMd5,
-                        copyObjectSize, sourceVerId, serverSideEncryption);
+                        copyObjectSize, sourceVerId, serverSideEncryption,
+                        lastModified);
                 });
             }
             const backendInfo = new BackendInfo(objectLocationConstraint);
@@ -250,6 +257,30 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
                         stream.pipe(hashedStream);
                         const numberPartSize =
                             Number.parseInt(part.size, 10);
+
+                        if (cloudBackends[config.locationConstraints
+                        [objectLocationConstraint].type]) {
+                            return multipleBackendGateway.uploadPart(null, null,
+                            hashedStream, numberPartSize,
+                            objectLocationConstraint, destObjectKey, uploadId,
+                            partNumber, destBucketName, log, err => {
+                                if (err) {
+                                    log.error('error putting ' +
+                                    'part to AWS', {
+                                        error: err,
+                                        method:
+                                        'objectPutCopyPart::' +
+                                        'multipleBackendGateway.' +
+                                        'uploadPart',
+                                    });
+                                    return cb(errors.InternalError);
+                                }
+                                // skip to end of waterfall
+                                // because don't need to store
+                                // part metadata
+                                return cb(skipError);
+                            });
+                        }
                         if (serverSideEncryption) {
                             return kms.createCipherBundle(
                                 serverSideEncryption,
@@ -310,19 +341,26 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
                         });
                     });
                 }, err => {
+                    // Digest the final combination of all of the part streams
+                    totalHash = totalHash.digest('hex');
                     if (err) {
+                        if (err === skipError) {
+                            return next(skipError, destBucketMD, totalHash,
+                                lastModified, sourceVerId,
+                                serverSideEncryption);
+                        }
                         log.debug('error transferring data from source',
                         { error: err, method: 'goGetData' });
                         return next(err, destBucketMD);
                     }
-                    // Digest the final combination of all of the part streams
-                    totalHash = totalHash.digest('hex');
                     return next(null, destBucketMD, locations, totalHash,
-                        copyObjectSize, sourceVerId, serverSideEncryption);
+                        copyObjectSize, sourceVerId, serverSideEncryption,
+                        lastModified);
                 });
         },
         function getExistingPartInfo(destBucketMD, locations, totalHash,
-            copyObjectSize, sourceVerId, serverSideEncryption, next) {
+            copyObjectSize, sourceVerId, serverSideEncryption, lastModified,
+            next) {
             const partKey =
                 `${uploadId}${constants.splitter}${paddedPartNumber}`;
             metadata.getObjectMD(mpuBucketName, partKey, {}, log,
@@ -344,13 +382,12 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
                     }
                     return next(null, destBucketMD, locations, totalHash,
                         copyObjectSize, sourceVerId, serverSideEncryption,
-                        oldLocations);
+                        lastModified, oldLocations);
                 });
         },
         function storeNewPartMetadata(destBucketMD, locations, totalHash,
-            copyObjectSize, sourceVerId, serverSideEncryption, oldLocations,
-            next) {
-            const lastModified = new Date().toJSON();
+            copyObjectSize, sourceVerId, serverSideEncryption, lastModified,
+            oldLocations, next) {
             const metaStoreParams = {
                 partNumber: paddedPartNumber,
                 contentMD5: totalHash,
@@ -381,7 +418,7 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
         serverSideEncryption) => {
         const corsHeaders = collectCorsHeaders(request.headers.origin,
             request.method, destBucketMD);
-        if (err) {
+        if (err && err !== skipError) {
             log.trace('error from copy part waterfall',
             { error: err });
             return callback(err, null, corsHeaders);

scality/cloudserver

lib/api/objectPutCopyPart.js
@@ -13,9 +13,13 @@ const logger = require('../utilities/logger');
 const services = require('../services');
 const setUpCopyLocator = require('./apiUtils/object/setUpCopyLocator');
 const { metadataValidateBucketAndObj } = require('../metadata/metadataUtils');
+const { config } = require('../Config');
+const multipleBackendGateway = require('../data/multipleBackendGateway');
 
 const versionIdUtils = versioning.VersionID;
 
+const skipError = new Error('skip');
+const externalBackends = constants.externalBackends;
 
 /**
  * PUT Part Copy during a multipart upload.
@@ -220,12 +224,14 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
         function goGetData(dataLocator, destBucketMD,
             objectLocationConstraint, copyObjectSize, sourceVerId, next) {
             const serverSideEncryption = destBucketMD.getServerSideEncryption();
+            const lastModified = new Date().toJSON();
 
             // skip if 0 byte object
             if (dataLocator.length === 0) {
                 return process.nextTick(() => {
                     next(null, destBucketMD, [], constants.emptyFileMd5,
-                        copyObjectSize, sourceVerId, serverSideEncryption);
+                        copyObjectSize, sourceVerId, serverSideEncryption,
+                        lastModified);
                 });
             }
             const backendInfo = new BackendInfo(objectLocationConstraint);
@@ -252,6 +258,32 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
                         stream.pipe(hashedStream);
                         const numberPartSize =
                             Number.parseInt(part.size, 10);
+
+                        // objectLocationConstraint is location of the
+                        // destination MPU object
+                        if (externalBackends[config.locationConstraints
+                        [objectLocationConstraint].type]) {
+                            return multipleBackendGateway.uploadPart(null, null,
+                            hashedStream, numberPartSize,
+                            objectLocationConstraint, destObjectKey, uploadId,
+                            partNumber, destBucketName, log, err => {
+                                if (err) {
+                                    log.error('error putting ' +
+                                    'part to AWS', {
+                                        error: err,
+                                        method:
+                                        'objectPutCopyPart::' +
+                                        'multipleBackendGateway.' +
+                                        'uploadPart',
+                                    });
+                                    return cb(errors.InternalError);
+                                }
+                                // skip to end of waterfall
+                                // because don't need to store
+                                // part metadata
+                                return cb(skipError);
+                            });
+                        }
                         if (serverSideEncryption) {
                             return kms.createCipherBundle(
                                 serverSideEncryption,
@@ -312,19 +344,26 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
                         });
                     });
                 }, err => {
+                    // Digest the final combination of all of the part streams
+                    totalHash = totalHash.digest('hex');
                     if (err) {
+                        if (err === skipError) {
+                            return next(skipError, destBucketMD, totalHash,
+                                lastModified, sourceVerId,
+                                serverSideEncryption);
+                        }
                         log.debug('error transferring data from source',
                         { error: err, method: 'goGetData' });
                         return next(err, destBucketMD);
                     }
-                    // Digest the final combination of all of the part streams
-                    totalHash = totalHash.digest('hex');
                     return next(null, destBucketMD, locations, totalHash,
-                        copyObjectSize, sourceVerId, serverSideEncryption);
+                        copyObjectSize, sourceVerId, serverSideEncryption,
+                        lastModified);
                 });
         },
         function getExistingPartInfo(destBucketMD, locations, totalHash,
-            copyObjectSize, sourceVerId, serverSideEncryption, next) {
+            copyObjectSize, sourceVerId, serverSideEncryption, lastModified,
+            next) {
             const partKey =
                 `${uploadId}${constants.splitter}${paddedPartNumber}`;
             metadata.getObjectMD(mpuBucketName, partKey, {}, log,
@@ -346,13 +385,12 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
                     }
                     return next(null, destBucketMD, locations, totalHash,
                         copyObjectSize, sourceVerId, serverSideEncryption,
-                        oldLocations);
+                        lastModified, oldLocations);
                 });
         },
         function storeNewPartMetadata(destBucketMD, locations, totalHash,
-            copyObjectSize, sourceVerId, serverSideEncryption, oldLocations,
-            next) {
-            const lastModified = new Date().toJSON();
+            copyObjectSize, sourceVerId, serverSideEncryption, lastModified,
+            oldLocations, next) {
             const metaStoreParams = {
                 partNumber: paddedPartNumber,
                 contentMD5: totalHash,
@@ -383,7 +421,7 @@ function objectPutCopyPart(authInfo, request, sourceBucket,
         serverSideEncryption) => {
         const corsHeaders = collectCorsHeaders(request.headers.origin,
             request.method, destBucketMD);
-        if (err) {
+        if (err && err !== skipError) {
             log.trace('error from copy part waterfall',
             { error: err });
             return callback(err, null, corsHeaders);