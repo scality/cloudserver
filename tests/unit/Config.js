f6e811e5d145587faa57461e578160b03a2501af
Rached Ben Mustapha <rached.ben-mustapha@scality.com>
2017-09-06 12:29:28 -0700

    FT: Allow updating account data on the fly

--- tests/unit/Config.js
+++ tests/unit/Config.js
@@ -3,4 +3,18 @@ describe('Config', () => {
         require('../../lib/Config');
         done();
     });
+
+    it('should emit an event when auth data is updated', done => {
+        const { ConfigObject } = require('../../lib/Config');
+        const config = new ConfigObject();
+        let emitted = false;
+        config.on('authdata-update', () => {
+            emitted = true;
+        });
+        config.setAuthDataAccounts([]);
+        if (emitted) {
+            return done();
+        }
+        return done(new Error('authdata-update event was not emitted'));
+    });
 });
