713859e463d9b006ea0fc5de4df30fd695f16ce1
Electra Chong <electra.chong@scality.com>
2017-11-07 15:35:06 -0800

    chore: add keep-alive to real awsclient config

--- tests/functional/aws-node-sdk/test/support/awsConfig.js
+++ tests/functional/aws-node-sdk/test/support/awsConfig.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
 const { config } = require('../../../../../lib/Config');
+const https = require('https');
 
 function getAwsCredentials(profile, credFile) {
     const filename = path.join(process.env.HOME, credFile);
@@ -25,6 +26,11 @@ function getRealAwsConfig(awsLocation) {
         return { credentials, signatureVersion: 'v4' };
     }
     return {
+        httpOptions: {
+            agent: new https.Agent({
+                keepAlive: true,
+            }),
+        },
         accessKeyId: locCredentials.accessKey,
         secretAccessKey: locCredentials.secretKey,
         signatureVersion: 'v4',
