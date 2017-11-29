DeepDiver1975/S3

FIX: S3C-1087 put and copy metadata for external backends

lib/data/external/AzureClient.js
@@ -1,7 +1,7 @@
 const { errors, s3middleware } = require('arsenal');
 const azure = require('azure-storage');
 const createLogger = require('../multipleBackendLogger');
-const { logHelper } = require('./utils');
+const { logHelper, translateMetaHeaders } = require('./utils');
 const { config } = require('../../Config');
 const { validateAndFilterMpuParts } =
     require('../../api/apiUtils/object/processMpuParts');
@@ -49,26 +49,6 @@ class AzureClient {
         return `${requestBucketName}/${requestObjectKey}`;
     }
 
-    _translateMetaHeaders(metaHeaders, tags) {
-        const translatedMetaHeaders = {};
-        if (tags) {
-            // tags are passed as string of format 'key1=value1&key2=value2'
-            const tagObj = {};
-            const tagArr = tags.split('&');
-            tagArr.forEach(keypair => {
-                const equalIndex = keypair.indexOf('=');
-                const key = keypair.substring(0, equalIndex);
-                tagObj[key] = keypair.substring(equalIndex + 1);
-            });
-            translatedMetaHeaders.tags = JSON.stringify(tagObj);
-        }
-        Object.keys(metaHeaders).forEach(headerName => {
-            const translated = headerName.replace(/-/g, '_');
-            translatedMetaHeaders[translated] = metaHeaders[headerName];
-        });
-        return translatedMetaHeaders;
-    }
-
     _getMetaHeaders(objectMD) {
         const metaHeaders = {};
         Object.keys(objectMD).forEach(mdKey => {
@@ -77,7 +57,7 @@ class AzureClient {
                 metaHeaders[mdKey] = objectMD[mdKey];
             }
         });
-        return this._translateMetaHeaders(metaHeaders);
+        return translateMetaHeaders(metaHeaders);
     }
 
     // Before putting or deleting object on Azure, check if MPU exists with
@@ -126,7 +106,7 @@ class AzureClient {
             const azureKey = this._createAzureKey(keyContext.bucketName,
                 keyContext.objectKey, this._bucketMatch);
             const options = { metadata:
-                this._translateMetaHeaders(keyContext.metaHeaders,
+                translateMetaHeaders(keyContext.metaHeaders,
                 keyContext.tagging) };
             if (size === 0) {
                 return this._errorWrapper('put', 'createBlockBlobFromText',
@@ -354,7 +334,7 @@ class AzureClient {
     }
 
     copyObject(request, destLocationConstraintName, sourceKey,
-    sourceLocationConstraintName, log, callback) {
+    sourceLocationConstraintName, storeMetadataParams, log, callback) {
         const destContainerName = request.bucketName;
         const destObjectKey = request.objectKey;
 
@@ -365,10 +345,16 @@ class AzureClient {
         config.locationConstraints[sourceLocationConstraintName]
         .details.azureContainerName;
 
+        let options;
+        if (storeMetadataParams.metaHeaders) {
+            options = { metadata:
+            translateMetaHeaders(storeMetadataParams.metaHeaders) };
+        }
+
         this._errorWrapper('copyObject', 'startCopyBlob',
             [`${this._azureStorageEndpoint}` +
                 `${sourceContainerName}/${sourceKey}`,
-                this._azureContainerName, destAzureKey,
+                this._azureContainerName, destAzureKey, options,
             (err, res) => {
                 if (err) {
                     if (err.code === 'CannotVerifyCopySource') {

lib/data/external/utils.js
@@ -112,6 +112,25 @@ const utils = {
         }
         return cb();
     },
+    translateMetaHeaders(metaHeaders, tags) {
+        const translatedMetaHeaders = {};
+        if (tags) {
+            // tags are passed as string of format 'key1=value1&key2=value2'
+            const tagObj = {};
+            const tagArr = tags.split('&');
+            tagArr.forEach(keypair => {
+                const equalIndex = keypair.indexOf('=');
+                const key = keypair.substring(0, equalIndex);
+                tagObj[key] = keypair.substring(equalIndex + 1);
+            });
+            translatedMetaHeaders.tags = JSON.stringify(tagObj);
+        }
+        Object.keys(metaHeaders).forEach(headerName => {
+            const translated = headerName.substring(11).replace(/-/g, '_');
+            translatedMetaHeaders[translated] = metaHeaders[headerName];
+        });
+        return translatedMetaHeaders;
+    },
 };
 
 module.exports = utils;

scality/cloudserver

lib/data/external/AzureClient.js
@@ -3,7 +3,7 @@ const url = require('url');
 const { errors, s3middleware } = require('arsenal');
 const azure = require('azure-storage');
 const createLogger = require('../multipleBackendLogger');
-const { logHelper } = require('./utils');
+const { logHelper, translateAzureMetaHeaders } = require('./utils');
 const { config } = require('../../Config');
 const { validateAndFilterMpuParts } =
     require('../../api/apiUtils/object/processMpuParts');
@@ -58,26 +58,6 @@ class AzureClient {
         return `${requestBucketName}/${requestObjectKey}`;
     }
 
-    _translateMetaHeaders(metaHeaders, tags) {
-        const translatedMetaHeaders = {};
-        if (tags) {
-            // tags are passed as string of format 'key1=value1&key2=value2'
-            const tagObj = {};
-            const tagArr = tags.split('&');
-            tagArr.forEach(keypair => {
-                const equalIndex = keypair.indexOf('=');
-                const key = keypair.substring(0, equalIndex);
-                tagObj[key] = keypair.substring(equalIndex + 1);
-            });
-            translatedMetaHeaders.tags = JSON.stringify(tagObj);
-        }
-        Object.keys(metaHeaders).forEach(headerName => {
-            const translated = headerName.replace(/-/g, '_');
-            translatedMetaHeaders[translated] = metaHeaders[headerName];
-        });
-        return translatedMetaHeaders;
-    }
-
     _getMetaHeaders(objectMD) {
         const metaHeaders = {};
         Object.keys(objectMD).forEach(mdKey => {
@@ -86,7 +66,7 @@ class AzureClient {
                 metaHeaders[mdKey] = objectMD[mdKey];
             }
         });
-        return this._translateMetaHeaders(metaHeaders);
+        return translateAzureMetaHeaders(metaHeaders);
     }
 
     // Before putting or deleting object on Azure, check if MPU exists with
@@ -135,7 +115,7 @@ class AzureClient {
             const azureKey = this._createAzureKey(keyContext.bucketName,
                 keyContext.objectKey, this._bucketMatch);
             const options = { metadata:
-                this._translateMetaHeaders(keyContext.metaHeaders,
+                translateAzureMetaHeaders(keyContext.metaHeaders,
                 keyContext.tagging) };
             if (size === 0) {
                 return this._errorWrapper('put', 'createBlockBlobFromText',
@@ -363,7 +343,7 @@ class AzureClient {
     }
 
     copyObject(request, destLocationConstraintName, sourceKey,
-    sourceLocationConstraintName, log, callback) {
+    sourceLocationConstraintName, storeMetadataParams, log, callback) {
         const destContainerName = request.bucketName;
         const destObjectKey = request.objectKey;
 
@@ -374,10 +354,16 @@ class AzureClient {
         config.locationConstraints[sourceLocationConstraintName]
         .details.azureContainerName;
 
+        let options;
+        if (storeMetadataParams.metaHeaders) {
+            options = { metadata:
+            translateAzureMetaHeaders(storeMetadataParams.metaHeaders) };
+        }
+
         this._errorWrapper('copyObject', 'startCopyBlob',
             [`${this._azureStorageEndpoint}` +
                 `${sourceContainerName}/${sourceKey}`,
-                this._azureContainerName, destAzureKey,
+                this._azureContainerName, destAzureKey, options,
             (err, res) => {
                 if (err) {
                     if (err.code === 'CannotVerifyCopySource') {

lib/data/external/utils.js
@@ -112,6 +112,25 @@ const utils = {
         }
         return cb();
     },
+    translateAzureMetaHeaders(metaHeaders, tags) {
+        const translatedMetaHeaders = {};
+        if (tags) {
+            // tags are passed as string of format 'key1=value1&key2=value2'
+            const tagObj = {};
+            const tagArr = tags.split('&');
+            tagArr.forEach(keypair => {
+                const equalIndex = keypair.indexOf('=');
+                const key = keypair.substring(0, equalIndex);
+                tagObj[key] = keypair.substring(equalIndex + 1);
+            });
+            translatedMetaHeaders.tags = JSON.stringify(tagObj);
+        }
+        Object.keys(metaHeaders).forEach(headerName => {
+            const translated = headerName.substring(11).replace(/-/g, '_');
+            translatedMetaHeaders[translated] = metaHeaders[headerName];
+        });
+        return translatedMetaHeaders;
+    },
 };
 
 module.exports = utils;