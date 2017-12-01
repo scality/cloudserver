c42ddef0bd955a1d37e2a52b62d02d8896fdd25c
Salim <salim.salaues@scality.com>
2017-11-30 16:32:15 -0800

    Change env to existing

--- tests.bash
+++ tests.bash
@@ -4,11 +4,11 @@ set -e #exit at the first error
 
 cat >>~/.aws/credentials <<EOF
 [default]
-aws_access_key_id = $AWS_ACCESS_KEY_ID_DEFAULT
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY_DEFAULT
+aws_access_key_id = $AWS_S3_BACKEND_ACCESS_KEY
+aws_secret_access_key = $AWS_S3_BACKEND_SECRET_KEY
 [default_2]
-aws_access_key_id = $AWS_ACCESS_KEY_ID_DEFAULT_2
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY_DEFAULT_2
+aws_access_key_id = $AWS_S3_BACKEND_ACCESS_KEY_2
+aws_secret_access_key = $AWS_S3_BACKEND_SECRET_KEY_2
 [google]
 aws_access_key_id = $AWS_ACCESS_KEY_ID_GOOGLE
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY_GOOGLE
