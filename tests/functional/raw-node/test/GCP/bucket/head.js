92401fd58dd00eb874e963e1aa00c41bb3165ef0
Alexander Chan <alexander.chan@scality.com>
2018-01-29 10:27:27 -0800

    S3C-1115 FT: Adds GCP Backend healthcheck
    Implements the APIs to enable backend healthcheck of GCP storages buckets

---
+++ tests/functional/raw-node/test/GCP/bucket/head.js
@@ -0,0 +1,81 @@
+const assert = require('assert');
+const { GCP } = require('../../../../../../lib/data/external/GCP');
+const { gcpRequestRetry } = require('../../../utils/gcpUtils');
+const { getRealAwsConfig } =
+    require('../../../../aws-node-sdk/test/support/awsConfig');
+
+const credentialOne = 'gcpbackend';
+
+describe('GCP: HEAD Bucket', () => {
+    const config = getRealAwsConfig(credentialOne);
+    const gcpClient = new GCP(config);
+
+    describe('without existing bucket', () => {
+        beforeEach(function beforeFn(done) {
+            this.currentTest.bucketName = `somebucket-${Date.now()}`;
+            return done();
+        });
+
+        it('should return 404', function testFn(done) {
+            gcpClient.headBucket({
+                Bucket: this.test.bucketName,
+            }, err => {
+                assert(err);
+                assert.strictEqual(err.statusCode, 404);
+                return done();
+            });
+        });
+    });
+
+    describe('with existing bucket', () => {
+        beforeEach(function beforeFn(done) {
+            this.currentTest.bucketName = `somebucket-${Date.now()}`;
+            process.stdout
+                .write(`Creating test bucket ${this.currentTest.bucketName}\n`);
+            gcpRequestRetry({
+                method: 'PUT',
+                bucket: this.currentTest.bucketName,
+                authCredentials: config.credentials,
+            }, 0, (err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                if (!res) {
+                    // should not happen
+                    return done(
+                        new Error('Error: success response does not result'));
+                }
+                this.currentTest.bucketObj = {
+                    MetaVersionId: res.headers['x-goog-metageneration'],
+                };
+                return done();
+            });
+        });
+
+        afterEach(function afterFn(done) {
+            gcpRequestRetry({
+                method: 'DELETE',
+                bucket: this.currentTest.bucketName,
+                authCredentials: config.credentials,
+            }, 0, err => {
+                if (err) {
+                    process.stdout
+                        .write(`err deleting bucket: ${err.code}\n`);
+                } else {
+                    process.stdout.write('Deleted bucket\n');
+                }
+                return done(err);
+            });
+        });
+
+        it('should get bucket information', function testFn(done) {
+            gcpClient.headBucket({
+                Bucket: this.test.bucketName,
+            }, (err, res) => {
+                assert.equal(err, null, `Expected success, but got ${err}`);
+                assert.deepStrictEqual(this.test.bucketObj, res);
+                return done();
+            });
+        });
+    });
+});
