7aaba13bb54829ee971e0c5b909b008328fa657a
Lauren Spiegel <lhspiegel@gmail.com>
2018-02-01 11:48:14 -0800

    FT: Switch out mongo search for spark

--- tests/unit/utils/validateSearch.js
+++ tests/unit/utils/validateSearch.js
@@ -8,7 +8,7 @@ describe('validate search where clause', () => {
     const tests = [
         {
             it: 'should allow a valid simple search with table attribute',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador"',
+            searchParams: '`x-amz-meta-dog`="labrador"',
             result: undefined,
         },
         {
@@ -19,75 +19,75 @@ describe('validate search where clause', () => {
         },
         {
             it: 'should allow valid search with AND',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador" ' +
-            'AND userMd.`x-amz-meta-age`="5"',
+            searchParams: '`x-amz-meta-dog`="labrador" ' +
+            'AND `x-amz-meta-age`="5"',
             result: undefined,
         },
         {
             it: 'should allow valid search with OR',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador" ' +
-            'OR userMd.`x-amz-meta-age`="5"',
+            searchParams: '`x-amz-meta-dog`="labrador" ' +
+            'OR `x-amz-meta-age`="5"',
             result: undefined,
         },
         {
             it: 'should allow valid search with double AND',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador" ' +
-                'AND userMd.`x-amz-meta-age`="5" ' +
-                'AND userMd.`x-amz-meta-whatever`="ok"',
+            searchParams: '`x-amz-meta-dog`="labrador" ' +
+                'AND `x-amz-meta-age`="5" ' +
+                'AND `x-amz-meta-whatever`="ok"',
             result: undefined,
         },
         {
             it: 'should allow valid chained search with tables and columns',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador" ' +
-                'AND userMd.`x-amz-meta-age`="5" ' +
+            searchParams: '`x-amz-meta-dog`="labrador" ' +
+                'AND `x-amz-meta-age`="5" ' +
                 'AND `content-length`="10"' +
                 'OR isDeleteMarker="true"' +
-                'AND userMd.`x-amz-meta-whatever`="ok"',
+                'AND `x-amz-meta-whatever`="ok"',
             result: undefined,
         },
         {
             it: 'should allow valid LIKE search',
-            searchParams: 'userMd.`x-amz-meta-dog` LIKE "lab%" ' +
-                'AND userMd.`x-amz-meta-age` LIKE "5%" ' +
+            searchParams: '`x-amz-meta-dog` LIKE "lab%" ' +
+                'AND `x-amz-meta-age` LIKE "5%" ' +
                 'AND `content-length`="10"',
             result: undefined,
         },
         {
             it: 'should disallow a LIKE search with invalid attribute',
-            searchParams: 'userNotMd.`x-amz-meta-dog` LIKE "labrador"',
+            searchParams: '`x-zma-meta-dog` LIKE "labrador"',
             result: errors.InvalidArgument.customizeDescription('Search ' +
-            'param contains unknown attribute: userNotMd'),
+            'param contains unknown attribute: x-zma-meta-dog'),
         },
         {
             it: 'should disallow a simple search with unknown attribute',
-            searchParams: 'userNotMd.`x-amz-meta-dog`="labrador"',
+            searchParams: '`x-zma-meta-dog`="labrador"',
             result: errors.InvalidArgument.customizeDescription('Search ' +
-            'param contains unknown attribute: userNotMd'),
+            'param contains unknown attribute: x-zma-meta-dog'),
         },
         {
             it: 'should disallow a compound search with unknown ' +
                 'attribute on right',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador" AND ' +
-                'userNotMd.`x-amz-meta-dog`="labrador"',
+            searchParams: '`x-amz-meta-dog`="labrador" AND ' +
+                '`x-zma-meta-dog`="labrador"',
             result: errors.InvalidArgument.customizeDescription('Search ' +
-            'param contains unknown attribute: userNotMd'),
+            'param contains unknown attribute: x-zma-meta-dog'),
         },
         {
             it: 'should disallow a compound search with unknown ' +
                 'attribute on left',
-            searchParams: 'userNotMd.`x-amz-meta-dog`="labrador" AND ' +
-                'userMd.`x-amz-meta-dog`="labrador"',
+            searchParams: '`x-zma-meta-dog`="labrador" AND ' +
+                '`x-amz-meta-dog`="labrador"',
             result: errors.InvalidArgument.customizeDescription('Search ' +
-            'param contains unknown attribute: userNotMd'),
+            'param contains unknown attribute: x-zma-meta-dog'),
         },
         {
             it: 'should disallow a chained search with one invalid ' +
                 'table attribute',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador" ' +
-                'AND userMd.`x-amz-meta-age`="5" ' +
-                'OR userNotMd.`x-amz-meta-whatever`="ok"',
+            searchParams: '`x-amz-meta-dog`="labrador" ' +
+                'AND `x-amz-meta-age`="5" ' +
+                'OR `x-zma-meta-whatever`="ok"',
             result: errors.InvalidArgument.customizeDescription('Search ' +
-                'param contains unknown attribute: userNotMd'),
+                'param contains unknown attribute: x-zma-meta-whatever'),
         },
         {
             it: 'should disallow a simple search with unknown ' +
@@ -99,10 +99,10 @@ describe('validate search where clause', () => {
         {
             it: 'should disallow a chained search with one invalid ' +
                 'column attribute',
-            searchParams: 'userMd.`x-amz-meta-dog`="labrador" ' +
-                'AND userMd.`x-amz-meta-age`="5" ' +
+            searchParams: '`x-amz-meta-dog`="labrador" ' +
+                'AND `x-amz-meta-age`="5" ' +
                 'OR madeUp="something"' +
-                'OR userMd.`x-amz-meta-whatever`="ok"',
+                'OR `x-amz-meta-whatever`="ok"',
             result: errors.InvalidArgument.customizeDescription('Search ' +
                 'param contains unknown attribute: madeUp'),
         },
@@ -111,8 +111,12 @@ describe('validate search where clause', () => {
     tests.forEach(test => {
         it(test.it, () => {
             const actualResult =
-                validateSearch(test.searchParams);
-            assert.deepStrictEqual(actualResult, test.result);
+                  validateSearch(test.searchParams);
+            if (test.result === undefined) {
+                assert(typeof actualResult.ast === 'object');
+            } else {
+                assert.deepStrictEqual(actualResult.error, test.result);
+            }
         });
     });
 });
