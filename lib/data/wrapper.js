tuanda2/S3

S3C-1179 FT: Adds GCP MPU Func
Adds MPU functionalities to GCP Backend

lib/data/wrapper.js
@@ -569,6 +569,18 @@ const data = {
                     };
                     locations.push(partResult);
                     return cb();
+                } else if (
+                  partInfo &&
+                  partInfo.dataStoreType === 'gcp') {
+                    const partResult = {
+                        key: partInfo.key,
+                        dataStoreName: partInfo.dataStoreName,
+                        dataStoreETag: partInfo.dataStoreETag,
+                        size: numberPartSize,
+                        partNumber: partInfo.partNumber,
+                    };
+                    locations.push(partResult);
+                    return cb();
                 }
                 return cb(skipError);
             });
@@ -680,8 +692,15 @@ const data = {
         config.getLocationConstraintType(sourceLocationConstraintName) ===
         'aws_s3';
 
+        const locationTypeMatchGCP =
+        config.backends.data === 'multiple' &&
+        config.getLocationConstraintType(sourceLocationConstraintName) ===
+        config.getLocationConstraintType(destLocationConstraintName) &&
+        config.getLocationConstraintType(sourceLocationConstraintName) ===
+        'gcp';
+
         // NOTE: using multipleBackendGateway.uploadPartCopy only if copying
-        // from AWS to AWS
+        // from AWS to AWS or from GCP to GCP
 
         if (locationTypeMatchAWS && dataLocator.length === 1) {
             const awsSourceKey = dataLocator[0].key;
@@ -692,6 +711,19 @@ const data = {
                     return callback(error);
                 }
                 return callback(skipError, eTag,
+                    lastModified, serverSideEncryption);
+            });
+        }
+
+        if (locationTypeMatchGCP && dataLocator.lenegth === 1) {
+            const gcpSourceKey = dataLocator[0].key;
+            return multipleBackendGateway.uploadPartCopy(request,
+            destLocationConstraintName, gcpSourceKey,
+            sourceLocationConstraintName, log, (error, eTag) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, eTag,
                     lastModified, serverSideEncryption);
             });
         }

scality/cloudserver

lib/data/wrapper.js
@@ -569,6 +569,18 @@ const data = {
                     };
                     locations.push(partResult);
                     return cb();
+                } else if (
+                  partInfo &&
+                  partInfo.dataStoreType === 'gcp') {
+                    const partResult = {
+                        key: partInfo.key,
+                        dataStoreName: partInfo.dataStoreName,
+                        dataStoreETag: partInfo.dataStoreETag,
+                        size: numberPartSize,
+                        partNumber: partInfo.partNumber,
+                    };
+                    locations.push(partResult);
+                    return cb();
                 }
                 return cb(skipError);
             });
@@ -672,26 +684,26 @@ const data = {
                     lastModified, serverSideEncryption, []);
             });
         }
-
-        const locationTypeMatchAWS =
-        config.backends.data === 'multiple' &&
-        config.getLocationConstraintType(sourceLocationConstraintName) ===
-        config.getLocationConstraintType(destLocationConstraintName) &&
-        config.getLocationConstraintType(sourceLocationConstraintName) ===
-        'aws_s3';
+        const srcType =
+            config.getLocationConstraintType(sourceLocationConstraintName);
+        const dstType =
+            config.getLocationConstraintType(destLocationConstraintName);
+        const locationTypeMatch =
+            config.backends.data === 'multiple' && srcType === dstType &&
+            constants.hasCopyPartBackends[srcType];
 
         // NOTE: using multipleBackendGateway.uploadPartCopy only if copying
-        // from AWS to AWS
-
-        if (locationTypeMatchAWS && dataLocator.length === 1) {
-            const awsSourceKey = dataLocator[0].key;
+        // from AWS to AWS or from GCP to GCP
+        if (locationTypeMatch && dataLocator.length === 1) {
+            const sourceKey = dataLocator[0].key;
             return multipleBackendGateway.uploadPartCopy(request,
-            destLocationConstraintName, awsSourceKey,
+            destLocationConstraintName, sourceKey,
             sourceLocationConstraintName, log, (error, eTag) => {
                 if (error) {
                     return callback(error);
                 }
-                return callback(skipError, eTag,
+                const doSkip = srcType === 'aws' ? skipError : null;
+                return callback(doSkip, eTag,
                     lastModified, serverSideEncryption);
             });
         }