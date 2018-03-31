tuanda2/S3

ZENKO-144 ft: https proxy

lib/data/external/AzureClient.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const fs = require('fs');
 
 const { errors, s3middleware } = require('arsenal');
 const azure = require('azure-storage');
@@ -23,11 +24,27 @@ class AzureClient {
         this._dataStoreName = config.dataStoreName;
         this._bucketMatch = config.bucketMatch;
         if (config.proxy) {
-            const parsedUrl = url.parse(config.proxy);
+            const parsedUrl = url.parse(config.proxy.url);
             if (!parsedUrl.port) {
                 parsedUrl.port = 80;
             }
-            this._client.setProxy(parsedUrl);
+            const proxyParams = {
+                protocol: parsedUrl.protocol,
+                host: parsedUrl.hostname,
+                proxyAuth: parsedUrl.auth,
+                port: parsedUrl.prt,
+            };
+            if (config.proxy.certFilePaths) {
+                const { keyPath, certPath, caPath } =
+                    config.proxy.certFilePaths;
+                proxyParams.cert = certPath ?
+                    fs.readFileSync(certPath, 'ascii') : undefined;
+                proxyParams.key = keyPath ?
+                    fs.readFileSync(keyPath, 'ascii') : undefined;
+                proxyParams.ca = caPath ?
+                    fs.readFileSync(caPath, 'ascii') : undefined;
+            }
+            this._client.setProxy(proxyParams);
         }
     }
 

scality/cloudserver

lib/data/external/AzureClient.js
@@ -22,12 +22,21 @@ class AzureClient {
             this._azureStorageEndpoint);
         this._dataStoreName = config.dataStoreName;
         this._bucketMatch = config.bucketMatch;
-        if (config.proxy) {
-            const parsedUrl = url.parse(config.proxy);
+        if (config.proxy && config.proxy.url) {
+            const parsedUrl = url.parse(config.proxy.url);
             if (!parsedUrl.port) {
                 parsedUrl.port = 80;
             }
-            this._client.setProxy(parsedUrl);
+            const proxyParams = {
+                protocol: parsedUrl.protocol,
+                host: parsedUrl.hostname,
+                proxyAuth: parsedUrl.auth,
+                port: parsedUrl.port,
+            };
+            if (config.proxy.certs) {
+                Object.assign(proxyParams, config.proxy.certs);
+            }
+            this._client.setProxy(proxyParams);
         }
     }
 