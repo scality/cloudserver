1b619e9dfae5e4f26ca5160c41981332250d2cbd
philipyoo <philip.yoo@scality.com>
2018-04-19 15:08:44 -0700

    fix: append new authdata accounts
    Instead of overwriting existing accounts, append new
    accounts. This is needed to support file backend
    locally

--- tests/unit/Config.js
+++ tests/unit/Config.js
@@ -6,14 +6,32 @@ describe('Config', () => {
         done();
     });
 
-    it('should emit an event when auth data is updated', done => {
+    it('should emit an event when auth data is updated and append new ' +
+    'accounts to existing accounts', done => {
         const { ConfigObject } = require('../../lib/Config');
         const config = new ConfigObject();
+
+        const existingDataCount = config.authData.accounts.length;
         let emitted = false;
+
         config.on('authdata-update', () => {
+            const newCount = config.authData.accounts.length;
+            assert.strictEqual(existingDataCount + 1, newCount);
+
             emitted = true;
         });
-        config.setAuthDataAccounts([]);
+
+        const newAcct = {
+            name: 'Bart2',
+            email: 'sampleaccount1@sampling.com',
+            arn: 'arn:aws:iam::123456789016:root',
+            canonicalID:
+            '79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2bf',
+            shortid: '123456789016',
+            keys: [{ access: 'accessKey1', secret: 'verySecretKey1' }],
+        };
+        config.setAuthDataAccounts([newAcct]);
+
         if (emitted) {
             return done();
         }
