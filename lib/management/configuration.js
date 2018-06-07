djsharma/S3

ft: ZENKO-319 adds sproxyd location type management

lib/management/configuration.js
@@ -171,6 +171,21 @@ function patchConfiguration(instanceId, newConf, log, cb) {
                         };
                     }
                     break;
+                case 'location-scality-sproxyd-v1':
+                    location.type = 'scality';
+                    if (l.details && l.details.bootstrapList &&
+                        l.details.proxyPath) {
+                        locations.details = {
+                            connector: {
+                                sproxyd: {
+                                    chordCos: l.details.chordCos || null,
+                                    bootstrap: l.details.bootstrapList,
+                                    path: l.details.proxyPath,
+                                },
+                            },
+                        };
+                    }
+                    break;
                 default:
                     log.info('unknown location type', { locationType:
                         l.locationType });

scality/cloudserver

lib/management/configuration.js
@@ -159,6 +159,21 @@ function patchConfiguration(newConf, log, cb) {
                         };
                     }
                     break;
+                case 'location-scality-sproxyd-v1':
+                    location.type = 'scality';
+                    if (l.details && l.details.bootstrapList &&
+                        l.details.proxyPath) {
+                        location.details = {
+                            connector: {
+                                sproxyd: {
+                                    chordCos: l.details.chordCos || null,
+                                    bootstrap: l.details.bootstrapList,
+                                    path: l.details.proxyPath,
+                                },
+                            },
+                        };
+                    }
+                    break;
                 default:
                     log.info('unknown location type', { locationType:
                         l.locationType });