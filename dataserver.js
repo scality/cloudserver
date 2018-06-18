scality/cloudserver

commit 1fd51c8fe17c5bc95199539f240d205821581108

ft: ZENKO-141 allows MongoDB/metadata Scality/data
Allows for the case where both MongoDB and Scality backend is enabled
and therefore would not require dataserver to be started

dataserver.js
@@ -6,7 +6,9 @@ const logger = require('./lib/utilities/logger');
 
 if (config.backends.data === 'file' ||
     (config.backends.data === 'multiple' &&
-     config.backends.metadata !== 'scality')) {
+     config.backends.metadata !== 'scality') &&
+     (config.backends.auth !== 'scality' &&
+      config.backends.metadata !== 'mongodb')) {
     const dataServer = new arsenal.network.rest.RESTServer(
         { bindAddress: config.dataDaemon.bindAddress,
             port: config.dataDaemon.port,

commit 94a1e98e8eda47c2204add4378be28fba624e16b

bugfix: Fix data server regression
ZENKO-464 Fixes regression that prevented s3 from starting properly
and renames the start_mongo script to cloudserver for clear definition.
Regression introduced in commit 1fd51c8fe17c5bc95199539f240d205821581108

dataserver.js
@@ -6,9 +6,7 @@ const logger = require('./lib/utilities/logger');
 
 if (config.backends.data === 'file' ||
     (config.backends.data === 'multiple' &&
-     config.backends.metadata !== 'scality') &&
-     (config.backends.auth !== 'scality' &&
-      config.backends.metadata !== 'mongodb')) {
+     config.backends.metadata !== 'scality')) {
     const dataServer = new arsenal.network.rest.RESTServer(
         { bindAddress: config.dataDaemon.bindAddress,
             port: config.dataDaemon.port,