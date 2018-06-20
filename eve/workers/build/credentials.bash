b1c03f6fd7fdba011d584c9a4339c35cbe5d15f8
Salim <salim.salaues@scality.com>
2018-06-20 10:13:54 -0700

    feature: Proxy tests on Eve
    ZENKO-565 Runs ft_awssdk_external_backends tests behind squid proxy

--- eve/workers/build/credentials.bash
+++ eve/workers/build/credentials.bash
@@ -6,6 +6,20 @@ mkdir -p ~/.aws
 cat >>/root/.aws/exports <<EOF
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
+export b2backend_B2_ACCOUNT_ID="$b2backend_B2_ACCOUNT_ID"
+export b2backend_B2_STORAGE_ACCESS_KEY="$b2backend_B2_STORAGE_ACCESS_KEY"
+export b2backend_B2_STORAGE_ENDPOINT="$b2backend_B2_STORAGE_ENDPOINT"
+export gcpbackend2_GCP_SERVICE_EMAIL="$gcpbackend2_GCP_SERVICE_EMAIL"
+export gcpbackend2_GCP_SERVICE_KEY="$gcpbackend2_GCP_SERVICE_KEY"
+export gcpbackend2_GCP_SERVICE_KEYFILE="$gcpbackend2_GCP_SERVICE_KEYFILE"
+export gcpbackend_GCP_SERVICE_EMAIL="$gcpbackend_GCP_SERVICE_EMAIL"
+export gcpbackend_GCP_SERVICE_KEY="$gcpbackend_GCP_SERVICE_KEY"
+export gcpbackendmismatch_GCP_SERVICE_EMAIL="$gcpbackendmismatch_GCP_SERVICE_EMAIL"
+export gcpbackendmismatch_GCP_SERVICE_KEY="$gcpbackendmismatch_GCP_SERVICE_KEY"
+export gcpbackend_GCP_SERVICE_KEYFILE="$gcpbackend_GCP_SERVICE_KEYFILE"
+export gcpbackendmismatch_GCP_SERVICE_KEYFILE="$gcpbackendmismatch_GCP_SERVICE_KEYFILE"
+export gcpbackendnoproxy_GCP_SERVICE_KEYFILE="$gcpbackendnoproxy_GCP_SERVICE_KEYFILE"
+export gcpbackendproxy_GCP_SERVICE_KEYFILE="$gcpbackendproxy_GCP_SERVICE_KEYFILE"
 export GOOGLE_SERVICE_EMAIL="$GCP_BACKEND_SERVICE_EMAIL"
 export GOOGLE_SERVICE_KEY="$GCP_BACKEND_SERVICE_KEY"
 export azurebackend2_AZURE_STORAGE_ACCESS_KEY="$AZURE_BACKEND_ACCESS_KEY_2"
