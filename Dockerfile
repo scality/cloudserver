pugetc/S3

ft: ZENKO-448 Move to alpine based image

Dockerfile
@@ -1,19 +1,25 @@
-FROM node:6-slim
+FROM node:6-alpine
 MAINTAINER Giorgio Regni <gr@scality.com>
+
+RUN echo -e '#!/bin/sh\napk add --update "$@" && rm -rf /var/cache/apk/*' > /usr/bin/apk_add \
+    && chmod +x /usr/bin/apk_add
+
+ADD ./package.json ./package-lock.json /usr/src/app/
 
 WORKDIR /usr/src/app
 
-# Keep the .git directory in order to properly report version
-COPY . /usr/src/app
-
-RUN apt-get update \
-    && apt-get install -y jq python git build-essential --no-install-recommends \
+RUN apk_add jq bash\
+    && apk_add --virtual build-deps \
+       python \
+       build-base \
+       git \
     && npm install --production \
-    && apt-get autoremove --purge -y python git build-essential \
-    && rm -rf /var/lib/apt/lists/* \
+    && apk del build-deps \
     && npm cache clear \
     && rm -rf ~/.node-gyp \
     && rm -rf /tmp/npm-*
+
+ADD . /usr/src/app
 
 VOLUME ["/usr/src/app/localData","/usr/src/app/localMetadata"]
 

scality/cloudserver

Dockerfile
@@ -1,25 +1,28 @@
-FROM node:6-slim
+FROM node:6-alpine
 MAINTAINER Giorgio Regni <gr@scality.com>
+
+EXPOSE 8000
+
+COPY ./package.json ./package-lock.json /usr/src/app/
 
 WORKDIR /usr/src/app
 
-# Keep the .git directory in order to properly report version
-COPY ./package.json .
-
-RUN apt-get update \
-    && apt-get install -y jq python git build-essential --no-install-recommends \
+RUN apk add --update jq bash\
+    && apk add --virtual build-deps \
+                         python \
+                         build-base \
+                         git \
     && npm install --production \
-    && apt-get autoremove --purge -y python git build-essential \
-    && rm -rf /var/lib/apt/lists/* \
+    && apk del build-deps \
     && npm cache clear \
     && rm -rf ~/.node-gyp \
-    && rm -rf /tmp/npm-*
+    && rm -rf /tmp/npm-* \
+    && rm -rf /var/cache/apk/*
 
-COPY ./ ./
+COPY . /usr/src/app
 
 VOLUME ["/usr/src/app/localData","/usr/src/app/localMetadata"]
 
 ENTRYPOINT ["/usr/src/app/docker-entrypoint.sh"]
-CMD [ "npm", "start" ]
 
-EXPOSE 8000
+CMD [ "npm", "start" ]