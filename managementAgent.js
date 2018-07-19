007f742b92af991ee6f314c27c59b1433bbb6757
jeremyds <jeremydesanlis@yahoo.fr>
2018-07-19 08:40:30 -0700

    ft: ZENKO-713: management agent websocket server
    This commit creates a websocket server which adds a '/watch' route. The
    management agent registers a listener to the new configuration event and
    then sends it to its client.

--- managementAgent.js
+++ managementAgent.js
@@ -1,60 +1,162 @@
-const WebSocket = require('ws');
 const Uuid = require('uuid');
+const WebSocket = require('ws');
 
 const logger = require('./lib/utilities/logger');
 const { initManagement } = require('./lib/management');
-const { addOverlayMessageListener } = require('./lib/management/push');
 const _config = require('./lib/Config').config;
+const { managementAgentMessageType } = require('./lib/management/agentClient');
+const { addOverlayMessageListener } = require('./lib/management/push');
 
-process.env.REPORT_TOKEN = process.env.REPORT_TOKEN
-                           || _config.reportToken
-                           || Uuid.v4();
 
-function managementAgentWS() {
-    const port = _config.managementAgent.port || 8010;
-    const wss = new WebSocket.Server({
-        port,
-        clientTracking: true,
-        path: '/watch',
-    });
+// TODO: auth?
 
-    wss.on('connection', () => {
-        logger.info('management agent: client connected to watch route');
-    });
+const CHECK_BROKEN_CONNECTIONS_FREQUENCY_MS = 15000;
 
-    wss.on('listening', () => {
-        logger.info('management agent websocket server listening', { port });
-    });
 
-    wss.on('error', error => {
-        logger.error('management agent websocket server error', { error });
-    });
+class ManagementAgentServer {
+    constructor() {
+        this.port = _config.managementAgent.port || 8010;
+        this.wss = null;
+        this.loadedOverlay = null;
 
-    addOverlayMessageListener(remoteOverlay => {
-        wss.clients.forEach(client => {
+        this.stop = this.stop.bind(this);
+        process.on('SIGINT', this.stop);
+        process.on('SIGHUP', this.stop);
+        process.on('SIGQUIT', this.stop);
+        process.on('SIGTERM', this.stop);
+        process.on('SIGPIPE', () => {});
+    }
+
+    start(_cb) {
+        const cb = _cb || function noop() {};
+
+        /* Define REPORT_TOKEN env variable needed by the management
+         * module. */
+        process.env.REPORT_TOKEN = process.env.REPORT_TOKEN
+          || _config.reportToken
+          || Uuid.v4();
+
+        initManagement(logger.newRequestLogger(), overlay => {
+            let error = null;
+
+            if (overlay) {
+                this.loadedOverlay = overlay;
+                this.startServer();
+            } else {
+                error = new Error('failed to init management');
+            }
+            return cb(error);
+        });
+    }
+
+    stop() {
+        if (this.wss) {
+            this.wss.close(() => {
+                logger.info('server shutdown');
+            });
+        }
+    }
+
+    startServer() {
+        this.wss = new WebSocket.Server({
+            port: this.port,
+            clientTracking: true,
+            path: '/watch',
+        });
+
+        this.wss.on('connection', this.onConnection.bind(this));
+        this.wss.on('listening', this.onListening.bind(this));
+        this.wss.on('error', this.onError.bind(this));
+
+        setInterval(this.checkBrokenConnections.bind(this),
+                    CHECK_BROKEN_CONNECTIONS_FREQUENCY_MS);
+
+        addOverlayMessageListener(this.onNewOverlay.bind(this));
+    }
+
+    onConnection(socket, request) {
+        function hearthbeat() {
+            this.isAlive = true;
+        }
+        logger.info('client connected to watch route', {
+            ip: request.connection.remoteAddress,
+        });
+
+        /* eslint-disable no-param-reassign */
+        socket.isAlive = true;
+        socket.on('pong', hearthbeat.bind(socket));
+
+        if (socket.readyState !== socket.OPEN) {
+            logger.error('client socket not in ready state', {
+                state: socket.readyState,
+                client: socket._socket._peername,
+            });
+            return;
+        }
+
+        const msg = {
+            messageType: managementAgentMessageType.NEW_OVERLAY,
+            payload: this.loadedOverlay,
+        };
+        socket.send(JSON.stringify(msg), error => {
+            if (error) {
+                logger.error('failed to send remoteOverlay to client', {
+                    error,
+                    client: socket._socket._peername,
+                });
+            }
+        });
+    }
+
+    onListening() {
+        logger.info('websocket server listening',
+                    { port: this.port });
+    }
+
+    onError(error) {
+        logger.error('websocket server error', { error });
+    }
+
+    onNewOverlay(remoteOverlay) {
+        this.loadedOverlay = remoteOverlay;
+        this.wss.clients.forEach(client => {
             if (client.readyState !== client.OPEN) {
-                logger.warning('client socket not in ready state', { client });
+                logger.error('client socket not in ready state', {
+                    state: client.readyState,
+                    client: client._socket._peername,
+                });
                 return;
             }
-            logger.info('NEW OVERLAY');
             const msg = {
-                messageType: 'NEW_OVERLAY_VERSION',
+                messageType: managementAgentMessageType.NEW_OVERLAY,
                 payload: remoteOverlay,
             };
             client.send(JSON.stringify(msg), error => {
                 if (error) {
                     logger.error('failed to send remoteOverlay to management' +
-                                 ' agent client', { error, client });
+                                 ' agent client', {
+                                     error,
+                                     client: client._socket._peername,
+                                 });
                 }
             });
         });
-    });
+    }
+
+    checkBrokenConnections() {
+        this.wss.clients.forEach(client => {
+            if (!client.isAlive) {
+                logger.info('close broken connection', {
+                    client: client._socket._peername,
+                });
+                client.terminate();
+                return;
+            }
+            client.isAlive = false;
+            client.ping();
+        });
+    }
 }
 
-setTimeout(() => {
-    initManagement(logger.newRequestLogger());
-}, 5000);
-
-setTimeout(() => {
-    managementAgentWS();
-}, 6);
+const server = new ManagementAgentServer();
+server.start();
