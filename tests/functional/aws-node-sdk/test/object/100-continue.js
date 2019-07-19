6ac5122a8029c17720df17dbce041f1104cc2afb
anurag4dsb <anurag.mittal@scality.com>
2019-07-19 10:51:31 -0700

    added retuen in cb

--- tests/functional/aws-node-sdk/test/object/100-continue.js
+++ tests/functional/aws-node-sdk/test/object/100-continue.js
@@ -80,7 +80,9 @@ class ContinueRequestHandler {
             assert.strictEqual(req.socket.bytesWritten, expected);
             return cbOnce();
         });
-        req.on('error', err => cbOnce(err));
+        req.on('error', err => {
+            return cbOnce(err)
+        });
     }
 }
 
@@ -126,7 +128,7 @@ describe('PUT public object with 100-continue header', () => {
         it('should wait for continue event before sending body', done =>
             continueRequest.sendsBodyOnContinue(done));
 
-        it('should continue if a public user', done =>
+        it.only('should continue if a public user', done =>
             continueRequest.setRequestPath(invalidSignedURL)
                 .sendsBodyOnContinue(done));
     });
