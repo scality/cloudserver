b07c42c6469ebde6dc554338966f199737b0e1dd
naren-scality <narendran.rajendran@scality.com>
2019-09-27 13:59:18 -0700

    Removed tagging from unsupported queries list

--- constants.js
+++ constants.js
@@ -99,7 +99,6 @@ const constants = {
         'encryption',
         'publicAccessBlock',
         'policyStatus',
-        'tagging',
         'object-lock',
     ],
     // Headers supported by AWS that we do not currently support.
