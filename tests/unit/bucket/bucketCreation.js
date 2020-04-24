5a071cc181b1654aec137373ace490e1a8f661d0
Ilke <ilke.akcay@scality.com>
2020-04-24 11:04:09 -0700

    ft: S3C-2785 extend putBucket api for object lock

--- tests/unit/bucket/bucketCreation.js
+++ tests/unit/bucket/bucketCreation.js
@@ -70,3 +70,16 @@ describe('bucket creation', () => {
         });
     });
 });
+describe('bucket creation with object lock', () => {
+    it('should return 200 when creating a bucket with object lock', done => {
+        const bucketName = 'test-bucket-with-objectlock';
+        const headers = {
+            'x-amz-bucket-object-lock-enabled': true,
+        };
+        createBucket(authInfo, bucketName, headers,
+            normalBehaviorLocationConstraint, log, err => {
+                assert.ifError(err);
+                done();
+            });
+    });
+});
