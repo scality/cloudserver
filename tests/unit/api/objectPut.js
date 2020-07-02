57d346c36af8266b501776aa23a4252b10db58df
Ilke <ilke.akcay@scality.com>
2020-07-02 10:39:33 -0700

    bf: S3C-3121 should create bucket without object lock

--- tests/unit/api/objectPut.js
+++ tests/unit/api/objectPut.js
@@ -35,7 +35,7 @@ const testPutBucketRequestLock = new DummyRequest({
     namespace,
     headers: {
         'host': `${bucketName}.s3.amazonaws.com`,
-        'x-amz-bucket-object-lock-enabled': true,
+        'x-amz-bucket-object-lock-enabled': 'true',
     },
     url: '/',
 });
