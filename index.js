6d54936f1b967a9c97d2578d40daadfe2264e244
Rahul Padigela <rahul.padigela@scality.com>
2020-08-24 22:50:58 -0700

    improvement: S3C-2997 add timestamp for exceptions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict'; // eslint-disable-line strict
 
+/**
+ * Catch uncaught execeptions and add timestamp to aid debugging
+ */
+process.on('uncaughtException', err => {
+    process.stderr.write(`${new Date().toISOString()} ${err.stack}`);
+});
 require('./lib/server.js')();
