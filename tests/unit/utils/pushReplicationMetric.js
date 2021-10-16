c7975c6aacb76eddcfc31deac5fd9354ae3f3db6
Jonathan Gramain <jonathan.gramain@scality.com>
2021-10-15 17:04:13 -0700

    bf(CLDSRV-37): Push metrics for backbeat routes

--- tests/unit/utils/pushReplicationMetric.js
+++ tests/unit/utils/pushReplicationMetric.js
@@ -1,48 +1,113 @@
 const assert = require('assert');
 const { ObjectMD } = require('arsenal').models;
 
-const { shouldPushMetric } =
+const { getMetricToPush } =
     require('../../../lib/routes/utilities/pushReplicationMetric');
 
-describe('shouldPushMetric', () => {
+describe('getMetricToPush', () => {
     it('should push metrics when putting a new replica version', () => {
-        const prevObjectMD = undefined;
+        const prevObjectMD = new ObjectMD()
+            .setVersionId('1');
         const objectMD = new ObjectMD()
+            .setVersionId('2')
             .setReplicationStatus('REPLICA');
-        const result = shouldPushMetric(prevObjectMD, objectMD);
-        assert.strictEqual(result, true);
-    });
-
-    it('should push metrics when putting a new replica version with public ACL',
-    () => {
-        const prevObjectMD = undefined;
-        const objectMD = new ObjectMD()
-              .setReplicationStatus('REPLICA');
-        const publicACL = objectMD.getAcl();
-        publicACL.Canned = 'public-read';
-        objectMD.setAcl(publicACL);
-        const result = shouldPushMetric(prevObjectMD, objectMD);
-        assert.strictEqual(result, true);
+        const result = getMetricToPush(prevObjectMD, objectMD);
+        assert.strictEqual(result, 'replicateObject');
     });
 
     it('should not push metrics for non-replica operations', () => {
-        const prevObjectMD = undefined;
+        const prevObjectMD = new ObjectMD();
         const objectMD = new ObjectMD()
             .setReplicationStatus('COMPLETED');
-        const result = shouldPushMetric(prevObjectMD, objectMD);
-        assert.strictEqual(result, false);
+        const result = getMetricToPush(prevObjectMD, objectMD);
+        assert.strictEqual(result, null);
     });
 
-    it('should not push metrics when updating replica in-place (ACL/Tagging)',
+    it('should push metrics for replica operations with tagging', () => {
+        const prevObjectMD = new ObjectMD()
+            .setVersionId('1');
+        const objectMD = new ObjectMD()
+            .setVersionId('1')
+            .setReplicationStatus('REPLICA')
+            .setTags({ 'object-tag-key': 'object-tag-value' });
+        const result = getMetricToPush(prevObjectMD, objectMD);
+        assert.strictEqual(result, 'replicateTags');
+    });
+
+    it('should push metrics for replica operations when deleting tagging',
+    () => {
+        const prevObjectMD = new ObjectMD()
+            .setTags({ 'object-tag-key': 'object-tag-value' });
+        const objectMD = new ObjectMD().setReplicationStatus('REPLICA');
+        const result = getMetricToPush(prevObjectMD, objectMD);
+        assert.strictEqual(result, 'replicateTags');
+    });
+
+    it('should not push metrics for replica operations with tagging ' +
+        'if tags are equal',
+        () => {
+            const prevObjectMD = new ObjectMD()
+                .setVersionId('1')
+                .setTags({ 'object-tag-key': 'object-tag-value' });
+            const objectMD = new ObjectMD()
+                .setVersionId('1')
+                .setReplicationStatus('REPLICA')
+                .setTags({ 'object-tag-key': 'object-tag-value' });
+            const result = getMetricToPush(prevObjectMD, objectMD);
+            assert.strictEqual(result, null);
+        }
+    );
+
+    it('should push metrics for replica operations with acl', () => {
+        const prevObjectMD = new ObjectMD()
+            .setVersionId('1');
+        const objectMD = new ObjectMD();
+        const publicACL = objectMD.getAcl();
+        publicACL.Canned = 'public-read';
+        objectMD
+            .setReplicationStatus('REPLICA')
+            .setAcl(publicACL)
+            .setVersionId('1');
+        const result = getMetricToPush(prevObjectMD, objectMD);
+        assert.strictEqual(result, 'replicateTags');
+    });
+
+
+    it('should push metrics for replica operations when resetting acl',
     () => {
         const prevObjectMD = new ObjectMD();
         const publicACL = prevObjectMD.getAcl();
         publicACL.Canned = 'public-read';
-        const objectMD = new ObjectMD()
+        prevObjectMD
             .setReplicationStatus('REPLICA')
-            .setTags({ 'object-tag-key': 'object-tag-value' })
-            .setAcl(publicACL);
-        const result = shouldPushMetric(prevObjectMD, objectMD);
-        assert.strictEqual(result, false);
+            .setAcl(publicACL)
+            .setVersionId('1');
+        const objectMD = new ObjectMD();
+        const privateACL = objectMD.getAcl();
+        privateACL.Canned = 'private';
+        objectMD
+            .setReplicationStatus('REPLICA')
+            .setAcl(privateACL)
+            .setVersionId('1');
+        const result = getMetricToPush(prevObjectMD, objectMD);
+        assert.strictEqual(result, 'replicateTags');
     });
+
+    it('should not push metrics for replica operations with acl ' +
+        'when they are equal',
+        () => {
+            const objectMD = new ObjectMD();
+            const publicACL = objectMD.getAcl();
+            publicACL.Canned = 'public-read';
+            objectMD
+                .setReplicationStatus('REPLICA')
+                .setAcl(publicACL)
+                .setVersionId('1');
+            const prevObjectMD = new ObjectMD()
+                .setAcl(publicACL)
+                .setVersionId('1');
+            const result = getMetricToPush(prevObjectMD, objectMD);
+            assert.strictEqual(result, null);
+        }
+    );
 });
