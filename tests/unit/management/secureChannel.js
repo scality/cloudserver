501fbda5c248d4dcc69467b7a85617fd6fa21c50
Francois Ferrand <francois.ferrand@scality.com>
2021-10-22 14:30:49 +0200

    Allow configuring CLDSRV-41 capability
    Issue: CLDSRV-41
    Issue: ARTESCA-2190

--- tests/unit/management/secureChannel.js
+++ tests/unit/management/secureChannel.js
@@ -11,9 +11,21 @@ describe('report handler', () => {
         assert.strictEqual(c.locationTypeS3Custom, true);
         assert.strictEqual(c.locationTypeSproxyd, true);
         assert.strictEqual(c.locationTypeHyperdriveV2, true);
+        assert.strictEqual(c.locationTypeLocal, true);
         assert.strictEqual(c.preferredReadLocation, true);
         assert.strictEqual(c.managedLifecycle, true);
         assert.strictEqual(c.secureChannelOptimizedPath, true);
         assert.strictEqual(c.s3cIngestLocation, true);
+    });
+    it('should allow configure local file system capability', () => {
+        const OLD_ENV = process.env;
+
+        process.env.LOCAL_VOLUME_CAPABILITY = 'true';
+        assert.strictEqual(getCapabilities().locationTypeLocal, true);
+
+        process.env.LOCAL_VOLUME_CAPABILITY = 'false';
+        assert.strictEqual(getCapabilities().locationTypeLocal, false);
+
+        process.env = OLD_ENV;
     });
 });
