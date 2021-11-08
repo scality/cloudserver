55a3d5ecba382038a2ae0a26d2bf5059f6e592ed
Taylor McKinnon <mail@tmacs.space>
2021-11-08 15:59:50 -0800

    ft(CLDSRV-55): Add config loading for utapi v2 event filter

--- tests/utapi/awsNodeSdk.js
+++ tests/utapi/awsNodeSdk.js
@@ -471,4 +471,26 @@ describe('utapi v2 metrics incoming and outgoing bytes', function t() {
             next => deleteBucket(bucket, next),
         ], done);
     });
+
+    it('should not push a metric for a filtered bucket', done => {
+        const bucket = 'utapi-event-filter-deny-bucket';
+        const objectSize = 1024 * 1024;
+        const objSize = objectSize * 2;
+        const key = '1.txt';
+        async.series([
+            next => createBucket(bucket, next),
+            next => putObject(bucket, key, objSize, next),
+            next => wait(WAIT_MS, next),
+            next => wait(WAIT_MS, () => {
+                checkMetrics(0, 0, 0);
+                next();
+            }),
+            next => deleteObject(bucket, key, next),
+            next => wait(WAIT_MS, () => {
+                checkMetrics(0, 0, 0);
+                next();
+            }),
+            next => deleteBucket(bucket, next),
+        ], done);
+    });
 });
