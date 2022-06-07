63a790a3d76ac5935f1346be601ca0c7f81fddf5
KillianG <killian.gardahaut@scality.com>
2022-06-07 13:54:50 +0200

    Revert "Merge remote-tracking branch 'origin/improvement/CLDSRV-167-put-bucket-tagging' into w/8.4/improvement/CLDSRV-167-put-bucket-tagging"
    This reverts commit 518253dc5e76a723a1340a06591e21c963c457bd, reversing
    changes made to ca91044fde783b1ea5102edddeb4efa7d0fd020f.

--- constants.js
+++ constants.js
@@ -183,6 +183,7 @@ const constants = {
     // Bucket specific queries supported by AWS that we do not currently support
     // these queries may or may not be supported at object level
     unsupportedBucketQueries: [
+        'tagging',
     ],
     suppressedUtapiEventFields: [
         'object',
