e08aaa7bcc8177f53606ea6f7c200983297e709a
Nicolas Humbert <nicolas.humbert@scality.com>
2023-07-11 16:43:00 -0400

    CLDSRV-412 Test null version in Lifecycle list of non-current versions
    (cherry picked from commit 98f855f997c3e6fdd2ad78bd654b92b2d9c3cfaa)

--- tests/functional/backbeat/listDeleteMarker.js
+++ tests/functional/backbeat/listDeleteMarker.js
@@ -4,17 +4,18 @@ const BucketUtility = require('../aws-node-sdk/lib/utility/bucket-util');
 const { removeAllVersions } = require('../aws-node-sdk/lib/utility/versioning-util');
 const { makeBackbeatRequest, runIfMongoV1 } = require('./utils');
 
-const testBucket = 'bucket-for-list-lifecycle-null-tests';
-
 const credentials = {
     accessKey: 'accessKey1',
     secretKey: 'verySecretKey1',
 };
 
-runIfMongoV1('listLifecycle if null version', () => {
+runIfMongoV1('listLifecycle with non-current delete marker', () => {
     let bucketUtil;
     let s3;
-    let versionForKey2;
+    let expectedVersionId;
+    let expectedDMVersionId;
+    const testBucket = 'bucket-for-list-lifecycle-noncurrent-dm-tests';
+    const keyName = 'key0';
 
     before(done => {
         bucketUtil = new BucketUtility('account1', { signatureVersion: 'v4' });
@@ -22,24 +23,22 @@ runIfMongoV1('listLifecycle if null version', () => {
 
         return async.series([
             next => s3.createBucket({ Bucket: testBucket }, next),
-            next => s3.putObject({ Bucket: testBucket, Key: 'key1', Body: '123' }, next),
-            next => s3.putObject({ Bucket: testBucket, Key: 'key2', Body: '123' }, next),
             next => s3.putBucketVersioning({
                 Bucket: testBucket,
                 VersioningConfiguration: { Status: 'Enabled' },
             }, next),
-            next => s3.putObject({ Bucket: testBucket, Key: 'key1', Body: '123' }, (err, data) => {
+            next => s3.deleteObject({ Bucket: testBucket, Key: keyName }, (err, data) => {
                 if (err) {
                     return next(err);
                 }
-                // delete version to create a null current version for key1.
-                return s3.deleteObject({ Bucket: testBucket, Key: 'key1', VersionId: data.VersionId }, next);
+                expectedDMVersionId = data.VersionId;
+                return next();
             }),
-            next => s3.putObject({ Bucket: testBucket, Key: 'key2', Body: '123' }, (err, data) => {
+            next => s3.putObject({ Bucket: testBucket, Key: keyName }, (err, data) => {
                 if (err) {
                     return next(err);
                 }
-                versionForKey2 = data.VersionId;
+                expectedVersionId = data.VersionId;
                 return next();
             }),
         ], done);
@@ -50,100 +49,10 @@ runIfMongoV1('listLifecycle if null version', () => {
         next => s3.deleteBucket({ Bucket: testBucket }, next),
     ], done));
 
-    it('should return the null noncurrent versions', done => {
+    it('should return the current version', done => {
         makeBackbeatRequest({
             method: 'GET',
             bucket: testBucket,
-            queryObj: { 'list-type': 'noncurrent' },
-            authCredentials: credentials,
-        }, (err, response) => {
-            assert.ifError(err);
-            assert.strictEqual(response.statusCode, 200);
-            const data = JSON.parse(response.body);
-
-            assert.strictEqual(data.IsTruncated, false);
-            assert(!data.NextKeyMarker);
-            assert.strictEqual(data.MaxKeys, 1000);
-
-            const contents = data.Contents;
-            assert.strictEqual(contents.length, 1);
-            assert.strictEqual(contents[0].Key, 'key2');
-            assert.strictEqual(contents[0].VersionId, 'null');
-            return done();
-        });
-    });
-
-    it('should return the null current versions', done => {
-        makeBackbeatRequest({
-            method: 'GET',
-            bucket: testBucket,
-            queryObj: { 'list-type': 'current' },
-            authCredentials: credentials,
-        }, (err, response) => {
-            assert.ifError(err);
-            assert.strictEqual(response.statusCode, 200);
-            const data = JSON.parse(response.body);
-
-            assert.strictEqual(data.IsTruncated, false);
-            assert(!data.NextKeyMarker);
-            assert.strictEqual(data.MaxKeys, 1000);
-
-            const contents = data.Contents;
-            assert.strictEqual(contents.length, 2);
-
-            const firstKey = contents[0];
-            assert.strictEqual(firstKey.Key, 'key1');
-            assert.strictEqual(firstKey.VersionId, 'null');
-
-            const secondKey = contents[1];
-            assert.strictEqual(secondKey.Key, 'key2');
-            assert.strictEqual(secondKey.VersionId, versionForKey2);
-            return done();
-        });
-    });
-});
-
-runIfMongoV1('listLifecycle with null current version after versioning suspended', () => {
-    let bucketUtil;
-    let s3;
-    let expectedVersionId;
-    const nullObjectBucket = 'bucket-for-list-lifecycle-current-null-tests';
-    const keyName = 'key0';
-
-    before(done => {
-        bucketUtil = new BucketUtility('account1', { signatureVersion: 'v4' });
-        s3 = bucketUtil.s3;
-
-        return async.series([
-            next => s3.createBucket({ Bucket: nullObjectBucket }, next),
-            next => s3.putBucketVersioning({
-                Bucket: nullObjectBucket,
-                VersioningConfiguration: { Status: 'Enabled' },
-            }, next),
-            next => s3.putObject({ Bucket: nullObjectBucket, Key: keyName }, (err, data) => {
-                if (err) {
-                    return next(err);
-                }
-                expectedVersionId = data.VersionId;
-                return next();
-            }),
-            next => s3.putBucketVersioning({
-                Bucket: nullObjectBucket,
-                VersioningConfiguration: { Status: 'Suspended' },
-            }, next),
-            next => s3.putObject({ Bucket: nullObjectBucket, Key: keyName }, next),
-        ], done);
-    });
-
-    after(done => async.series([
-        next => removeAllVersions({ Bucket: nullObjectBucket }, next),
-        next => s3.deleteBucket({ Bucket: nullObjectBucket }, next),
-    ], done));
-
-    it('should return list of current versions when bucket has a null current version', done => {
-        makeBackbeatRequest({
-            method: 'GET',
-            bucket: nullObjectBucket,
             queryObj: { 'list-type': 'current' },
             authCredentials: credentials,
         }, (err, response) => {
@@ -157,15 +66,109 @@ runIfMongoV1('listLifecycle with null current version after versioning suspended
             assert.strictEqual(data.Contents.length, 1);
             const key = data.Contents[0];
             assert.strictEqual(key.Key, keyName);
-            assert.strictEqual(key.VersionId, 'null');
+            assert.strictEqual(key.VersionId, expectedVersionId);
             return done();
         });
     });
 
-    it('should return list of non-current versions when bucket has a null current version', done => {
+    it('should return the non-current delete marker', done => {
         makeBackbeatRequest({
             method: 'GET',
-            bucket: nullObjectBucket,
+            bucket: testBucket,
+            queryObj: { 'list-type': 'noncurrent' },
+            authCredentials: credentials,
+        }, (err, response) => {
+            assert.ifError(err);
+            assert.strictEqual(response.statusCode, 200);
+            const data = JSON.parse(response.body);
+
+            assert.strictEqual(data.IsTruncated, false);
+            assert(!data.NextKeyMarker);
+            assert.strictEqual(data.MaxKeys, 1000);
+            assert.strictEqual(data.Contents.length, 1);
+            const key = data.Contents[0];
+            assert.strictEqual(key.Key, keyName);
+            assert.strictEqual(key.VersionId, expectedDMVersionId);
+            return done();
+        });
+    });
+
+    it('should return no orphan delete marker', done => {
+        makeBackbeatRequest({
+            method: 'GET',
+            bucket: testBucket,
+            queryObj: { 'list-type': 'orphan' },
+            authCredentials: credentials,
+        }, (err, response) => {
+            assert.ifError(err);
+            assert.strictEqual(response.statusCode, 200);
+            const data = JSON.parse(response.body);
+
+            assert.strictEqual(data.IsTruncated, false);
+            assert(!data.NextKeyMarker);
+            assert.strictEqual(data.MaxKeys, 1000);
+            assert.strictEqual(data.Contents.length, 0);
+            return done();
+        });
+    });
+});
+
+runIfMongoV1('listLifecycle with current delete marker version', () => {
+    let bucketUtil;
+    let s3;
+    let expectedVersionId;
+    const testBucket = 'bucket-for-list-lifecycle-current-dm-tests';
+    const keyName = 'key0';
+
+    before(done => {
+        bucketUtil = new BucketUtility('account1', { signatureVersion: 'v4' });
+        s3 = bucketUtil.s3;
+
+        return async.series([
+            next => s3.createBucket({ Bucket: testBucket }, next),
+            next => s3.putBucketVersioning({
+                Bucket: testBucket,
+                VersioningConfiguration: { Status: 'Enabled' },
+            }, next),
+            next => s3.putObject({ Bucket: testBucket, Key: keyName }, (err, data) => {
+                if (err) {
+                    return next(err);
+                }
+                expectedVersionId = data.VersionId;
+                return next();
+            }),
+            next => s3.deleteObject({ Bucket: testBucket, Key: keyName }, next),
+        ], done);
+    });
+
+    after(done => async.series([
+        next => removeAllVersions({ Bucket: testBucket }, next),
+        next => s3.deleteBucket({ Bucket: testBucket }, next),
+    ], done));
+
+    it('should return no current object if current version is a delete marker', done => {
+        makeBackbeatRequest({
+            method: 'GET',
+            bucket: testBucket,
+            queryObj: { 'list-type': 'current' },
+            authCredentials: credentials,
+        }, (err, response) => {
+            assert.ifError(err);
+            assert.strictEqual(response.statusCode, 200);
+            const data = JSON.parse(response.body);
+
+            assert.strictEqual(data.IsTruncated, false);
+            assert(!data.NextKeyMarker);
+            assert.strictEqual(data.MaxKeys, 1000);
+            assert.strictEqual(data.Contents.length, 0);
+            return done();
+        });
+    });
+
+    it('should return the non-current version', done => {
+        makeBackbeatRequest({
+            method: 'GET',
+            bucket: testBucket,
             queryObj: { 'list-type': 'noncurrent' },
             authCredentials: credentials,
         }, (err, response) => {
@@ -180,6 +183,25 @@ runIfMongoV1('listLifecycle with null current version after versioning suspended
             const key = data.Contents[0];
             assert.strictEqual(key.Key, keyName);
             assert.strictEqual(key.VersionId, expectedVersionId);
+            return done();
+        });
+    });
+
+    it('should return no orphan delete marker', done => {
+        makeBackbeatRequest({
+            method: 'GET',
+            bucket: testBucket,
+            queryObj: { 'list-type': 'orphan' },
+            authCredentials: credentials,
+        }, (err, response) => {
+            assert.ifError(err);
+            assert.strictEqual(response.statusCode, 200);
+            const data = JSON.parse(response.body);
+
+            assert.strictEqual(data.IsTruncated, false);
+            assert(!data.NextKeyMarker);
+            assert.strictEqual(data.MaxKeys, 1000);
+            assert.strictEqual(data.Contents.length, 0);
             return done();
         });
     });
