2f945599ca219571653f6ca604d7c1283aab21eb
Nicolas Humbert <nicolas.humbert@scality.com>
2024-01-25 13:50:15 +0100

    CLDSRV-498 Handling isNull master version with no versionId
    In certain cases, a master version may not have a versionId and be set as null (isNull:true). For instance, this occurs when a customer:
    
    Create a bucket.
    
    Put an object to it.
    
    Put bucket versioning.
    
    Put metadata (BackbeatClient.putMetadata), which results in the master version being set to null (isNull:true) with no versionId.
    
    Currently, if an object is put after these steps, CloudServer fails to appropriately generate a null version. This is because CloudServer doesn't handle situations where the master version is set to isNull:true with no versionId.
    
    The correct approach when an object is put should be to:
    
    Create the new version key.
    
    Create a new null version key, assigning it a “default non-version version id”.
    
    Update this “default non-version version id” to the `nullVersionId` field of the master key.

--- tests/functional/raw-node/test/routes/routeBackbeatForReplication.js
+++ tests/functional/raw-node/test/routes/routeBackbeatForReplication.js
@@ -172,4 +172,69 @@ describeSkipIfAWS('backbeat routes for replication', () => {
             return done();
         });
     });
+
+    it('should successfully put object after replicating a null version', done => {
+        let objMD;
+        let expectedVersionId;
+        return async.series([
+            next => s3.putObject({ Bucket: bucketSource, Key: keyName, Body: new Buffer(testData) }, next),
+            next => s3.putBucketVersioning({ Bucket: bucketSource, VersioningConfiguration: { Status: 'Enabled' } },
+                next),
+            next => s3.putBucketVersioning({ Bucket: bucketDestination, VersioningConfiguration:
+                { Status: 'Enabled' } }, next),
+            next => makeBackbeatRequest({
+                method: 'GET',
+                resourceType: 'metadata',
+                bucket: bucketSource,
+                objectKey: keyName,
+                queryObj: {
+                    versionId: 'null',
+                },
+                authCredentials: backbeatAuthCredentials,
+            }, (err, data) => {
+                if (err) {
+                    return next(err);
+                }
+                objMD = JSON.parse(data.body).Body;
+                return next();
+            }),
+            next => makeBackbeatRequest({
+                method: 'PUT',
+                resourceType: 'metadata',
+                bucket: bucketDestination,
+                objectKey: keyName,
+                queryObj: {
+                    versionId: 'null',
+                },
+                authCredentials: backbeatAuthCredentials,
+                requestBody: objMD,
+            }, next),
+            next => s3.putObject({ Bucket: bucketDestination, Key: keyName, Body: new Buffer(testData) },
+            (err, data) => {
+                if (err) {
+                    return next(err);
+                }
+                expectedVersionId = data.VersionId;
+                return next();
+            }),
+            next => s3.headObject({ Bucket: bucketDestination, Key: keyName, VersionId: 'null' }, next),
+            next => s3.listObjectVersions({ Bucket: bucketDestination }, next),
+        ], (err, data) => {
+            if (err) {
+                return done(err);
+            }
+            const headObjectRes = data[6];
+            assert.strictEqual(headObjectRes.VersionId, 'null');
+
+            const listObjectVersionsRes = data[7];
+            const { Versions } = listObjectVersionsRes;
+
+            assert.strictEqual(Versions.length, 2);
+
+            const [currentVersion, nonCurrentVersion] = Versions;
+            assert.strictEqual(currentVersion.VersionId, expectedVersionId);
+            assert.strictEqual(nonCurrentVersion.VersionId, 'null');
+            return done();
+        });
+    });
 });
