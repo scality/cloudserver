6c5b5a0bf54322d55ec9518445174676c568958e
Maha Benzekri <maha.benzekri@scality.com>
2024-03-31 15:00:09 +0200

    wip

--- tests/functional/aws-node-sdk/test/bucket/getBucketQuota.js
+++ tests/functional/aws-node-sdk/test/bucket/getBucketQuota.js
@@ -10,9 +10,7 @@ describe('aws-sdk test get bucket quota', () => {
 
     before(() => {
         const config = getConfig('default', { signatureVersion: 'v4' });
-        console.log(config);
         s3 = new S3(config);
-        console.log(s3);
     });
 
     beforeEach(done => s3.createBucket({ Bucket: bucket }, done));
@@ -20,20 +18,19 @@ describe('aws-sdk test get bucket quota', () => {
     afterEach(done => s3.deleteBucket({ Bucket: bucket }, done));
 
     it('should return the Quota', done => {
-        const quota = {"quota":1000};
+        const quota = { quota: 1000 };
         async.series([
             next => s3.updateBucketQuota({
                 AccountId: s3.AccountId,
-                quota: quota,
+                quota,
                 Bucket: bucket
             }, next),
             next => s3.bucketGetQuota({
                 AccountId: s3.AccountId,
                 Bucket: bucket,
-
             }, next),
         ], (err, data) => {
-            console.log(data);
+            assert.ifError(err);
             assert.deepStrictEqual(data, quota);
             done();
         });
