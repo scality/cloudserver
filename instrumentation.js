8ff87942871d3b3d8a747a488dec4bfd8ac1ef9a
Anurag Mittal <anurag.mittal@scality.com>
2024-06-05 02:01:13 +0200

    S3C-8896: Added based Otel instrumentation

---
+++ instrumentation.js
@@ -0,0 +1,25 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} = require('@opentelemetry/sdk-metrics');
+
+const sdk = new NodeSDK({
+    traceExporter: new ConsoleSpanExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new ConsoleMetricExporter(),
+    }),
+    instrumentations: [getNodeAutoInstrumentations({
+        // disabling fs automatic instrumentation because
+        // it can be noisy and expensive during startup
+        '@opentelemetry/instrumentation-fs': {
+            enabled: false,
+        },
+    })],
+});
+
+sdk.start();
