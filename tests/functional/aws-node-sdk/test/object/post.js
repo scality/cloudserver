77a4fa03344b71ae8f6d6cc27b7ffa2b99f20a39
Will Toozs <william.toozshobson@scality.com>
2024-06-12 16:15:24 +0200

    first functional tests are fucntional

--- tests/functional/aws-node-sdk/test/object/post.js
+++ tests/functional/aws-node-sdk/test/object/post.js
@@ -1,126 +1,311 @@
+const AWS = require('aws-sdk');
+
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const FormData = require('form-data');
 const assert = require('assert');
 
-const withV4 = require('../support/withV4');
-const BucketUtility = require('../../lib/utility/bucket-util');
-const provideRawOutput = require('../../lib/utility/provideRawOutput');
-const { taggingTests, generateMultipleTagQuery }
-    = require('../../lib/utility/tagging');
-const genMaxSizeMetaHeaders
-    = require('../../lib/utility/genMaxSizeMetaHeaders');
-const changeObjectLock = require('../../../../utilities/objectLock-util');
+// const filename = 'test-file.txt';
+// const bucketName = 'your-bucket-name';
+// const url = `http://localhost:8000/${bucketName}/`;
 
-const bucket = 'bucket2putstuffin4324242';
-const object = 'object2putstuffin';
+const generateBucketName = () => `test-bucket-${crypto.randomBytes(8).toString('hex')}`;
+let bucketName;
+const filename = 'test-file.txt';
 
-function _checkError(err, code, statusCode) {
-    assert(err, 'Expected error but found none');
-    assert.strictEqual(err.code, code);
-    assert.strictEqual(err.statusCode, statusCode);
-}
+let url;
+const s3 = new AWS.S3();
 
-describe('PUT object', () => {
-    withV4(sigCfg => {
-        let bucketUtil;
-        let s3;
+const calculateFields = (additionalConditions) => {
+    const ak = process.env.AWS_ACCESS_KEY_ID;
+    const sk = process.env.AWS_SECRET_ACCESS_KEY;
+    const region = 'us-east-1';
+    const service = 's3';
 
-        beforeEach(() => {
-            bucketUtil = new BucketUtility('default', sigCfg);
-            s3 = bucketUtil.s3;
-            return s3.createBucket({ Bucket: bucket }).promise()
-            .catch(err => {
-                process.stdout.write(`Error creating bucket: ${err}\n`);
-                throw err;
+    const now = new Date();
+    const formattedDate = now.toISOString().replace(/[:-]|\.\d{3}/g, '');
+    const formatDate = (date) => {
+        const year = date.getUTCFullYear();
+        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+        const day = date.getUTCDate().toString().padStart(2, '0');
+        return `${year}${month}${day}`;
+    };
+    const shortFormattedDate = formatDate(now);
+
+    const credential = `${ak}/${shortFormattedDate}/${region}/${service}/aws4_request`;
+    const conditionsFields = [
+        { bucket: bucketName },
+        { key: filename },
+        { 'x-amz-credential': credential },
+        { 'x-amz-algorithm': 'AWS4-HMAC-SHA256' },
+        { 'x-amz-date': formattedDate },
+    ];
+    if (additionalConditions) {
+        additionalConditions.forEach(field => {
+            const key = Object.keys(field)[0];
+            const value = field[key];
+            conditionsFields.push({ [key]: value });
+        });
+    }
+    const policy = {
+        expiration: new Date(new Date().getTime() + 60000).toISOString(),
+        conditions: conditionsFields,
+    };
+    const policyBase64 = Buffer.from(JSON.stringify(policy)).toString('base64');
+
+    const getSignatureKey = (key, dateStamp, regionName, serviceName) => {
+        const kDate = crypto.createHmac('sha256', `AWS4${key}`).update(dateStamp).digest();
+        const kRegion = crypto.createHmac('sha256', kDate).update(regionName).digest();
+        const kService = crypto.createHmac('sha256', kRegion).update(serviceName).digest();
+        const kSigning = crypto.createHmac('sha256', kService).update('aws4_request').digest();
+        return kSigning;
+    };
+    const signingKey = getSignatureKey(sk, shortFormattedDate, region, service);
+    const signature = crypto.createHmac('sha256', signingKey).update(policyBase64).digest('hex');
+
+    const returnFields = [
+        { name: 'X-Amz-Credential', value: credential },
+        { name: 'X-Amz-Algorithm', value: 'AWS4-HMAC-SHA256' },
+        { name: 'X-Amz-Signature', value: signature },
+        { name: 'X-Amz-Date', value: formattedDate },
+        { name: 'Policy', value: policyBase64 },
+        { name: 'bucket', value: bucketName },
+        { name: 'key', value: filename },
+    ];
+    if (!additionalConditions) {
+        return returnFields;
+    }
+    if (additionalConditions) {
+        additionalConditions.forEach(field => {
+            const key = Object.keys(field)[0];
+            const value = field[key];
+            returnFields.push({ name: key, value: value });
+        });
+    }
+
+    return returnFields;
+};
+
+describe('AWS S3 POST Object with Policy', function () {
+    this.timeout(10000); // Increase timeout for potentially slow operations
+
+    beforeEach(done => {
+        bucketName = generateBucketName();
+        url = `https://${bucketName}.s3.amazonaws.com/`;
+        const filePath = path.join(__dirname, 'test-file.txt');
+        const fileContent = 'This is a test file';
+
+        fs.writeFile(filePath, fileContent, err => {
+            if (err) {
+                return done(err);
+            }
+
+            // Create the bucket
+            s3.createBucket({ Bucket: bucketName }, (err) => {
+                if (err) {
+                    return done(err);
+                }
+                done();
             });
         });
+    });
 
-        afterEach(() => {
-            process.stdout.write('Emptying bucket');
-            return bucketUtil.empty(bucket)
-            .then(() => {
-                process.stdout.write('Deleting bucket');
-                return bucketUtil.deleteOne(bucket);
-            })
-            .catch(err => {
-                process.stdout.write('Error in afterEach');
-                throw err;
-            });
-        });
+    afterEach(done => {
+        const filePath = path.join(__dirname, 'test-file.txt');
 
-        it('should put an object and set the acl via query param',
-            done => {
-                const params = { Bucket: bucket, Key: 'key',
-                    ACL: 'public-read', StorageClass: 'STANDARD' };
-                const url = s3.getSignedUrl('putObject', params);
-                provideRawOutput(['-verbose', '-X', 'PUT', url,
-                    '--upload-file', 'uploadFile'], httpCode => {
-                    assert.strictEqual(httpCode, '200 OK');
-                    s3.getObjectAcl({ Bucket: bucket, Key: 'key' },
-                    (err, result) => {
-                        assert.equal(err, null, 'Expected success, ' +
-                        `got error ${JSON.stringify(err)}`);
-                        assert.deepStrictEqual(result.Grants[1], { Grantee:
-                        { Type: 'Group', URI:
-                            'http://acs.amazonaws.com/groups/global/AllUsers',
-                        }, Permission: 'READ' });
-                        done();
-                    });
-                });
-            });
+        // Delete the file
+        fs.unlink(filePath, err => {
+            if (err) {
+                return done(err);
+            }
 
-        it('should put an object with key slash',
-            done => {
-                const params = { Bucket: bucket, Key: '/' };
-                s3.putObject(params, err => {
-                    assert.equal(err, null, 'Expected success, ' +
-                    `got error ${JSON.stringify(err)}`);
+            // Delete the bucket and its contents
+            const deleteBucket = () => {
+                s3.deleteBucket({ Bucket: bucketName }, (err) => {
+                    if (err && err.code !== 'NoSuchBucket') {
+                        return done(err);
+                    }
                     done();
                 });
-            });
+            };
 
-        it('should return error if putting object w/ > 2KB user-defined md',
-            done => {
-                const metadata = genMaxSizeMetaHeaders();
-                const params = { Bucket: bucket, Key: '/', Metadata: metadata };
-                s3.putObject(params, err => {
-                    assert.strictEqual(err, null, `Unexpected err: ${err}`);
-                    // add one more byte to be over the limit
-                    metadata.header0 = `${metadata.header0}${'0'}`;
-                    s3.putObject(params, err => {
-                        assert(err, 'Expected err but did not find one');
-                        assert.strictEqual(err.code, 'MetadataTooLarge');
-                        assert.strictEqual(err.statusCode, 400);
-                        done();
-                    });
+            // List objects in the bucket
+            s3.listObjects({ Bucket: bucketName }, (err, data) => {
+                if (err && err.code === 'NoSuchBucket') {
+                    return done(); // Ignore the error if the bucket does not exist
+                } else if (err) {
+                    return done(err);
+                }
+
+                if (data.Contents.length === 0) {
+                    // Bucket is already empty
+                    return deleteBucket();
+                }
+
+                // Delete all objects in the bucket
+                const objects = data.Contents.map(item => ({ Key: item.Key }));
+                s3.deleteObjects({
+                    Bucket: bucketName,
+                    Delete: { Objects: objects }
+                }, (err) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    deleteBucket();
                 });
             });
+        });
+    });
 
-        it('should return InvalidRequest error if putting object with ' +
-            'object lock retention date and mode when object lock is not ' +
-            'enabled on the bucket', done => {
-            const date = new Date(2050, 10, 10);
-            const params = {
-                Bucket: bucket,
-                Key: 'key',
-                ObjectLockRetainUntilDate: date,
-                ObjectLockMode: 'GOVERNANCE',
-            };
-            s3.putObject(params, err => {
-                const expectedErrMessage
-                    = 'Bucket is missing ObjectLockConfiguration';
-                assert.strictEqual(err.code, 'InvalidRequest');
-                assert.strictEqual(err.message, expectedErrMessage);
-                done();
-            });
+    it('should successfully upload an object to S3 using a POST form', done => {
+        const fields = calculateFields();
+        const formData = new FormData();
+
+        fields.forEach(field => {
+            formData.append(field.name, field.value);
         });
 
-        it('should return Not Implemented error for obj. encryption using ' +
-            'customer-provided encryption keys', done => {
-            const params = { Bucket: bucket, Key: 'key',
-                SSECustomerAlgorithm: 'AES256' };
-            s3.putObject(params, err => {
-                assert.strictEqual(err.code, 'NotImplemented');
-                done();
+        formData.append('file', fs.createReadStream(path.join(__dirname, 'test-file.txt')));
+
+        formData.getLength((err, length) => {
+            if (err) {
+                return done(err);
+            }
+
+            axios.post(url, formData, {
+                headers: {
+                    ...formData.getHeaders(),
+                    'Content-Length': length,
+                },
+            })
+                .then(response => {
+                    assert.equal(response.status, 204);
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
+        });
+    });
+
+    it('should successfully upload a larger file to S3 using a POST form', done => {
+        const largeFilePath = path.join(__dirname, 'large-test-file.txt');
+        const largeFileContent = 'This is a larger test file'.repeat(10000); // Simulate a larger file
+
+        fs.writeFile(largeFilePath, largeFileContent, err => {
+            if (err) {
+                return done(err);
+            }
+
+            const fields = calculateFields();
+            const formData = new FormData();
+
+            fields.forEach(field => {
+                formData.append(field.name, field.value);
             });
+
+            formData.append('file', fs.createReadStream(largeFilePath));
+
+            formData.getLength((err, length) => {
+                if (err) {
+                    return done(err);
+                }
+
+                axios.post(url, formData, {
+                    headers: {
+                        ...formData.getHeaders(),
+                        'Content-Length': length,
+                    },
+                })
+                    .then(response => {
+                        assert.equal(response.status, 204);
+                        fs.unlink(largeFilePath, done); // Clean up the large file
+                    })
+                    .catch(err => {
+                        fs.unlink(largeFilePath, () => done(err)); // Clean up and propagate the error
+                    });
+            });
+        });
+    });
+
+    it('should handle error when bucket does not exist', done => {
+        bucketName = generateBucketName();
+        const tempUrl = `https://${bucketName}.s3.amazonaws.com/`;
+        const fields = calculateFields();
+        const formData = new FormData();
+
+        fields.forEach(field => {
+            formData.append(field.name, field.value);
+        });
+
+        formData.append('file', fs.createReadStream(path.join(__dirname, 'test-file.txt')));
+
+        formData.getLength((err, length) => {
+            if (err) {
+                return done(err);
+            }
+
+            axios.post(tempUrl, formData, {
+                headers: {
+                    ...formData.getHeaders(),
+                    'Content-Length': length,
+                },
+            })
+                .then(() => {
+                    done(new Error('Expected error but got success response'));
+                })
+                .catch(err => {
+
+                    assert.equal(err.response.status, 404);
+                    done();
+                });
+        });
+    });
+
+    it('should upload an object with additional metadata', done => {
+        const additionalConditions = [
+            { 'x-amz-meta-test-meta': 'test-value' },
+        ];
+        const fields = calculateFields(additionalConditions);
+        const formData = new FormData();
+
+        fields.forEach(field => {
+            formData.append(field.name, field.value);
+        });
+
+        formData.append('file', fs.createReadStream(path.join(__dirname, 'test-file.txt')));
+
+        formData.getLength((err, length) => {
+            if (err) {
+                return done(err);
+            }
+
+            axios.post(url, formData, {
+                headers: {
+                    ...formData.getHeaders(),
+                    'Content-Length': length,
+                },
+            })
+                .then(response => {
+                    assert.equal(response.status, 204);
+
+                    // Verify metadata
+                    s3.headObject({ Bucket: bucketName, Key: filename }, (err, data) => {
+                        if (err) {
+                            return done(err);
+                        }
+                        assert.equal(data.Metadata['test-meta'], 'test-value');
+                        done();
+                    });
+                })
+                .catch(err => {
+                    done(err);
+                });
+        });
+    });
+});
         });
 
         it('should return InvalidRedirectLocation if putting object ' +
