bcea25d318727ceef05b727b781c057b2e09314e
Anurag Mittal <anurag.mittal@scality.com>
2024-06-17 15:18:39 +0200

    updated Otelstack

--- instrumentation.js
+++ instrumentation.js
@@ -6,15 +6,16 @@ const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumenta
 const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto');
 const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { IORedisInstrumentation } = require('@opentelemetry/instrumentation-ioredis');
 
 const {
     SEMRESATTRS_SERVICE_NAME,
     SEMRESATTRS_SERVICE_VERSION,
-  } = require('@opentelemetry/semantic-conventions');
+} = require('@opentelemetry/semantic-conventions');
 
 // Define resource with service name and version
 const resource = new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME,
+    [SEMRESATTRS_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME || 'cloudserver-service',
     [SEMRESATTRS_SERVICE_VERSION]: '7.70.47',
 });
 
@@ -43,6 +44,17 @@ const sdk = new opentelemetry.NodeSDK({
             '@opentelemetry/instrumentation-fs': {
                 enabled: false,
             },
+            '@opentelemetry/instrumentation-http': {
+                responseHook: (span, operations) => {
+                    span.updateName(`${operations.req.path.split('&')[0]}`);
+                },
+            },
+        }),
+        new IORedisInstrumentation({
+            requestHook: (span, { cmdName, cmdArgs }) => {
+                span.updateName(`Redis:: ${cmdName.toUpperCase()} cache operation  for ${cmdArgs[0].split(':')[0]}`);
+            },
+            // see under for available configuration
         }),
     ],
 });
@@ -56,3 +68,4 @@ webTracerProvider.register();
 
 // Start the Node SDK
 sdk.start();
+
