73c0be05cd7fe77d0a05f89554ea6c567b781f18
Maha Benzekri <maha.benzekri@scality.com>
2024-12-13 14:28:22 +0100

    test

--- tests/utapi/utilities.js
+++ tests/utapi/utilities.js
@@ -480,7 +480,7 @@ const testEvents = [{
     },
 }];
 
-describe('utapi v2 pushmetrics utility', () => {
+describe.skip('utapi v2 pushmetrics utility', () => {
     const log = new werelogs.Logger('utapi-utility');
     const { UtapiClient, utapiVersion } = require('utapi');
     let pushMetric;
