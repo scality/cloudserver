9d563c70f18bf2db46341c2647ced0c19ae9c2fb
Mickael Bourgois <mickael.bourgois@scality.com>
2025-06-16 17:15:41 +0200

    CLDSRV-657: Change cloudserver version before sse

--- .github/docker/local.sh
+++ .github/docker/local.sh
@@ -9,8 +9,8 @@ export S3METADATA=file
 export S3VAULT=scality
 export MPU_TESTING="yes"
 
-export CLOUDSERVER_IMAGE_BEFORE_SSE_MIGRATION=ghcr.io/scality/cloudserver:7.70.66
-export CLOUDSERVER_IMAGE_ORIGINAL=ghcr.io/scality/cloudserver:7.70.70
+export CLOUDSERVER_IMAGE_BEFORE_SSE_MIGRATION=ghcr.io/scality/cloudserver:8.8.45
+export CLOUDSERVER_IMAGE_ORIGINAL=ghcr.io/scality/cloudserver:8.8.51
 
 export VAULT_IMAGE_BEFORE_SSE_MIGRATION=ghcr.io/scality/vault:7.70.31
 export VAULT_IMAGE_ORIGINAL=ghcr.io/scality/vault:7.70.32
