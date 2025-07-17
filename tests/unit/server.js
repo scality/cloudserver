1861efd47c32c8cf794c7e072c50fc2e152d9ab3
Maha Benzekri <maha.benzekri@scality.com>
2025-07-17 12:17:01 +0200

    unit test addition for timeout disabing
    Issue: CLDSRV-716

--- tests/unit/server.js
+++ tests/unit/server.js
@@ -2,6 +2,8 @@
 
 const assert = require('assert');
 const sinon = require('sinon');
+const http = require('http');
+const https = require('https');
 const arsenal = require('arsenal');
 const uuid = require('uuid');
 const logger = require('../../lib/utilities/logger');
@@ -208,5 +210,43 @@ describe('S3Server', () => {
             server.routeRequest(req, resp);
             sinon.assert.calledOnce(routesMock);
         });
+    });
+});
+
+describe('S3Server request timeout', () => {
+    let sandbox;
+    let mockServer;
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+        
+        // Create a mock server to capture the requestTimeout setting
+        mockServer = {
+            requestTimeout: null,
+            on: sandbox.stub(),
+            listen: sandbox.stub(),
+            address: sandbox.stub().returns({ address: '127.0.0.1', port: 8000 }),
+        };
+        
+        // Mock server creation to return our mock
+        sandbox.stub(http, 'createServer').returns(mockServer);
+        sandbox.stub(https, 'createServer').returns(mockServer);
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should set server.requestTimeout to 0 when starting server', () => {
+        const server = new S3Server({
+            ...defaultConfig,
+            https: false
+        });
+        
+        // Call _startServer which should set requestTimeout = 0
+        server._startServer(() => {}, 8000, '127.0.0.1');
+        
+        // Verify that requestTimeout was set to 0
+        assert.strictEqual(mockServer.requestTimeout, 0);
     });
 });
