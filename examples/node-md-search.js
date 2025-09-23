9e4ce4cee377038f2c16f41d2bcc04fbe1841d69
Maha Benzekri <maha.benzekri@scality.com>
2025-09-23 17:57:03 +0200

    mdSearch example update due to aws-sdk migration

--- examples/node-md-search.js
+++ examples/node-md-search.js
@@ -1,28 +1,27 @@
-const { S3 } = require('aws-sdk');
+const { S3Client, ListObjectsCommand } = require('@aws-sdk/client-s3');
+
 const config = {
-    sslEnabled: false,
+    forcePathStyle: true,
     endpoint: 'http://127.0.0.1:8000',
-    signatureCache: false,
-    signatureVersion: 'v4',
     region: 'us-east-1',
-    s3ForcePathStyle: true,
-    accessKeyId: 'accessKey1',
-    secretAccessKey: 'verySecretKey1',
+    credentials: {
+        accessKeyId: 'accessKey1',
+        secretAccessKey: 'verySecretKey1',
+    },
 };
-const s3Client = new S3(config);
+const s3Client = new S3Client(config);
 
-const encodedSearch =
-    encodeURIComponent('x-amz-meta-color="blue"');
-const req = s3Client.listObjects({ Bucket: 'bucketname' });
 
-// the build event
-req.on('build', () => {
-    req.httpRequest.path = `${req.httpRequest.path}?search=${encodedSearch}`;
-});
-req.on('success', res => {
-    process.stdout.write(`Result ${res.data}`);
-});
-req.on('error', err => {
-    process.stdout.write(`Error ${err}`);
-});
-req.send();
+// In AWS SDK v3, we need to handle the custom search parameter differently
+// Since v3 doesn't have the same event system, we'll need to construct the URL manually
+const listObjectsCommand = new ListObjectsCommand({ Bucket: 'bucketname' });
+
+// For custom search functionality, you would need to handle this at the HTTP level
+// or use the underlying HTTP client middleware
+s3Client.send(listObjectsCommand)
+    .then(res => {
+        process.stdout.write(`Result ${JSON.stringify(res)}`);
+    })
+    .catch(err => {
+        process.stdout.write(`Error ${err}`);
+    });
