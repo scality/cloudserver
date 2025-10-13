cda4b50550e38b9f7f0f03c38ac5355b89508068
Maha Benzekri <maha.benzekri@scality.com>
2025-10-13 16:21:56 +0200

    fixups on get object tagging check error

--- tests/functional/aws-node-sdk/lib/utility/checkError.js
+++ tests/functional/aws-node-sdk/lib/utility/checkError.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 
 function checkError(err, code, statusCode) {
     assert(err, 'Expected error but found none');
-    assert.strictEqual(err.code, code);
-    assert.strictEqual(err.statusCode, statusCode);
+    assert.strictEqual(err.name, code);
+    assert.strictEqual(err.$metadata.httpStatusCode, statusCode);
 }
 
 module.exports = checkError;
