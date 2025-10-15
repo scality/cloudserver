d4531904fd6585a381b3eec3507359d02b7d9f3d
DarkIsDude <edouard.comtet@gmail.com>
2025-10-15 14:51:43 +0200

    ✅ update test to manage race condition
    Issue: CLDSRV-765

--- tests/functional/aws-node-sdk/test/object/copyPart.js
+++ tests/functional/aws-node-sdk/test/object/copyPart.js
@@ -609,18 +609,43 @@ describe('Object Part Copy', () => {
                     process.stdout.write(
                       'Overwriting first part in MPU test and completing MPU at the same time',
                     );
+
+                    const completeMPU = () => s3
+                      .completeMultipartUpload({
+                        Bucket: destBucketName,
+                        Key: randomDestObjName,
+                        UploadId: uploadId,
+                        MultipartUpload: {
+                          Parts: [{ ETag: etag, PartNumber: 1 }],
+                        },
+                      }).promise();
+
                     const [completeRes, uploadRes] = await Promise.all([
-                      s3
-                        .completeMultipartUpload({
-                          Bucket: destBucketName,
-                          Key: randomDestObjName,
-                          UploadId: uploadId,
-                          MultipartUpload: {
-                            Parts: [{ ETag: etag, PartNumber: 1 }],
-                          },
-                        })
-                        .promise()
-                        .catch(err => {
+                      completeMPU()
+                        .catch(async err => {
+                          // The completeMPU delete part in a 2 steps
+                          // (first mark it as deletion with `delete: true` in Mongo, then delete it).
+                          // At the same time, the uploadPartCopy update the same data in Mongo.
+                          // In that case, the completeMPU fail with an InternalError (DeleteConflict in the logs)
+                          const raceConditionOccurred = err && err.code === 'InternalError';
+
+                          if (raceConditionOccurred) {
+                            process.stdout.write('Retrying the complete MPU');
+
+                            try {
+                              await completeMPU();
+                            } catch (e) {
+                              if (e.code === 'NoSuchUpload') {
+                                process.stdout.write('Race condition done');
+                                return Promise.resolve(null);
+                              }
+
+                              throw e;
+                            }
+                 
+                            throw new Error('Expected error on the retry of complete MPU');
+                          }
+
                           throw err;
                         }),
                       s3
@@ -648,12 +673,13 @@ describe('Object Part Copy', () => {
                       assert(uploadRes.LastModified);
                     }
 
-                    assert.strictEqual(completeRes.Bucket, destBucketName);
-                    assert.strictEqual(completeRes.Key, randomDestObjName);
-                    assert.strictEqual(completeRes.ETag, finalObjETag);
-                    process.stdout.write(
-                      'Getting object put by MPU with ' + 'overwrite part',
-                    );
+                    if (completeRes !== null) {
+                      assert.strictEqual(completeRes.Bucket, destBucketName);
+                      assert.strictEqual(completeRes.Key, randomDestObjName);
+                      assert.strictEqual(completeRes.ETag, finalObjETag);
+                    }
+
+                    process.stdout.write('Getting object put by MPU with overwrite part');
                     const resGet = await s3
                       .getObject({
                         Bucket: destBucketName,
