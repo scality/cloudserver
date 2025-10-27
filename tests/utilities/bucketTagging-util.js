954f00f25f77efc30596bd5ac984713bd33a1e4b
Maha Benzekri <maha.benzekri@scality.com>
2025-10-27 17:19:20 +0100

    tests utilities migration
    Issue: CLDSRV-724

--- tests/utilities/bucketTagging-util.js
+++ tests/utilities/bucketTagging-util.js
@@ -5,11 +5,11 @@ function assertError(err, expectedErr) {
     if (expectedErr === null) {
         assert.strictEqual(err, null, `expected no error but got '${err}'`);
     } else {
-        assert.strictEqual(err.code, expectedErr, 'incorrect error response ' +
-            `code: should be '${expectedErr}' but got '${err.code}'`);
-        assert.strictEqual(err.statusCode, errors[expectedErr].code,
+        assert.strictEqual(err.Code, expectedErr, 'incorrect error response ' +
+            `code: should be '${expectedErr}' but got '${err.Code}'`);
+        assert.strictEqual(err.$metadata.httpStatusCode, errors[expectedErr].code,
             'incorrect error status code: should be  ' +
-            `${errors[expectedErr].code}, but got '${err.statusCode}'`);
+            `${errors[expectedErr].code}, but got '${err.$metadata.httpStatusCode}'`);
     }
 }
 
