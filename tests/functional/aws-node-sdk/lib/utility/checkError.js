409dbc241c4407e92382620c69f75c586f78c52f
Maha Benzekri <maha.benzekri@scality.com>
2025-10-27 17:19:20 +0100

    tests utilities migration
    Issue: CLDSRV-724

--- tests/functional/aws-node-sdk/lib/utility/checkError.js
+++ tests/functional/aws-node-sdk/lib/utility/checkError.js
@@ -2,8 +2,12 @@ const assert = require('assert');
 
 function checkError(err, code, statusCode) {
     assert(err, 'Expected error but found none');
-    assert.strictEqual(err.code, code);
-    assert.strictEqual(err.statusCode, statusCode);
+    if (code) {
+        assert.strictEqual(err.name, code);
+    }
+    if (statusCode) {
+        assert.strictEqual(err.$metadata.httpStatusCode, statusCode);
+    }
 }
 
 module.exports = checkError;
