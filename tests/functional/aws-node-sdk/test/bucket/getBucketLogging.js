959a8bd1a487713db2572b6d3e0973eff1e6ec79
Maha Benzekri <maha.benzekri@scality.com>
2025-10-28 07:37:39 +0100

    get operation related bucket tests migration
    Issue: CLDSRV-724

--- tests/functional/aws-node-sdk/test/bucket/getBucketLogging.js
+++ tests/functional/aws-node-sdk/test/bucket/getBucketLogging.js
@@ -1,4 +1,9 @@
 const assert = require('assert');
+const {
+    CreateBucketCommand,
+    GetBucketLoggingCommand,
+    PutBucketLoggingCommand,
+} = require('@aws-sdk/client-s3');
 
 const withV4 = require('../support/withV4');
 const BucketUtility = require('../../lib/utility/bucket-util');
@@ -20,81 +25,83 @@ describe('GET bucket logging', () => {
 
         afterEach(done => {
             process.stdout.write('Deleting buckets\n');
-            bucketUtil.deleteOne(bucketName).then(() => bucketUtil.deleteOne(targetBucket)).then(() => done())
-            .catch(err => {
-                if (err && err.code !== 'NoSuchBucket') {
-                    return done(err);
-                }
-                return done();
-            });
+            bucketUtil.deleteOne(bucketName)
+                .then(() => bucketUtil.deleteOne(targetBucket))
+                .then(() => done())
+                .catch(err => {
+                    if (err && err.name !== 'NoSuchBucket') {
+                        return done(err);
+                    }
+                    return done();
+                });
         });
 
         describe('without existing bucket', () => {
             it('should return NoSuchBucket', done => {
-                s3.getBucketLogging({ Bucket: bucketName }, err => {
-                    assert(err);
-                    assert.strictEqual(err.code, 'NoSuchBucket');
-                    assert.strictEqual(err.statusCode, 404);
-                    return done();
-                });
+                s3.send(new GetBucketLoggingCommand({ Bucket: bucketName }))
+                    .then(() => {
+                        done(new Error('Expected error but succeeded'));
+                    })
+                    .catch(err => {
+                        assert(err);
+                        assert.strictEqual(err.name, 'NoSuchBucket');
+                        assert.strictEqual(err.$metadata.httpStatusCode, 404);
+                        done();
+                    });
             });
         });
 
         describe('on bucket without logging configuration', () => {
             before(done => {
                 process.stdout.write('Creating bucket without logging\n');
-                s3.createBucket({ Bucket: bucketName }, err => {
-                    if (err) {
+                s3.send(new CreateBucketCommand({ Bucket: bucketName }))
+                    .then(() => done())
+                    .catch(err => {
                         process.stdout.write('error creating bucket', err);
-                        return done(err);
-                    }
-                    return done();
-                });
+                        done(err);
+                    });
             });
 
             it('should return empty BucketLoggingStatus', done => {
-                s3.getBucketLogging({ Bucket: bucketName }, (err, data) => {
-                    assert.strictEqual(err, null,
-                        `Found unexpected err ${err}`);
-                    // When no logging is configured, AWS returns empty object
-                    assert(data);
-                    assert.strictEqual(Object.keys(data).length, 0, 'Expected data to have no keys');
-                    return done();
-                });
+                s3.send(new GetBucketLoggingCommand({ Bucket: bucketName }))
+                    .then(data => {
+                        // When no logging is configured, AWS returns empty object
+                        assert(data);
+                        assert.strictEqual(Object.keys(data).length, 1, 'Expected data to have only $metadata key');
+                        assert(data.$metadata);
+                        done();
+                    })
+                    .catch(err => {
+                        done(err);
+                    });
             });
         });
 
         describe('with existing logging configuration', () => {
             before(done => {
                 process.stdout.write('Creating buckets and setting logging\n');
-                return s3.createBucket({ Bucket: bucketName }, err => {
-                    if (err) {
-                        return done(err);
-                    }
-                    return s3.createBucket({ Bucket: targetBucket }, err => {
-                        if (err) {
-                            return done(err);
-                        }
-                        return s3.putBucketLogging({
-                            Bucket: bucketName,
-                            BucketLoggingStatus: validLoggingConfig,
-                        }, done);
-                    });
-                });
+                s3.send(new CreateBucketCommand({ Bucket: bucketName }))
+                    .then(() => s3.send(new CreateBucketCommand({ Bucket: targetBucket })))
+                    .then(() => s3.send(new PutBucketLoggingCommand({
+                        Bucket: bucketName,
+                        BucketLoggingStatus: validLoggingConfig,
+                    })))
+                    .then(() => done())
+                    .catch(done);
             });
 
             it('should return bucket logging configuration successfully', done => {
-                s3.getBucketLogging({ Bucket: bucketName }, (err, data) => {
-                    assert.strictEqual(err, null,
-                        `Found unexpected err ${err}`);
-                    assert(data.LoggingEnabled);
-                    assert.strictEqual(data.LoggingEnabled.TargetBucket,
-                        targetBucket);
-                    assert.strictEqual(data.LoggingEnabled.TargetPrefix, 'logs/');
-                    return done();
-                });
+                s3.send(new GetBucketLoggingCommand({ Bucket: bucketName }))
+                    .then(data => {
+                        assert(data.LoggingEnabled);
+                        assert.strictEqual(data.LoggingEnabled.TargetBucket, targetBucket);
+                        assert.strictEqual(data.LoggingEnabled.TargetPrefix, 'logs/');
+                        done();
+                    })
+                    .catch(err => {
+                        done(err);
+                    });
             });
         });
     });
 });
-
