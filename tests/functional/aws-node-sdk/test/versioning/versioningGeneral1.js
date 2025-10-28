c24608aed8401ec659e19c7f906ce43c42830156
Maha Benzekri <maha.benzekri@scality.com>
2025-10-28 08:02:54 +0100

    rest of versionning tests migration
    Issue: CLDSRV-724

--- tests/functional/aws-node-sdk/test/versioning/versioningGeneral1.js
+++ tests/functional/aws-node-sdk/test/versioning/versioningGeneral1.js
@@ -1,6 +1,17 @@
 const assert = require('assert');
-const { S3 } = require('aws-sdk');
 const async = require('async');
+
+const {
+    S3Client,
+    CreateBucketCommand,
+    DeleteBucketCommand,
+    PutBucketVersioningCommand,
+    PutObjectCommand,
+    ListObjectsCommand,
+    DeleteObjectCommand,
+    ListObjectVersionsCommand,
+    DeleteObjectsCommand,
+} = require('@aws-sdk/client-s3');
 
 const getConfig = require('../support/config');
 
@@ -22,31 +33,30 @@ function comp(v1, v2) {
     return 0;
 }
 
-
 describe('aws-node-sdk test bucket versioning listing', function testSuite() {
     this.timeout(600000);
     let s3;
     const masterVersions = [];
     const allVersions = [];
 
-    // setup test
-    before(done => {
+    before(async () => {
         const config = getConfig('default', { signatureVersion: 'v4' });
-        s3 = new S3(config);
-        s3.createBucket({ Bucket: bucket }, done);
+        s3 = new S3Client(config);
+        await s3.send(new CreateBucketCommand({ Bucket: bucket }));
     });
 
-    // delete bucket after testing
-    after(done => s3.deleteBucket({ Bucket: bucket }, done));
+    after(async () => {
+        await s3.send(new DeleteBucketCommand({ Bucket: bucket }));
+    });
 
-    it('should accept valid versioning configuration', done => {
+    it('should accept valid versioning configuration', async () => {
         const params = {
             Bucket: bucket,
             VersioningConfiguration: {
                 Status: 'Enabled',
             },
         };
-        s3.putBucketVersioning(params, done);
+        await s3.send(new PutBucketVersioningCommand(params));
     });
 
     it('should create a bunch of objects and their versions', done => {
@@ -58,12 +68,14 @@ describe('aws-node-sdk test bucket versioning listing', function testSuite() {
             masterVersions.push(key);
             const params = { Bucket: bucket, Key: key, Body: value };
             async.timesLimit(versioncount, 10, (j, next2) =>
-                s3.putObject(params, (err, data) => {
-                    assert.strictEqual(err, null);
-                    assert(data.VersionId, 'invalid versionId');
-                    allVersions.push({ Key: key, VersionId: data.VersionId });
-                    next2();
-                }), next1);
+                s3.send(new PutObjectCommand(params))
+                    .then(data => {
+                        assert(data.VersionId, 'invalid versionId');
+                        allVersions.push({ Key: key, VersionId: data.VersionId });
+                        next2();
+                    })
+                    .catch(next2),
+                next1);
         }, err => {
             assert.strictEqual(err, null);
             assert.strictEqual(allVersions.length, keycount * versioncount);
@@ -71,14 +83,12 @@ describe('aws-node-sdk test bucket versioning listing', function testSuite() {
         });
     });
 
-    it('should list all latest versions', done => {
+    it('should list all latest versions', async () => {
         const params = { Bucket: bucket, MaxKeys: 1000, Delimiter: '/' };
-        s3.listObjects(params, (err, data) => {
-            const keys = data.Contents.map(entry => entry.Key);
-            assert.deepStrictEqual(keys.sort(), masterVersions.sort(),
-                    'not same keys');
-            done();
-        });
+        const data = await s3.send(new ListObjectsCommand(params));
+        const keys = data.Contents.map(entry => entry.Key);
+        assert.deepStrictEqual(keys.sort(), masterVersions.sort(),
+                'not same keys');
     });
 
     it('should create some delete markers', done => {
@@ -86,44 +96,78 @@ describe('aws-node-sdk test bucket versioning listing', function testSuite() {
         async.times(keycount, (i, next) => {
             const key = masterVersions[i];
             const params = { Bucket: bucket, Key: key };
-            s3.deleteObject(params, (err, data) => {
-                assert.strictEqual(err, null);
-                assert(data.VersionId, 'invalid versionId');
-                allVersions.push({ Key: key, VersionId: data.VersionId });
-                next();
-            });
+            s3.send(new DeleteObjectCommand(params))
+                .then(data => {
+                    assert(data.VersionId, 'invalid versionId');
+                    allVersions.push({ Key: key, VersionId: data.VersionId });
+                    next();
+                })
+                .catch(next);
         }, done);
     });
 
-    it('should list all latest versions', done => {
+    it('should list all latest versions', async () => {
         const params = { Bucket: bucket, MaxKeys: 1000, Delimiter: '/' };
-        s3.listObjects(params, (err, data) => {
-            const keys = data.Contents.map(entry => entry.Key);
-            assert.deepStrictEqual(keys.sort(), masterVersions.sort().slice(15),
-                    'not same keys');
-            done();
-        });
+        const data = await s3.send(new ListObjectsCommand(params));
+        const keys = data.Contents.map(entry => entry.Key);
+        assert.deepStrictEqual(keys.sort(), masterVersions.sort().slice(15),
+                'not same keys');
     });
 
     it('should list all versions', done => {
         const versions = [];
         const params = { Bucket: bucket, MaxKeys: 15, Delimiter: '/' };
-        async.retry(100, done => s3.listObjectVersions(params, (err, data) => {
-            data.Versions.forEach(version => versions.push({
-                Key: version.Key, VersionId: version.VersionId }));
-            data.DeleteMarkers.forEach(version => versions.push({
-                Key: version.Key, VersionId: version.VersionId }));
-            if (data.IsTruncated) {
-                params.KeyMarker = data.NextKeyMarker;
-                params.VersionIdMarker = data.NextVersionIdMarker;
-                return done('not done yet');
+    
+        async.retry(100, done => {
+            s3.send(new ListObjectVersionsCommand(params))
+                .then(data => {
+                    if (data.Versions) {
+                        data.Versions.forEach(version => versions.push({
+                            Key: version.Key, VersionId: version.VersionId }));
+                    }
+                    if (data.DeleteMarkers) {
+                        data.DeleteMarkers.forEach(version => versions.push({
+                            Key: version.Key, VersionId: version.VersionId }));
+                    }
+                    if (data.IsTruncated) {
+                        params.KeyMarker = data.NextKeyMarker;
+                        params.VersionIdMarker = data.NextVersionIdMarker;
+                        return done('not done yet');
+                    }
+                    return done();
+                })
+                .catch(err => {
+                    done(err);
+                });
+        }, err => {
+            if (err) {
+                return done(err);
             }
-            return done();
-        }), () => {
+
             assert.deepStrictEqual(versions.sort(comp), allVersions.sort(comp),
                     'not same versions');
-            const params = { Bucket: bucket, Delete: { Objects: allVersions } };
-            s3.deleteObjects(params, done);
+            
+            const objectsToDelete = versions
+                .filter(v => v && v.Key && v.VersionId)
+                .map(v => ({
+                    Key: String(v.Key),
+                    VersionId: String(v.VersionId),
+                }));
+
+            const deleteParams = { 
+                Bucket: bucket, 
+                Delete: { 
+                    Objects: objectsToDelete,
+                } 
+            };            
+            return s3.send(new DeleteObjectsCommand(deleteParams))
+                .then(() => {
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 });
+
