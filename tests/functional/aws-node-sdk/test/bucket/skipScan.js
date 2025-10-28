a1339977d2aec88623f32bd2648802ea9645554c
Maha Benzekri <maha.benzekri@scality.com>
2025-10-28 07:39:14 +0100

    other operations related bucket tests migration
    Issue: CLDSRV-724

--- tests/functional/aws-node-sdk/test/bucket/skipScan.js
+++ tests/functional/aws-node-sdk/test/bucket/skipScan.js
@@ -1,5 +1,9 @@
-const AWS = require('aws-sdk');
-const async = require('async');
+const { S3Client,
+    CreateBucketCommand,
+    DeleteBucketCommand,
+    PutObjectCommand,
+    ListObjectsCommand,
+    DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const assert = require('assert');
 
 const getConfig = require('../support/config');
@@ -7,16 +11,20 @@ const getConfig = require('../support/config');
 function cutAttributes(data) {
     const newContent = [];
     const newPrefixes = [];
-    data.Contents.forEach(item => {
-        newContent.push(item.Key);
-    });
-    /* eslint-disable no-param-reassign */
-    data.Contents = newContent;
-    data.CommonPrefixes.forEach(item => {
-        newPrefixes.push(item.Prefix);
-    });
-    /* eslint-disable no-param-reassign */
-    data.CommonPrefixes = newPrefixes;
+    if (data.Contents) {
+        data.Contents.forEach(item => {
+            newContent.push(item.Key);
+        });
+        /* eslint-disable no-param-reassign */
+        data.Contents = newContent;
+    }
+    if (data.CommonPrefixes) {
+        data.CommonPrefixes.forEach(item => {
+            newPrefixes.push(item.Prefix);
+        });
+        /* eslint-disable no-param-reassign */
+        data.CommonPrefixes = newPrefixes;
+    }
     if (data.NextMarker === '') {
         /* eslint-disable no-param-reassign */
         delete data.NextMarker;
@@ -35,61 +43,57 @@ const Bucket = `bucket-skip-scan-${Date.now()}`;
 
 describe('Skip scan cases tests', () => {
     let s3;
-    before(done => {
+    before(async () => {
         const config = getConfig('default', { signatureVersion: 'v4' });
-        s3 = new AWS.S3(config);
-        s3.createBucket(
-            { Bucket }, (err, data) => {
-                if (err) {
-                    done(err, data);
-                }
-                /* generating different prefixes every x > STREAK_LENGTH
-                   to force the metadata backends to skip */
-                const x = 120;
-                async.timesLimit(500, 10,
-                                 (n, next) => {
-                                     const o = {};
-                                     o.Bucket = Bucket;
-                                     // eslint-disable-next-line
-                                     o.Key = String.fromCharCode(65 + n / x) +
-                                         '/' + n % x;
-                                     o.Body = '';
-                                     s3.putObject(o, (err, data) => {
-                                         next(err, data);
-                                     });
-                                 }, done);
-            });
+        s3 = new S3Client(config);        
+        await s3.send(new CreateBucketCommand({ Bucket }));
+        const x = 120;
+        const promises = [];
+        for (let n = 0; n < 500; n++) {
+            const putObjectPromise = async () => {
+                const o = {};
+                o.Bucket = Bucket;
+                // eslint-disable-next-line
+                o.Key = String.fromCharCode(65 + n / x) +
+                    '/' + n % x;
+                o.Body = '';
+                await s3.send(new PutObjectCommand(o));
+            };
+            promises.push(putObjectPromise);
+        }        
+        for (let i = 0; i < promises.length; i += 10) {
+            const batch = promises.slice(i, i + 10);
+            await Promise.all(batch.map(fn => fn()));
+        }
     });
-    after(done => {
-        s3.listObjects({ Bucket }, (err, data) => {
-            async.each(data.Contents, (o, next) => {
-                s3.deleteObject({ Bucket, Key: o.Key }, next);
-            }, () => {
-                s3.deleteBucket({ Bucket }, done);
-            });
-        });
+    
+    after(async () => {
+        const data = await s3.send(new ListObjectsCommand({ Bucket }));
+        const deletePromises = data.Contents.map(o => 
+            s3.send(new DeleteObjectCommand({ Bucket, Key: o.Key }))
+        );
+        await Promise.all(deletePromises);
+        await s3.send(new DeleteBucketCommand({ Bucket }));
     });
-    it('should find all common prefixes in one shot', done => {
-        s3.listObjects({ Bucket, Delimiter: '/' }, (err, data) => {
-            assert.strictEqual(err, null);
-            cutAttributes(data);
-            assert.deepStrictEqual(data, {
-                IsTruncated: false,
-                Marker: '',
-                Contents: [],
-                Delimiter: '/',
-                Name: Bucket,
-                Prefix: '',
-                MaxKeys: 1000,
-                CommonPrefixes: [
-                    'A/',
-                    'B/',
-                    'C/',
-                    'D/',
-                    'E/',
-                ],
-            });
-            done();
+    
+    it('should find all common prefixes in one shot', async () => {
+        const { $metadata , ...data } = await s3.send(new ListObjectsCommand({ Bucket, Delimiter: '/' }));
+        cutAttributes(data);
+        assert.deepStrictEqual(data, {
+            IsTruncated: false,
+            Marker: '',
+            Delimiter: '/',
+            Name: Bucket,
+            Prefix: '',
+            MaxKeys: 1000,
+            CommonPrefixes: [
+                'A/',
+                'B/',
+                'C/',
+                'D/',
+                'E/',
+            ],
         });
+        assert.strictEqual($metadata.httpStatusCode, 200);
     });
 });
