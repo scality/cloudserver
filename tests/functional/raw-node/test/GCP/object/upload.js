22b738dd798bc949f5da12e50cf0d3fdfd41f1fc
Maha Benzekri <maha.benzekri@scality.com>
2025-10-29 14:30:45 +0100

    fixup

--- tests/functional/raw-node/test/GCP/object/upload.js
+++ tests/functional/raw-node/test/GCP/object/upload.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const async = require('async');
 const arsenal = require('arsenal');
-const { GCP } = arsenal.storage.data.external;
+const GCP = arsenal.storage.data.external.GCP;
 const { gcpRequestRetry, setBucketClass, genUniqID } =
     require('../../../utils/gcpUtils');
 const { getRealAwsConfig } =
