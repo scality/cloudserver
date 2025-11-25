f556c38643e2c12904720f0ebbb2afcd5c6f94c5
Maha Benzekri <maha.benzekri@scality.com>
2025-11-25 16:10:26 +0100

    try

--- tests/functional/aws-node-sdk/test/multipleBackend/utils.js
+++ tests/functional/aws-node-sdk/test/multipleBackend/utils.js
@@ -220,6 +220,7 @@ utils.putToAwsBackend = (s3, bucket, key, body, cb) => {
     s3.putObject({ Bucket: bucket, Key: key, Body: body,
     Metadata: { 'scal-location-constraint': awsLocation } },
         (err, result) => {
+            console.log('Put object to AWS backend', result);
             cb(err, result.VersionId);
         }
     );
