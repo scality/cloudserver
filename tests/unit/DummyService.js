b07e4388fa9fb9d8a5934e3fc96031d81f739ba8
Maha Benzekri <maha.benzekri@scality.com>
2025-12-02 17:53:55 +0100

    rest of code migration

--- tests/unit/DummyService.js
+++ tests/unit/DummyService.js
@@ -4,6 +4,63 @@ class DummyService {
     constructor(config = {}) {
         this.versioning = config.versioning;
     }
+
+    async send(command) {
+        const commandName = command.constructor.name;
+        const input = command.input;
+
+        switch (commandName) {
+            case 'HeadBucketCommand':
+                return {};
+            case 'GetBucketVersioningCommand':
+                if (this.versioning) {
+                    return { Status: 'Enabled' };
+                }
+                return {};
+            case 'HeadObjectCommand':
+                return {
+                    ContentLength: 1024 * 1024 * 1024,
+                };
+            case 'CompleteMultipartUploadCommand': {
+                const retObj = {
+                    Bucket: input.Bucket,
+                    Key: input.Key,
+                    ETag: `"${uuidv4().replace(/-/g, '')}"`,
+                    ContentLength: 1024 * 1024 * 1024,
+                };
+                if (this.versioning) {
+                    retObj.VersionId = uuidv4().replace(/-/g, '');
+                }
+                return retObj;
+            }
+            case 'PutObjectCommand': {
+                const retObj = {
+                    ETag: `"${uuidv4().replace(/-/g, '')}"`,
+                };
+                if (this.versioning) {
+                    retObj.VersionId = uuidv4().replace(/-/g, '');
+                }
+                return retObj;
+            }
+            case 'CopyObjectCommand': {
+                const retObj = {
+                    CopyObjectResult: {
+                        ETag: `"${uuidv4().replace(/-/g, '')}"`,
+                        LastModified: new Date().toISOString(),
+                    },
+                    VersionId: null,
+                };
+                if (this.versioning) {
+                    retObj.VersionId = uuidv4().replace(/-/g, '');
+                }
+                return retObj;
+            }
+            default:
+                throw new Error(`Unsupported command: ${commandName}`);
+        }
+    }
+
+    // Legacy SDK v2 callback-based interface (for backwards compatibility)
     headBucket(params, callback) {
         return callback();
     }
