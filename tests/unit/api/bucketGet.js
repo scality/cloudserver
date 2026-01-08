f7f586663081e7fbdf94c58bcb243f1891b21be1
DarkIsDude <edouard.comtet@gmail.com>
2026-01-08 17:00:37 +0100

    ✅ add new tests for the new response logic
    Issue: CLDSRV-813
    (cherry picked from commit e38e9c33c4904e0107e3409ecb5a14cd4810ba34)

--- tests/unit/api/bucketGet.js
+++ tests/unit/api/bucketGet.js
@@ -439,5 +439,99 @@ describe('bucketGet API V2', () => {
                 done();
             });
         });
+
+        it('should return user metadata if requested and present', done => {
+            const objectNameMeta = 'objectWithMeta';
+            const putRequest = new DummyRequest({
+                bucketName,
+                headers: { 'x-amz-meta-color': 'red' },
+                url: `/${bucketName}/${objectNameMeta}`,
+                namespace,
+                objectKey: objectNameMeta,
+            }, postBody);
+
+            const testGetRequest = Object.assign({
+                query: {},
+                url: baseUrl,
+            }, baseGetRequest);
+            testGetRequest.headers['x-amz-optional-object-attributes'] = 'x-amz-meta-color';
+
+            async.waterfall([
+                next => bucketPut(authInfo, testPutBucketRequest, log, next),
+                (_, next) => objectPut(authInfo, putRequest, undefined, log, next),
+                (_, next) => bucketGet(authInfo, testGetRequest, log, next),
+                (result, _, next) => parseString(result, next),
+            ],
+            (err, result) => {
+                assert.strictEqual(err, null);
+                const content = result.ListBucketResult.Contents[0];
+                assert.strictEqual(content.Key[0], objectNameMeta);
+                assert.strictEqual(content['x-amz-meta-color'][0], 'red');
+                done();
+            });
+        });
+
+        it('should return all user metadata if wildcard requested', done => {
+            const objectNameMeta = 'objectWithMetaWildcard';
+            const putRequest = new DummyRequest({
+                bucketName,
+                headers: { 'x-amz-meta-color': 'red', 'x-amz-meta-size': 'large' },
+                url: `/${bucketName}/${objectNameMeta}`,
+                namespace,
+                objectKey: objectNameMeta,
+            }, postBody);
+
+            const testGetRequest = Object.assign({
+                query: {},
+                url: baseUrl,
+            }, baseGetRequest);
+            testGetRequest.headers['x-amz-optional-object-attributes'] = 'x-amz-meta-*';
+
+            async.waterfall([
+                next => bucketPut(authInfo, testPutBucketRequest, log, next),
+                (_, next) => objectPut(authInfo, putRequest, undefined, log, next),
+                (_, next) => bucketGet(authInfo, testGetRequest, log, next),
+                (result, _, next) => parseString(result, next),
+            ],
+            (err, result) => {
+                assert.strictEqual(err, null);
+                const content = result.ListBucketResult.Contents[0];
+                assert.strictEqual(content.Key[0], objectNameMeta);
+                assert.strictEqual(content['x-amz-meta-color'][0], 'red');
+                assert.strictEqual(content['x-amz-meta-size'][0], 'large');
+                done();
+            });
+        });
+
+        it('should return user metadata in version listing if requested', done => {
+            const objectNameMeta = 'objectWithMetaVersion';
+            const putRequest = new DummyRequest({
+                bucketName,
+                headers: { 'x-amz-meta-ver': '1' },
+                url: `/${bucketName}/${objectNameMeta}`,
+                namespace,
+                objectKey: objectNameMeta,
+            }, postBody);
+
+            const testGetRequest = Object.assign({
+                query: { versions: '' },
+                url: `${baseUrl}?versions`,
+            }, baseGetRequest);
+            testGetRequest.headers['x-amz-optional-object-attributes'] = 'x-amz-meta-ver';
+
+            async.waterfall([
+                next => bucketPut(authInfo, testPutBucketRequest, log, next),
+                (_, next) => objectPut(authInfo, putRequest, undefined, log, next),
+                (_, next) => bucketGet(authInfo, testGetRequest, log, next),
+                (result, _, next) => parseString(result, next),
+            ],
+            (err, result) => {
+                assert.strictEqual(err, null);
+                const version = result.ListVersionsResult.Version[0];
+                assert.strictEqual(version.Key[0], objectNameMeta);
+                assert.strictEqual(version['x-amz-meta-ver'][0], '1');
+                done();
+            });
+        });
     });
 });
