9471b0cadbe464e44789715cb0737cd599f9eba5
Mickael Bourgois <mickael.bourgois@scality.com>
2026-03-13 20:31:41 +0100

    CLDSRV-835: Normalize async provideRawOutput

--- tests/functional/aws-node-sdk/test/legacy/authV4QueryTests.js
+++ tests/functional/aws-node-sdk/test/legacy/authV4QueryTests.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const process = require('node:process');
 const cp = require('child_process');
+const util = require('util');
 const { parseString } = require('xml2js');
+const parseStringAsync = util.promisify(parseString);
 
 const {
     S3Client,
@@ -16,6 +18,7 @@ const {
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const getConfig = require('../support/config');
 const provideRawOutput = require('../../lib/utility/provideRawOutput');
+const provideRawOutputAsync = util.promisify(provideRawOutput);
 
 const random = Math.round(Math.random() * 100).toString();
 const bucket = `mybucket-${random}`;
@@ -46,10 +49,8 @@ describe('aws-node-sdk v4auth query tests', function testSuite() {
 
     it('should do an empty bucket listing', async () => {
         const url = await getSignedUrl(s3, new ListBucketsCommand({}), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', url], httpCode => {
-            assert.strictEqual(httpCode, '200 OK');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', url]);
+        assert.strictEqual(httpCode, '200 OK');
     });
 
     it('should create a bucket', async () => {
@@ -59,37 +60,26 @@ describe('aws-node-sdk v4auth query tests', function testSuite() {
             new CreateBucketCommand(params),
             { expiresIn: 900 }
         );
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'PUT', url], httpCode => {
-            assert.strictEqual(httpCode, '200 OK');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'PUT', url]);
+        assert.strictEqual(httpCode, '200 OK');
     });
 
     it('should do a bucket listing with result', async () => {
         const url = await getSignedUrl(s3, new ListBucketsCommand({}), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', url], (httpCode, rawOutput) => {
-            assert.strictEqual(httpCode, '200 OK');
-            parseString(rawOutput.stdout, (err, xml) => {
-                if (err) {
-                    assert.ifError(err);
-                }
-                const bucketNames = xml.ListAllMyBucketsResult
-                    .Buckets[0].Bucket.map(item => item.Name[0]);
-                const whereIsMyBucket = bucketNames.indexOf(bucket);
-                assert(whereIsMyBucket > -1);
-                resolve();
-            });
-        }));
+        const { httpCode, rawOutput } = await provideRawOutputAsync(['-verbose', url]);
+        assert.strictEqual(httpCode, '200 OK');
+        const xml = await parseStringAsync(rawOutput.stdout);
+        const bucketNames = xml.ListAllMyBucketsResult
+            .Buckets[0].Bucket.map(item => item.Name[0]);
+        assert(bucketNames.indexOf(bucket) > -1);
     });
 
     it('should put an object', async () => {
         const params = { Bucket: bucket, Key: 'key' };
         const url = await getSignedUrl(s3, new PutObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'PUT', url,
-            '--upload-file', 'uploadFile'], httpCode => {
-            assert.strictEqual(httpCode, '200 OK');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'PUT', url,
+            '--upload-file', 'uploadFile']);
+        assert.strictEqual(httpCode, '200 OK');
     });
 
     it('should put an object with an acl setting and a storage class setting', async () => {
@@ -101,11 +91,9 @@ describe('aws-node-sdk v4auth query tests', function testSuite() {
             ContentType: 'text/plain',
         };
         const url = await getSignedUrl(s3, new PutObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'PUT', url,
-            '--upload-file', 'uploadFile'], httpCode => {
-            assert.strictEqual(httpCode, '200 OK');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'PUT', url,
+            '--upload-file', 'uploadFile']);
+        assert.strictEqual(httpCode, '200 OK');
     });
 
     it('should put an object with native characters', async () => {
@@ -113,38 +101,27 @@ describe('aws-node-sdk v4auth query tests', function testSuite() {
         'português-বাংলা-русский-日本語-ਪੰਜਾਬੀ-한국어-தமிழ்';
         const params = { Bucket: bucket, Key };
         const url = await getSignedUrl(s3, new PutObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'PUT', url,
-            '--upload-file', 'uploadFile'], httpCode => {
-            assert.strictEqual(httpCode, '200 OK');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'PUT', url,
+            '--upload-file', 'uploadFile']);
+        assert.strictEqual(httpCode, '200 OK');
     });
 
     // listObjects test
     it('should list objects in bucket', async () => {
         const params = { Bucket: bucket };
         const url = await getSignedUrl(s3, new ListObjectsCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', url], (httpCode, rawOutput) => {
-            assert.strictEqual(httpCode, '200 OK');
-            parseString(rawOutput.stdout, (err, result) => {
-                if (err) {
-                    assert.ifError(err);
-                }
-                assert.strictEqual(result.ListBucketResult
-                    .Contents[0].Key[0], 'key');
-                resolve();
-            });
-        }));
+        const { httpCode, rawOutput } = await provideRawOutputAsync(['-verbose', url]);
+        assert.strictEqual(httpCode, '200 OK');
+        const result = await parseStringAsync(rawOutput.stdout);
+        assert.strictEqual(result.ListBucketResult.Contents[0].Key[0], 'key');
     });
 
     // getObject test
     it('should get an object', async () => {
         const params = { Bucket: bucket, Key: 'key' };
         const url = await getSignedUrl(s3, new GetObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-o', 'download', url], httpCode => {
-            assert.strictEqual(httpCode, '200 OK');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-o', 'download', url]);
+        assert.strictEqual(httpCode, '200 OK');
     });
 
     it('downloaded file should equal file that was put', done => {
@@ -157,28 +134,22 @@ describe('aws-node-sdk v4auth query tests', function testSuite() {
     it('should delete an object', async () => {
         const params = { Bucket: bucket, Key: 'key' };
         const url = await getSignedUrl(s3, new DeleteObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'DELETE', url], httpCode => {
-            assert.strictEqual(httpCode, '204 NO CONTENT');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'DELETE', url]);
+        assert.strictEqual(httpCode, '204 NO CONTENT');
     });
 
     it('should return a 204 on delete of an already deleted object', async () => {
         const params = { Bucket: bucket, Key: 'key' };
         const url = await getSignedUrl(s3, new DeleteObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'DELETE', url], httpCode => {
-            assert.strictEqual(httpCode, '204 NO CONTENT');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'DELETE', url]);
+        assert.strictEqual(httpCode, '204 NO CONTENT');
     });
 
     it('should return 204 on delete of non-existing object', async () => {
         const params = { Bucket: bucket, Key: 'randomObject' };
         const url = await getSignedUrl(s3, new DeleteObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'DELETE', url], httpCode => {
-            assert.strictEqual(httpCode, '204 NO CONTENT');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'DELETE', url]);
+        assert.strictEqual(httpCode, '204 NO CONTENT');
     });
 
     it('should delete an object with native characters', async () => {
@@ -186,18 +157,14 @@ describe('aws-node-sdk v4auth query tests', function testSuite() {
         'português-বাংলা-русский-日本語-ਪੰਜਾਬੀ-한국어-தமிழ்';
         const params = { Bucket: bucket, Key };
         const url = await getSignedUrl(s3, new DeleteObjectCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'DELETE', url], httpCode => {
-            assert.strictEqual(httpCode, '204 NO CONTENT');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'DELETE', url]);
+        assert.strictEqual(httpCode, '204 NO CONTENT');
     });
 
     it('should delete a bucket', async () => {
         const params = { Bucket: bucket };
         const url = await getSignedUrl(s3, new DeleteBucketCommand(params), { expiresIn: 900 });
-        await new Promise(resolve => provideRawOutput(['-verbose', '-X', 'DELETE', url], httpCode => {
-            assert.strictEqual(httpCode, '204 NO CONTENT');
-            resolve();
-        }));
+        const { httpCode } = await provideRawOutputAsync(['-verbose', '-X', 'DELETE', url]);
+        assert.strictEqual(httpCode, '204 NO CONTENT');
     });
 });
