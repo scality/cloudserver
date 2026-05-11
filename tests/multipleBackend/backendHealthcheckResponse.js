5242cdfc966c82782149e4676d4d65f5af5c8efe
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2026-05-11 12:48:59 +0300

    CLDSRV-899: Raise Healthcheck response test timeout to 60s
    To reduce flakiness in mutliple-backend/Healthcheck response test.

--- tests/multipleBackend/backendHealthcheckResponse.js
+++ tests/multipleBackend/backendHealthcheckResponse.js
@@ -20,7 +20,9 @@ const log = new DummyRequestLogger();
 const locConstraints = Object.keys(config.locationConstraints);
 const azureClient = getAzureClient();
 
-describe('Healthcheck response', () => {
+describe('Healthcheck response', function describeHealthcheck() {
+    this.timeout(60000);
+
     it('should return result for every location constraint in ' +
     'locationConfig and every external locations with flightCheckOnStartUp ' +
     'set to true', done => {
